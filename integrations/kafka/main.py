2b02b5ba4da25f01341bd2a68c8e7acfe77ce9d6
danielsinai <daniel.sinai.main@gmail.com>
2023-06-03 00:28:16 +0300

    Kafka integration brings topics

--- integrations/kafka/main.py
+++ integrations/kafka/main.py
@@ -14,12 +14,19 @@ class Integration:
         topics = self.admin_client.describe_topics(topics=topic_list)
         topics_configurations = self.admin_client.describe_configs(config_resources=[
             ConfigResource(ConfigResourceType.TOPIC, topic) for topic in topic_list])
+        topics_configurations = topics_configurations[0]
 
         for topic in topics:
-            topics_configurations = topics_configurations[0]
-            for resource in topics_configurations.resources:
+            resources = list(topics_configurations.resources)
+            for resource in resources:
                 if topic["topic"] == resource[3]:
-                    topic["config"] = resource[4].to_list()
+                    config = []
+                    for config_entry in resource[4]:
+                        config_name = config_entry[0]
+                        config_value = config_entry[1]
+                        config.append(
+                            {"name": config_name, "value": config_value})
+                    topic["config"] = config
                     break
         return topics
 
