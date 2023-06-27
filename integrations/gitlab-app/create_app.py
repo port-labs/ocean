3d979e6ec4ede8f9b14f035fe95c53d1a6e87eab
yair <yairsimantov20@gmail.com>
2023-06-27 11:43:16 +0300

    idea fixes and types

--- integrations/gitlab-app/create_app.py
+++ integrations/gitlab-app/create_app.py
@@ -3,19 +3,22 @@ from typing import List, Dict
 from gitlabapp.integration import GitlabIntegration
 from port_ocean.port_ocean import Ocean
 from pydantic import BaseSettings, AnyHttpUrl, Field
+from pydantic.tools import parse_obj_as
 
 
 class LogicSettings(BaseSettings):
     token_mapping: Dict[str, List[str]] = Field(alias="tokenMapping")
     app_host: AnyHttpUrl = Field(alias="appHost")
-    gitlab_host: AnyHttpUrl = Field(alias="gitlabHost", default="https://gitlab.com")
+    gitlab_host: AnyHttpUrl = Field(
+        alias="gitlabHost", default=parse_obj_as(AnyHttpUrl, "https://gitlab.com")
+    )
 
     class Config(BaseSettings.Config):
         env_prefix = "LOGIC_"
 
 
-def create_app():
-    app = Ocean(integration_class=GitlabIntegration, config_class=LogicSettings)
+def create_app() -> Ocean:
+    app = Ocean(integration_class=GitlabIntegration, config_factory=LogicSettings)
     # noinspection PyUnresolvedReferences
     from gitlabapp import ocean
 
