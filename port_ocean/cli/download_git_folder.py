e839ad4d29aecf76f1d39416ff600613f540b049
yair <yairsimantov20@gmail.com>
2023-07-09 14:36:46 +0300

    cosmetic changes

--- port_ocean/cli/download_git_folder.py
+++ port_ocean/cli/download_git_folder.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-import requests
+import httpx
 
 
 def download_folder(repo_url: str, folder_path: str, destination_path: str) -> None:
@@ -14,26 +14,27 @@ def download_folder(repo_url: str, folder_path: str, destination_path: str) -> N
     api_url = f"https://api.github.com/repos/{owner}/{repo_name}/contents/{folder_path}"
 
     # Send a GET request to the API
-    response = requests.get(api_url)
+    response = httpx.get(api_url)
 
     # Check if the request was successful
-    if response.status_code == 200:
-        # Create the destination folder if it doesn't exist
-        if not os.path.exists(destination_path):
-            os.makedirs(destination_path)
-
-        # Iterate over the files and download them
-        repo_contents = response.json()
-        for content in repo_contents:
-            if content["type"] == "file":
-                file_url = content["download_url"]
-                file_name = os.path.join(destination_path, content["name"])
-
-                # Download the file
-                with requests.get(file_url, stream=True) as response_file:
-                    with open(file_name, "wb") as system_file:
-                        shutil.copyfileobj(response_file.raw, system_file)
-
-        print("Folder downloaded successfully!")
-    else:
+    if not response.is_success:
         print("Failed to download the folder.")
+        exit(1)
+
+    # Create the destination folder if it doesn't exist
+    if not os.path.exists(destination_path):
+        os.makedirs(destination_path)
+
+    # Iterate over the files and download them
+    repo_contents = response.json()
+    for content in repo_contents:
+        if content["type"] == "file":
+            file_url = content["download_url"]
+            file_name = os.path.join(destination_path, content["name"])
+
+            # Download the file
+            with requests.get(file_url, stream=True) as response_file:
+                with open(file_name, "wb") as system_file:
+                    shutil.copyfileobj(response_file.raw, system_file)
+
+    print("Folder downloaded successfully!")
