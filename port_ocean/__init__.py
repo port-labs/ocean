2a681aed95ab92f9ef9bf14bc51016d2973e94d0
yair <yairsimantov20@gmail.com>
2023-07-18 14:56:51 +0300

    fixed lint

--- port_ocean/__init__.py
+++ port_ocean/__init__.py
@@ -3,8 +3,8 @@ from importlib.metadata import version
 
 warnings.filterwarnings("ignore", category=FutureWarning)
 
-from .ocean import Ocean
-from .run import run
+from .ocean import Ocean  # noqa: E402
+from .run import run  # noqa: E402
 
 __version__ = version("port-ocean")
 
