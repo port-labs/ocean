46e1ebd4890c3ea99b109c4b33a9e411932845ad
yair <yairsimantov20@gmail.com>
2023-07-18 14:53:11 +0300

    fixed default resources behavior

--- port_ocean/__init__.py
+++ port_ocean/__init__.py
@@ -1,11 +1,12 @@
 import warnings
 from importlib.metadata import version
 
+warnings.filterwarnings("ignore", category=FutureWarning)
+
 from .ocean import Ocean
 from .run import run
 
 __version__ = version("port-ocean")
 
-warnings.filterwarnings("ignore", category=FutureWarning)
 
 __all__ = ["Ocean", "run"]
