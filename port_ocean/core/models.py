0f3ac5bcb2102a3d2eb2dc68b74d0f997a76d656
yair <yairsimantov20@gmail.com>
2023-07-20 14:42:05 +0300

    fixed pydantic failure

--- port_ocean/core/models.py
+++ port_ocean/core/models.py
@@ -1,16 +1,37 @@
-from typing import Any
+from typing import Any, Type, TypeVar
 
 from pydantic import BaseModel
 from pydantic.fields import Field
 
+Model = TypeVar("Model", bound="BaseModel")
+
 
 class Entity(BaseModel):
-    identifier: str
+    identifier: str = Field(strict=False)
     blueprint: str
     title: str | None
     team: str | list[str] = []
     properties: dict[str, Any] = {}
     relations: dict[str, Any] = {}
+
+    @classmethod
+    def parse_obj(cls: Type["Model"], obj: dict[Any, Any]) -> "Model":
+        obj["identifier"] = str(obj.get("identifier"))
+        obj["blueprint"] = str(obj.get("blueprint"))
+        if obj.get("team"):
+            team = obj.get("team")
+            obj["team"] = (
+                [str(item) for item in team]
+                if isinstance(team, list)
+                else str(obj.get("team"))
+            )
+
+        for key, value in obj.get("relations", {}).items():
+            if isinstance(value, list):
+                obj["relations"][key] = [str(item) for item in value]
+            else:
+                obj["relations"][key] = str(value)
+        return super(Entity, cls).parse_obj(obj)
 
 
 class BlueprintRelation(BaseModel):
