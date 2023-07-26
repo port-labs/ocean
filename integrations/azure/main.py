eab56d578ff1654ac19616427ec04ed5b46efd2d
tankilevitch <tomtankilevitch@gmail.com>
2023-07-26 11:33:46 +0300

    add middleware to ocean app

--- integrations/azure/main.py
+++ integrations/azure/main.py
@@ -1,3 +1,11 @@
 # noinspection PyUnresolvedReferences
 # ruff: noqa: F401
 from azure_integration import ocean
+from integration import cloud_event_validation_middleware_handler
+
+from port_ocean.context.ocean import ocean
+
+
+ocean.app.fast_api_app.middleware("azure_cloud_event")(
+    cloud_event_validation_middleware_handler
+)
