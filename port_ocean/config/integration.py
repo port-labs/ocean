c78d04cfdc780131a5a8e6e6e48b3cc1cae39173
yair <yairsimantov20@gmail.com>
2023-07-27 00:52:39 +0300

    sorted the settings

--- port_ocean/config/integration.py
+++ port_ocean/config/integration.py
@@ -5,6 +5,22 @@ from pydantic import BaseSettings, BaseModel, Extra, AnyHttpUrl, parse_obj_as
 from port_ocean.config.base import BaseOceanSettings
 from port_ocean.core.event_listener import EventListenerSettingsType
 
+LogLevelType = Literal["ERROR", "WARNING", "INFO", "DEBUG", "CRITICAL"]
+
+
+class ApplicationSettings(BaseSettings):
+    log_level: LogLevelType = "DEBUG"
+    port: int = 8000
+
+    class Config:
+        env_prefix = "APPLICATION__"
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+        @classmethod
+        def customise_sources(cls, init_settings, env_settings, *_, **__):  # type: ignore
+            return env_settings, init_settings
+
 
 class PortSettings(BaseModel, extra=Extra.allow):
     client_id: str
@@ -24,20 +40,3 @@ class IntegrationConfiguration(BaseOceanSettings, extra=Extra.allow):
     batch_work_size: int = 20
     initialize_port_resources: bool = False
     integration: IntegrationSettings
-
-
-LogLevelType = Literal["ERROR", "WARNING", "INFO", "DEBUG", "CRITICAL"]
-
-
-class ApplicationSettings(BaseSettings):
-    log_level: LogLevelType = "DEBUG"
-    port: int = 8000
-
-    class Config:
-        env_prefix = "APPLICATION__"
-        env_file = ".env"
-        env_file_encoding = "utf-8"
-
-        @classmethod
-        def customise_sources(cls, init_settings, env_settings, *_, **__):  # type: ignore
-            return env_settings, init_settings
