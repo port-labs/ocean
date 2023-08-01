163d131ba4d9a659810615bdc8fc47bb717863b6
tankilevitch <tomtankilevitch@gmail.com>
2023-08-02 00:41:30 +0300

    add entities core implementation for interacting with newrelic api

---
+++ integrations/newrelic/newrelic_integration/core/entities.py
@@ -0,0 +1,136 @@
+from typing import Optional
+from loguru import logger
+
+from newrelic_integration.utils import (
+    get_port_resource_configuration_by_port_kind,
+    render_query,
+)
+from newrelic_integration.core.paging import send_paginated_graph_api_request
+from newrelic_integration.core.utils import send_graph_api_request
+
+
+class EntitiesHandler:
+    async def get_entity(self, entity_guid: str) -> dict:
+        query_template = """
+{
+  actor {
+    entity(guid: "{{ entity_guid }}") {
+      entityType
+      guid
+      domain
+      name
+      permalink
+      reporting
+      tags {
+        key
+        values
+      }
+      type
+    }
+  }
+}
+        """
+
+        query = await render_query(query_template, entity_guid=entity_guid)
+        response = await send_graph_api_request(
+            query=query, request_type="get_entity", entity_guid=entity_guid
+        )
+        entity = response.get("data", {}).get("actor", {}).get("entity", {})
+        self._format_tags(entity)
+        return entity
+
+    async def list_entities_by_resource_kind(self, resource_kind: str):
+        query_template = """
+{
+  actor {
+    entitySearch(query: "{{ entity_query_filter }}") {
+      results{{ next_cursor_request }} {
+        entities {
+          entityType
+          type
+          tags {
+            key
+            values
+          }
+          reporting
+          name
+          lastReportingChangeAt
+          guid
+          domain
+          accountId
+          alertSeverity
+          permalink
+        }
+        nextCursor
+      }
+    }
+  }
+}
+    """
+        resource_config = await get_port_resource_configuration_by_port_kind(
+            resource_kind
+        )
+        entity_query_filter = resource_config.get("selector", {}).get(
+            "entity_query_filter"
+        )
+        if not entity_query_filter:
+            logger.error(
+                "No entity_query_filter found for resource", resource_kind=resource_kind
+            )
+
+        async def extract_entities(
+            response: dict,
+        ) -> (Optional[str], list[Optional[dict]]):
+            results = (
+                response.get("data", {})
+                .get("actor", {})
+                .get("entitySearch", {})
+                .get("results", {})
+            )
+            return results.get("nextCursor"), results.get("entities", [])
+
+        async for entity in send_paginated_graph_api_request(
+            query_template,
+            extract_data=extract_entities,
+            entity_query_filter=entity_query_filter,
+        ):
+            self._format_tags(entity)
+            yield entity
+
+    async def list_entities_by_guids(self, entity_guids: list[str]):
+        # entities api doesn't support pagination
+        query = """
+{
+    actor {
+        entities(guids: {{ entity_guids }}) {
+            entityType
+            type
+            tags {
+                key
+                values
+            }
+            reporting
+            name
+            lastReportingChangeAt
+            guid
+            domain
+            accountId
+            alertSeverity
+            permalink
+        }
+    }
+}
+    """
+        query = await render_query(query, entity_guids=entity_guids)
+        response = await send_graph_api_request(
+            query, request_type="list_entities_by_guids", entity_guids=entity_guids
+        )
+        entities = response.get("data", {}).get("actor", {}).get("entities", [])
+        for entity in entities:
+            self._format_tags(entity)
+        return entities
+
+    @staticmethod
+    def _format_tags(entity: dict) -> dict:
+        entity["tags"] = {tag["key"]: tag["values"] for tag in entity.get("tags", [])}
+        return entity
---
+++ integrations/newrelic/newrelic_integration/core/utils.py
@@ -0,0 +1,28 @@
+import jinja2
+import httpx
+
+from port_ocean.context.ocean import ocean
+from loguru import logger
+
+
+async def render_query(query_template: str, **kwargs) -> str:
+    template = jinja2.Template(query_template, enable_async=True)
+    return await template.render_async(
+        **kwargs,
+    )
+
+
+async def send_graph_api_request(query: str, **log_fields) -> dict:
+    async with httpx.AsyncClient() as client:
+        logger.debug("Sending graph api request", **log_fields)
+        response = await client.post(
+            ocean.integration_config.get("new_relic_graphql_apiurl"),
+            headers={
+                "Content-Type": "application/json",
+                "API-Key": ocean.integration_config.get("new_relic_api_key"),
+            },
+            json={"query": query},
+        )
+        logger.debug("Received graph api response", **log_fields)
+        response.raise_for_status()
+        return response.json()
