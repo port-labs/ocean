9eafbf9c8cdd5fc8079b2465af4ab2d96aa0751e
Tom Tankilevitch <59158507+Tankilevitch@users.noreply.github.com>
2023-08-02 17:16:01 +0300

    Add `NewRelic` ocean integration [PORT-4382] (#44)
    * add .port/resources files
    
    * add default project files
    
    * add overrides for default portAppConfig
    
    * add utils methods
    
    * add async pagination implementation
    
    * add entities core implementation for interacting with newrelic api
    
    * add issues core implementation for interacting with newrelic API
    
    * add newrelic integration with ocean
    
    * update pyproject.toml and poetry.lock
    
    * improve querying efficiency for entities per issues
    
    * linting
    
    * fix the version
    
    * fix CR comments
    
    * add APM & ALERTING to spec.yaml
    
    * some adjustments
    
    * use same httpxclient through resync and on event
    
    * add NewRelicTypes: ['ISSUE'] to newRelicAlert
    
    * linting
    
    * fix
    
    * add changelog
    
    * bump rc2
    
    * update scorecards.json
    
    * bump rc3
    
    * fix scorecards
    
    * last scorecards fix
    
    * fix conditionName
    
    * update config.yaml to support 0.1.3 ocean
    
    * bump ocean
    
    * createMissingRelatedEntities: true
    bump rc
    
    * change readme
    
    * rc 0.1.0

---
+++ integrations/newrelic/newrelic_integration/core/utils.py
@@ -0,0 +1,38 @@
+from typing import Any, cast
+
+import httpx
+import jinja2
+from loguru import logger
+from port_ocean.context.ocean import ocean
+
+
+async def render_query(query_template: str, **kwargs: Any) -> str:
+    """
+    This function is used to render the query template with the given kwargs.
+    :param query_template: The query template to render.
+    :param kwargs: The kwargs to pass to the query template.
+    :return: The rendered query.
+    """
+    template = jinja2.Template(query_template, enable_async=True)
+    return await template.render_async(
+        **kwargs,
+    )
+
+
+async def send_graph_api_request(
+    async_client: httpx.AsyncClient, query: str, **log_fields: Any
+) -> Any:
+    logger.debug("Sending graph api request", **log_fields)
+    api_url = cast(str, ocean.integration_config.get("new_relic_graphql_apiurl"))
+    new_relic_api_key = cast(str, ocean.integration_config.get("new_relic_api_key"))
+    response = await async_client.post(
+        api_url,
+        headers={
+            "Content-Type": "application/json",
+            "API-Key": new_relic_api_key,
+        },
+        json={"query": query},
+    )
+    logger.debug("Received graph api response", **log_fields)
+    response.raise_for_status()
+    return response.json()
