ce23ab9aea333f05b69fc0b0a94338229e9136d7
tankilevitch <tomtankilevitch@gmail.com>
2023-08-03 13:50:30 +0300

    add logger contextualize as well as don't use RelationIdentifier anymore

--- integrations/newrelic/newrelic_integration/ocean.py
+++ integrations/newrelic/newrelic_integration/ocean.py
@@ -13,83 +13,93 @@ from newrelic_integration.utils import (
 
 @ocean.on_resync()
 async def resync_entities(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
-    logger.debug("Resyncing entities", kind=kind)
-    port_resource_configuration = await get_port_resource_configuration_by_port_kind(
-        kind
-    )
-    # mainly for issues newRelicAlert as it has different resync logic than entities
-    if not port_resource_configuration.get("selector", {}).get("entity_query_filter"):
-        logger.debug(
-            "Skipping resync for kind without entity_query_filter",
-            kind=kind,
+    with logger.contextualize(resource_kind=kind):
+        port_resource_configuration = (
+            await get_port_resource_configuration_by_port_kind(kind)
         )
-        return
-    else:
-        async with httpx.AsyncClient() as http_client:
-            async for entity in EntitiesHandler.list_entities_by_resource_kind(
-                http_client, kind
-            ):
-                number_of_open_issues = (
-                    await IssuesHandler.get_number_of_issues_by_entity_guid(
-                        http_client, entity["guid"], issue_state=IssueState.ACTIVATED
-                    )
-                )
-                entity["open_issues_count"] = number_of_open_issues
-                yield entity
+        # mainly for issues newRelicAlert as it has different resync logic than entities
+        if not port_resource_configuration.get("selector", {}).get(
+            "entity_query_filter"
+        ):
+            logger.debug(
+                "Skipping resync for kind without entity_query_filter",
+                kind=kind,
+            )
+            return
+        else:
+            async with httpx.AsyncClient() as http_client:
+                async for entity in EntitiesHandler.list_entities_by_resource_kind(
+                    http_client, kind
+                ):
+                    if port_resource_configuration.get("selector", {}).get(
+                        "calculate_issue_count"
+                    ):
+                        number_of_open_issues = (
+                            await IssuesHandler.get_number_of_issues_by_entity_guid(
+                                http_client,
+                                entity["guid"],
+                                issue_state=IssueState.ACTIVATED,
+                            )
+                        )
+                        entity["open_issues_count"] = number_of_open_issues
+                    yield entity
 
 
 @ocean.on_resync(kind="newRelicAlert")
 async def resync_issues(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
-    async with httpx.AsyncClient() as http_client:
-        issues = await IssuesHandler.list_issues(http_client)
-        for issue in issues:
-            yield issue
+    with logger.contextualize(resource_kind=kind):
+        async with httpx.AsyncClient() as http_client:
+            issues = await IssuesHandler.list_issues(http_client)
+            for issue in issues:
+                yield issue
 
 
 @ocean.router.post("/events")
 async def handle_issues_events(issue: IssueEvent) -> dict[str, bool]:
-    logger.info(
-        "Received issue event",
-        issue_id=issue.id,
-        issue_state=issue.state,
-    )
-    issue_record = issue.dict(by_alias=True)
-    async with httpx.AsyncClient() as http_client:
-        for entity_guid in issue_record["entityGuids"]:
-            # get the entity from new
-            entity = await EntitiesHandler.get_entity(
-                http_client, entity_guid=entity_guid
-            )
-            resource_configuration = (
-                await get_port_resource_configuration_by_newrelic_entity_type(
-                    entity["type"]
+    with logger.contextualize(issue_id=issue.id, issue_state=issue.state):
+        logger.info(
+            "Received issue event",
+        )
+        issue_record = issue.dict(by_alias=True)
+        async with httpx.AsyncClient() as http_client:
+            for entity_guid in issue_record["entityGuids"]:
+                # get the entity from new
+                entity = await EntitiesHandler.get_entity(
+                    http_client, entity_guid=entity_guid
                 )
-            )
-            if not resource_configuration:
-                logger.warning(
-                    "Received issue event for unknown entity type, ignoring",
-                    entity_type=entity["type"],
-                )
-            else:
-                number_of_open_issues = (
-                    await IssuesHandler().get_number_of_issues_by_entity_guid(
-                        http_client, entity_guid, issue_state=IssueState.ACTIVATED
+                resource_configuration = (
+                    await get_port_resource_configuration_by_newrelic_entity_type(
+                        entity["type"]
                     )
                 )
-                entity["open_issues_count"] = number_of_open_issues
-                relation_identifier = resource_configuration.get("selector", {}).get(
-                    "relation_identifier"
-                )
-                issue_record.setdefault(
-                    relation_identifier,
-                    {},
-                ).setdefault(
-                    "entity_guids", []
-                ).append(entity_guid)
-                await ocean.register_raw(resource_configuration["kind"], [entity])
-    issue_resource_config = (
-        await get_port_resource_configuration_by_newrelic_entity_type("ISSUE")
-    )
-    port_issue_kind = issue_resource_config.get("kind", "newRelicAlert")
-    await ocean.register_raw(port_issue_kind, [issue_record])
-    return {"ok": True}
+                if not resource_configuration:
+                    logger.warning(
+                        "Received issue event for unknown entity type, ignoring",
+                        entity_type=entity["type"],
+                    )
+                else:
+                    if resource_configuration.get("selector", {}).get(
+                        "calculate_issue_count"
+                    ):
+                        number_of_open_issues = (
+                            await IssuesHandler().get_number_of_issues_by_entity_guid(
+                                http_client,
+                                entity_guid,
+                                issue_state=IssueState.ACTIVATED,
+                            )
+                        )
+                        entity["open_issues_count"] = number_of_open_issues
+                    issue_record.setdefault(
+                        entity["type"],
+                        {},
+                    ).setdefault(
+                        "entity_guids", []
+                    ).append(entity_guid)
+                    await ocean.register_raw(resource_configuration["kind"], [entity])
+        issue_resource_config = (
+            await get_port_resource_configuration_by_newrelic_entity_type("ISSUE")
+        )
+        # get the port issue kind from the resource configuration if exists, otherwise use the default kind newRelicAlert
+        port_issue_kind = issue_resource_config.get("kind", "newRelicAlert")
+        await ocean.register_raw(port_issue_kind, [issue_record])
+        return {"ok": True}
