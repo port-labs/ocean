f49d9e8695ff57693b696775dd7fb6c646f3de86
yair <yairsimantov20@gmail.com>
2023-08-06 23:01:08 +0300

    fixed entity jq parsing

--- port_ocean/core/models.py
+++ port_ocean/core/models.py
@@ -10,7 +10,7 @@ class Entity(BaseModel):
     identifier: str
     blueprint: str
     title: str | None
-    team: str | list[str] = []
+    team: str | None | list[str | None] = []
     properties: dict[str, Any] = {}
     relations: dict[str, Any] = {}
 
@@ -18,19 +18,6 @@ class Entity(BaseModel):
     def parse_obj(cls: Type["Model"], obj: dict[Any, Any]) -> "Model":
         obj["identifier"] = str(obj.get("identifier"))
         obj["blueprint"] = str(obj.get("blueprint"))
-        if obj.get("team"):
-            team = obj.get("team")
-            obj["team"] = (
-                [str(item) for item in team]
-                if isinstance(team, list)
-                else str(obj.get("team"))
-            )
-
-        for key, value in obj.get("relations", {}).items():
-            if isinstance(value, list):
-                obj["relations"][key] = [str(item) for item in value]
-            else:
-                obj["relations"][key] = str(value)
         return super(Entity, cls).parse_obj(obj)
 
 
