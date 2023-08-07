e0dd6cdc1672bd5264a3eb5a9b2e0f0a99a95890
PeyGis <isaac.p.coffie@gmail.com>
2023-08-07 08:43:05 +0000

    add linting

--- integrations/pagerduty/clients/pagerduty.py
+++ integrations/pagerduty/clients/pagerduty.py
@@ -69,9 +69,7 @@ class PagerDutyClient:
 
                     # Fetch and update on-call user information for services
                     if data_key == "services":
-                        service_data = await self.update_oncall_users(
-                            data, data_key
-                        )
+                        service_data = await self.update_oncall_users(data, data_key)
                         all_data.extend(service_data)
                     else:
                         all_data.extend(data[data_key])
