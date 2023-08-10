c4879e511ce8238f5b2210bcf3ff5032726c6f4f
tankilevitch <tomtankilevitch@gmail.com>
2023-08-10 11:06:02 +0300

    add providers.tf to example

---
+++ deployment/terraform/azure/containerapp/examples/azure-integration/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.68.0"
+    }
+  }
+}
+provider "azurerm" {
+  # The AzureRM Provider supports authenticating using via the Azure CLI, a Managed Identity
+  # and a Service Principal. More information on the authentication methods supported by
+  # the AzureRM Provider can be found here:
+  # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs#authenticating-to-azure
+
+  # The features block allows changing the behaviour of the Azure Provider, more
+  # information can be found here:
+  # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/guides/features-block
+  features {}
+}
