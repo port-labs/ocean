a75f8b14872c3221e00cd3003dff46edc27a124a
danielsinai <daniel.sinai.main@gmail.com>
2023-08-17 14:13:12 +0300

    Fixed Pager Duty integration to use client inside the functions

--- integrations/pagerduty/main.py
+++ integrations/pagerduty/main.py
@@ -11,16 +11,14 @@ class ObjectKind:
     INCIDENTS = "incidents"
 
 
-pager_duty_client = PagerDutyClient(
-    ocean.integration_config.get("token", ""),
-    ocean.integration_config.get("api_url", ""),
-    ocean.integration_config.get("app_host", ""),
-)
-
-
 @ocean.on_resync(ObjectKind.INCIDENTS)
 async def on_incidents_resync(kind: str) -> list[dict[str, Any]]:
     logger.info(f"Listing Pagerduty resource: {kind}")
+    pager_duty_client = PagerDutyClient(
+        ocean.integration_config["token"],
+        ocean.integration_config["api_url"],
+        ocean.integration_config["app_host"],
+    )
 
     return await pager_duty_client.paginate_request_to_pager_duty(
         data_key=ObjectKind.INCIDENTS
@@ -30,6 +28,11 @@ async def on_incidents_resync(kind: str) -> list[dict[str, Any]]:
 @ocean.on_resync(ObjectKind.SERVICES)
 async def on_services_resync(kind: str) -> list[dict[str, Any]]:
     logger.info(f"Listing Pagerduty resource: {kind}")
+    pager_duty_client = PagerDutyClient(
+        ocean.integration_config["token"],
+        ocean.integration_config["api_url"],
+        ocean.integration_config["app_host"],
+    )
 
     services = await pager_duty_client.paginate_request_to_pager_duty(
         data_key=ObjectKind.SERVICES
@@ -39,6 +42,11 @@ async def on_services_resync(kind: str) -> list[dict[str, Any]]:
 
 @ocean.router.post("/webhook")
 async def upsert_incident_webhook_handler(data: dict[str, Any]) -> None:
+    pager_duty_client = PagerDutyClient(
+        ocean.integration_config["token"],
+        ocean.integration_config["api_url"],
+        ocean.integration_config["app_host"],
+    )
     event_type = data["event"]["event_type"]
     logger.info(f"Processing Pagerduty webhook for event type: {event_type}")
     if event_type in pager_duty_client.service_delete_events:
@@ -63,5 +71,10 @@ async def upsert_incident_webhook_handler(data: dict[str, Any]) -> None:
 
 @ocean.on_start()
 async def on_start() -> None:
+    pager_duty_client = PagerDutyClient(
+        ocean.integration_config["token"],
+        ocean.integration_config["api_url"],
+        ocean.integration_config["app_host"],
+    )
     logger.info("Subscribing to Pagerduty webhooks")
     await pager_duty_client.create_webhooks_if_not_exists()
