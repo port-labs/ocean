66fcf947efb0b995cf8a1a75bae8d28b1246aac5
yair <yairsimantov20@gmail.com>
2023-08-22 12:53:21 +0300

    showing integration icon

--- docs/framework-guides/src/components/Icon/index.tsx
+++ docs/framework-guides/src/components/Icon/index.tsx
@@ -1,30 +1,54 @@
 import React, {useEffect, useState} from 'react';
+import XMLParser from 'react-xml-parser';
+
 
 import './custom.css';
+
+const BUCKET_URL = "https://port-graphical-assets.s3.eu-west-1.amazonaws.com";
 
 function Icon(props): JSX.Element {
     const {iconName} = props;
     const [Icon, setIcon] = useState(null);
 
     useEffect(async () => {
-        for (const folder of ["blueprints", "blueprintsColor"]) {
+        for (const folder of ["blueprintsColor", "blueprints"]) {
             try {
-                const resposne = await fetch(`https://port-graphical-assets.s3.eu-west-1.amazonaws.com/icons/${folder}/${iconName}.svg`)
-                if (resposne.status === 200) {
-                    const text = await resposne.text();
-                    console.log(text);
-                    setIcon(<div className={"svg-container"} dangerouslySetInnerHTML={{__html: text}}/>);
+                const seachParams = new URLSearchParams({
+                    "list-type": '2',
+                    prefix: `icons/${folder}`
+                })
+                const response = await fetch(`${BUCKET_URL}?` + seachParams)
+                const r = await response.text()
+                const xml = new XMLParser().parseFromString(r);
+                const iconInS3 = xml.children.filter((child) => child.name === "Contents").map((content) => {
+                    const key = content.children.find((child) => child.name === "Key").value;
+                    const name = key.split("/").pop().split(".")[0];
+                    return [name, key]
+                }).find(([name, key]) => name.toLowerCase() === iconName.toLowerCase());
+
+                if (iconInS3) {
+                    const svgResponse = await fetch(`${BUCKET_URL}/${iconInS3[1]}`)
+                    const text = await svgResponse.text();
+                    setIcon(text);
+                    return;
                 }
             } catch (error) {
-            }
-
-            if (Icon) {
-                return;
             }
         }
     }, [iconName]);
 
-    return Icon;
+    return (
+        <div className={"svg-container"} style={{
+            backgroundColor: 'white',
+            borderRadius: "50%",
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            border: "1px solid #dddddd"
+        }}>
+            <div style={{width: '80%', height: '80%'}} dangerouslySetInnerHTML={{__html: Icon}}/>
+        </div>
+    );
 }
 
 export default Icon;
