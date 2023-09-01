ed1a8e647f6b4ba3d2bec300e0b849fe0ae1b552
yair <yairsimantov20@gmail.com>
2023-09-01 12:31:00 +0300

    lint

--- port_ocean/context/event.py
+++ port_ocean/context/event.py
@@ -14,7 +14,7 @@ from typing import (
 from uuid import uuid4
 
 from loguru import logger
-from pydispatch import dispatcher
+from pydispatch import dispatcher  # type: ignore
 from werkzeug.local import LocalStack, LocalProxy
 
 from port_ocean.context.resource import resource
