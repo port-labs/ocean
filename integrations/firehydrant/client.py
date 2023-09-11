865bba9d7a8a40d6bd28631b8478057dd50173d2
PeyGis <isaac.p.coffie@gmail.com>
2023-09-11 16:43:48 +0000

    improve on integration using pr comment

--- integrations/firehydrant/client.py
+++ integrations/firehydrant/client.py
@@ -15,14 +15,6 @@ endpoint_resource_type_mapper = {
 
 class FirehydrantClient:
     def __init__(self, base_url: str, api_key: str, app_host: str):
-        """
-        Initialize Firehydrant client
-
-        :param base_url: Firehydrant base URL
-        :param api_key: Firehydrant API key
-        :param app_host: Application host URL
-        :param http_client: httpx.AsyncClient instance
-        """
         self.base_url = base_url
         self.api_key = api_key
         self.app_host = app_host
@@ -42,15 +34,6 @@ class FirehydrantClient:
         query_params: Optional[dict[str, Any]] = None,
         json_data: Optional[dict[str, Any]] = None,
     ) -> dict[str, Any]:
-        """
-        Sends an API request to Firehydrant
-
-        :param endpoint: API endpoint URL
-        :param method: HTTP method (default: 'GET')
-        :param query_params: Query parameters (default: None)
-        :param json_data: JSON data to send in request body (default: None)
-        :return: Response JSON data
-        """
         try:
             response = await self.http_client.request(
                 method=method,
@@ -67,76 +50,11 @@ class FirehydrantClient:
             )
             raise
 
-    async def send_paginated_api_request(
-        self,
-        endpoint: str,
-        data_key: str,
-        method: str = "GET",
-        query_params: Optional[dict[str, Any]] = None,
-        json_data: Optional[dict[str, Any]] = None,
-    ) -> list[dict[str, Any]]:
-        """
-        Sends an API request to Firehydrant and returns all resources available. It does not use generator
-
-        :param endpoint: API endpoint URL
-        :param data_key: Resource key to fetch
-        :param method: HTTP method (default: 'GET')
-        :param query_params: Query parameters (default: None)
-        :param json_data: JSON data to send in request body (default: None)
-        :return: Response JSON data
-        """
-        try:
-            all_resources = []  # List to hold all fetched resources
-
-            while True:
-                response = await self.http_client.request(
-                    method=method,
-                    url=f"{self.base_url}/v1/{endpoint}",
-                    params=query_params,
-                    json=json_data,
-                    headers=self.api_auth_header,
-                )
-                response.raise_for_status()
-                response_json = response.json()
-
-                all_resources.extend(response_json.get(data_key, []))
-
-                # Check for paging information and decide whether to fetch more pages
-                paging_info = response_json.get("pagination")
-                if paging_info and paging_info.get("page", 0) < paging_info.get(
-                    "pages", 0
-                ):
-                    query_params = query_params or {}
-                    query_params["page"] = paging_info["page"] + 1
-                else:
-                    break
-
-            return all_resources
-
-        except httpx.HTTPStatusError as e:
-            print(
-                f"HTTP error with status code: {e.response.status_code} and response text: {e.response.text}"
-            )
-            raise
-
     async def get_paginated_resource(
         self, resource_type: str
     ) -> AsyncGenerator[list[dict[str, Any]], None]:
-        """
-        Fetch paginated resources of a specific type from the Firehydrant API.
-
-        Args:
-            resource_type (str): The type of resource to fetch, e.g., "incidents" or "webhooks."
-
-        Yields:
-            AsyncGenerator[list[dict[str, Any]], None]: A generator that yields lists of resource data.
-                Each yielded list contains a batch of resources of the specified type.
-
-        Returns:
-            None: This function does not return a value but yields resource data via asynchronous iteration.
-        """
         logger.info(f"Getting {resource_type} from Firehydrant")
-        endpoint = endpoint_resource_type_mapper.get(resource_type, "")
+        endpoint = endpoint_resource_type_mapper.get(resource_type, resource_type)
 
         pagination_params: dict[str, Any] = {
             "page": 1,
@@ -163,97 +81,56 @@ class FirehydrantClient:
         except Exception as e:
             logger.error(f"Error while fetching {resource_type}: {str(e)}")
 
+    async def get_single_incident(self, incident_id: str) -> dict[str, Any]:
+        return await self.send_api_request(endpoint=f"incidents/{incident_id}")
+
+    async def get_single_service(self, service_id: str) -> dict[str, Any]:
+        serice_data = await self.send_api_request(endpoint=f"services/{service_id}")
+        service_analytics_data = await self.compute_service_mean_time_metrics(
+            serice_data["active_incidents"]
+        )
+        serice_data["__incidentMetrics"] = service_analytics_data
+        return serice_data
+
+    async def get_single_environment(self, environment_id: str) -> dict[str, Any]:
+        return await self.send_api_request(endpoint=f"environments/{environment_id}")
+
+    async def get_single_retrospective(self, report_id: str) -> dict[str, Any]:
+        report_data = await self.send_api_request(
+            endpoint=f"post_mortems/reports/{report_id}"
+        )
+        enriched_data = await self.enrich_incident_report_data(report_data)
+        report_data["__enrichedData"] = enriched_data
+        return report_data
+
     async def enrich_incident_report_data(
         self, report: dict[str, Any]
     ) -> dict[str, Any]:
-        """
-        Enriches an incident report by adding tasks, duration, and retrospective questions. It takes an incident report as input and adds additional information to it, including tasks related to the incident, duration metrics, and retrospective questions. The resulting enriched report is returned as a dictionary.
-
-        Args:
-            report (dict[str, Any]): The incident report data to be enriched.
-
-        Returns:
-            dict[str, Any]: The enriched incident report with added information.
-        """
-
-        ## get incident task completion summary statistics
+        ## get incident taskS
         incident_id = report.get("incident", {}).get("id")
-        task_completion_status = await self.get_tasks_by_incident(
-            incident_id=incident_id
-        )
+        tasks = await self.get_tasks_by_incident(incident_id=incident_id)
 
         ## get incident duration data
         milestones = report.get("incident", {}).get("milestones", [])
         incident_duration = await self.compute_incident_duration(milestones=milestones)
 
-        ## get retrospective questions and answers
-        retro_responses = await self.get_retrospective_questions_and_answers(
-            questions=report.get("questions", [])
-        )
-
         return {
-            "tasks": task_completion_status,
+            "tasks": tasks,
             "duration": incident_duration,
-            "questions": retro_responses,
         }
 
-    async def get_tasks_by_incident(self, incident_id: str) -> dict[str, Any]:
-        """
-        Retrieve tasks associated with a specific incident. The returned data includes details about each task, such as the number of completed and incompleted tasks.
-
-        Args:
-            incident_id (str): The unique identifier of the incident for which to fetch tasks.
-
-        Returns:
-            dict[str, Any]: A dictionary containing information about tasks related to the incident.
-        """
+    async def get_tasks_by_incident(self, incident_id: str) -> list[dict[str, Any]]:
         logger.info(f"Getting tasks details for incident: {incident_id}")
-        params: dict[str, Any] = {
-            "page": 1,
-            "per_page": PAGE_SIZE,
-        }
-        task_url = f"incidents/{incident_id}/tasks"
+        task_endpoint = f"incidents/{incident_id}/tasks"
+        tasks = []
 
-        tasks = await self.send_paginated_api_request(
-            endpoint=task_url, query_params=params, data_key="data"
-        )
-        task_completion_status = await self.get_task_completion_status(tasks)
-        return task_completion_status
-
-    async def get_task_completion_status(
-        self, tasks: list[dict[str, Any]]
-    ) -> dict[str, Any]:
-        """
-        Return statistics about completed and uncompleted tasks. It returns a dictionary with counts of completed and uncompleted tasks, providing insights into task progress.
-
-        Args:
-            tasks (list[dict[str, Any]]): A list of task objects, each represented as a dictionary.
-
-        Returns:
-            dict[str, Any]: A dictionary containing statistics about completed and uncompleted tasks.
-        """
-        logger.info("Computing task completion statistics")
-        total_completed_tasks = 0
-        for task in tasks:
-            if task.get("state") == "done":
-                total_completed_tasks += 1
-
-        return {
-            "completed": total_completed_tasks,
-            "incompleted": len(tasks) - total_completed_tasks,
-        }
+        async for item in self.get_paginated_resource(task_endpoint):
+            tasks.extend(item)
+        return tasks
 
     async def compute_incident_duration(
         self, milestones: list[dict[str, Any]]
     ) -> Optional[float]:
-        """Compute the duration in minutes it took to resolve an incident based on milestones.
-
-        Args:
-            milestones (list[dict[str, Any]]): A list of milestone objects representing incident events.
-
-        Returns:
-            float: The duration in minutes it took to resolve the incident.
-        """
         started_time = None
         resolved_time = None
 
@@ -276,45 +153,11 @@ class FirehydrantClient:
             duration_hours = duration.total_seconds() / 60  # Convert to minutes
             return round(duration_hours, 2)
 
-        return None  # Return None if either "started" or "resolved" event is missing.
-
-    async def get_retrospective_questions_and_answers(
-        self, questions: list[dict[str, Any]]
-    ) -> list[dict[str, Any]]:
-        """Format retrospective questions by renaming title to 'question' and body to 'answer'.
-
-        Args:
-            questions (list[dict[str, Any]]): A list of retrospective question objects.
-
-        Returns:
-            list[dict[str, Any]]: A list of formatted retrospective questions with 'question' and 'answer' keys.
-        """
-
-        retro_responses_list = []
-        for question in questions:
-            retro_responses_list.append(
-                {"question": question.get("title"), "answer": question.get("body")}
-            )
-        return retro_responses_list
+        return None
 
     async def compute_service_mean_time_metrics(
         self, active_incidents_ids: list[Any]
     ) -> dict[str, Any]:
-        """Computes mean time service details for active incidents.
-
-        Args:
-            active_incidents_ids (list[Any]): A list of active incident IDs.
-
-        Returns:
-            dict[str, Any]: A dictionary containing computed mean time service metrics,
-            including time to acknowledge, time to identify, time to mitigate, and time to resolve.
-
-        Note:
-            This function calculates various incident analytics metrics based on milestone data.
-            The metrics may include Mean Time to Detection (MTTD), Mean Time to Acknowledged (MTTA),
-            Mean Time to Mitigation (MTTM), and Mean Time to Resolution (MTTR).
-        """
-
         if len(active_incidents_ids) == 0:
             return {}
 
@@ -342,30 +185,11 @@ class FirehydrantClient:
 
         return service_metrics
 
-    async def get_single_incident(self, incident_id: str) -> dict[str, Any]:
-        """Retrieve information about a single incident by its unique identifier.
-
-        Args:
-            incident_id (str): The unique identifier of the incident to retrieve.
-
-        Returns:
-            dict[str, Any]: A dictionary containing information about the specified incident.
-
-        """
-        return await self.send_api_request(endpoint=f"incidents/{incident_id}")
-
     async def compute_incident_analytics_metrics(
         self, milestones: list[dict[str, Any]]
     ) -> dict[str, Any]:
         """
         Compute incident analytics metrics based on milestone data. Formular retrieved from https://firehydrant.com/docs/managing-incidents/incident-metrics/
-
-        Args:
-            milestones (list[dict[str, Any]]): A list of milestone objects representing incident events.
-
-        Returns:
-            dict[str, Any]: A dictionary containing computed incident analytics metrics.
-
         """
 
         incident_times = {
@@ -414,25 +238,11 @@ class FirehydrantClient:
         return result
 
     async def create_webhooks_if_not_exists(self) -> None:
-        """
-        Create webhooks if they do not already exist in the Firehydrant account.
-
-        This function checks if webhooks are already configured in the Firehydrant account.
-        If webhooks do not exist, it creates them. If they do exist, it takes no action.
-        It uses specification from https://developers.firehydrant.com/#/operations/postV1Webhooks
-
-        Returns:
-            None: This function does not return a value
-        """
-
-        params: dict[str, Any] = {
-            "page": 1,
-            "per_page": PAGE_SIZE,
-        }
         webhook_endpoint = "webhooks"
-        all_subscriptions = await self.send_paginated_api_request(
-            endpoint=webhook_endpoint, query_params=params, data_key="data"
-        )
+        all_subscriptions = []
+
+        async for item in self.get_paginated_resource(webhook_endpoint):
+            all_subscriptions.extend(item)
 
         app_host_webhook_url = f"{self.app_host}/integration/webhook"
 
@@ -443,7 +253,7 @@ class FirehydrantClient:
         body = {
             "url": app_host_webhook_url,
             "state": "active",
-            "subscriptions": ["incidents"],
+            "subscriptions": ["incidents", "change_event"],
         }
 
         await self.send_api_request(
--- integrations/firehydrant/main.py
+++ integrations/firehydrant/main.py
@@ -25,20 +25,20 @@ def init_client() -> FirehydrantClient:
 
 ## Enriches the incident report data
 async def process_incident_tasks(
-    semaphore: asyncio.Semaphore, report: dict[str, Any]
+    http_client: FirehydrantClient, semaphore: asyncio.Semaphore, report: dict[str, Any]
 ) -> dict[str, Any]:
-    firehydrant_client = init_client()
     async with semaphore:
-        return await firehydrant_client.enrich_incident_report_data(report)
+        return await http_client.enrich_incident_report_data(report)
 
 
 ## Enriches the services data
 async def process_service_analytics(
-    semaphore: asyncio.Semaphore, service: dict[str, Any]
+    http_client: FirehydrantClient,
+    semaphore: asyncio.Semaphore,
+    service: dict[str, Any],
 ) -> dict[str, Any]:
-    firehydrant_client = init_client()
     async with semaphore:
-        return await firehydrant_client.compute_service_mean_time_metrics(
+        return await http_client.compute_service_mean_time_metrics(
             service.get("active_incidents", [])
         )
 
@@ -63,7 +63,10 @@ async def on_service_resync(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
     ):
         logger.debug(f"Received batch with {len(services)} services")
         semaphore = asyncio.Semaphore(5)
-        tasks = [process_service_analytics(semaphore, service) for service in services]
+        tasks = [
+            process_service_analytics(firehydrant_client, semaphore, service)
+            for service in services
+        ]
         service_analytics = await asyncio.gather(*tasks)
 
         yield [
@@ -94,7 +97,10 @@ async def on_retrospective_resync(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
             f"Received batch with {len(reports)} reports, getting their tasks completion status"
         )
         semaphore = asyncio.Semaphore(5)
-        tasks = [process_incident_tasks(semaphore, report) for report in reports]
+        tasks = [
+            process_incident_tasks(firehydrant_client, semaphore, report)
+            for report in reports
+        ]
         tasks_completion = await asyncio.gather(*tasks)
 
         yield [
@@ -107,12 +113,34 @@ async def on_retrospective_resync(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
 async def handle_firehydrant_webhook(webhook_data: dict[str, Any]) -> None:
     firehydrant_client = init_client()
     ## check for incident in the webhook data. Have a look at the sample webhook event payload https://firehydrant.com/docs/integration-guides/creating-webhooks/
-    if "incident" in webhook_data.get("data", {}):
-        incident_id = webhook_data.get("data", {}).get("incident", {}).get("id", "")
+    data = webhook_data["data"]
+    if "incident" in data:
+        incident_id = data["incident"]["id"]
         incident_data = await firehydrant_client.get_single_incident(
             incident_id=incident_id
         )
         await ocean.register_raw(ObjectKind.INCIDENT, [incident_data])
+
+        ## attempt to register retrospective data only if the incident is in postmoterm_completed status
+        if incident_data["current_milestone"] == "postmortem_completed":
+            retrospective_data = await firehydrant_client.get_single_retrospective(
+                report_id=incident_data["report_id"]
+            )
+            await ocean.register_raw(ObjectKind.RETROSPECTIVE, [retrospective_data])
+
+    if "environments" in data:
+        environment_id = data["environments"]["id"]
+        environment_data = await firehydrant_client.get_single_environment(
+            environment_id=environment_id
+        )
+        await ocean.register_raw(ObjectKind.ENVIRONMENT, [environment_data])
+
+    if "services" in data:
+        service_id = data["services"]["id"]
+        service_data = await firehydrant_client.get_single_service(
+            service_id=service_id
+        )
+        await ocean.register_raw(ObjectKind.SERVICE, [service_data])
 
     logger.info("Webhook event processed")
 
