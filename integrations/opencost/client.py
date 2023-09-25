e8c200780b8fb6ad53eb62031708b26adf47b39c
PeyGis <isaac.p.coffie@gmail.com>
2023-09-25 19:00:36 +0000

    lint code

--- integrations/opencost/client.py
+++ integrations/opencost/client.py
@@ -1,7 +1,6 @@
 from typing import Any, Optional
 import httpx
 from loguru import logger
-from port_ocean.context.event import event
 
 
 class OpenCostClient:
@@ -12,9 +11,7 @@ class OpenCostClient:
 
     @property
     def api_auth_header(self) -> dict[str, Any]:
-        return {
-            "Content-Type": "application/json"
-        }
+        return {"Content-Type": "application/json"}
 
     async def send_api_request(
         self,
@@ -38,16 +35,18 @@ class OpenCostClient:
                 f"HTTP error with status code: {e.response.status_code} and response text: {e.response.text}"
             )
             raise
-    
+
     async def get_cost_allocation(self) -> list[dict[str, Any]]:
-        """Calls the OpenCost allocation endpoint to return data for cost and usage"""
+        """Calls the OpenCost allocation endpoint to return data for cost and usage
+        https://www.opencost.io/docs/integrations/api
+        """
         endpoint = "allocation/compute"
-        parameters: dict[str, Any] = {
-            "window": self.window
-        }
-        cost_data = (await self.send_api_request(endpoint=endpoint, query_params=parameters))["data"][0]
+        parameters: dict[str, Any] = {"window": self.window}
+        cost_data = (
+            await self.send_api_request(endpoint=endpoint, query_params=parameters)
+        )["data"][0]
         return self.format_data(data=cost_data)
 
-    async def format_data(self, data: dict[str, Any]) -> list[dict[str, Any]]:
+    def format_data(self, data: dict[str, Any]) -> list[dict[str, Any]]:
         formatted_data = [value for key, value in data.items()]
         return formatted_data
--- integrations/opencost/main.py
+++ integrations/opencost/main.py
@@ -3,11 +3,17 @@ from enum import StrEnum
 from port_ocean.context.ocean import ocean
 from client import OpenCostClient
 
+
 class ObjectKind(StrEnum):
     COST = "cost"
 
+
 def init_client() -> OpenCostClient:
-    return OpenCostClient(ocean.integration_config["app_host"], ocean.integration_config.get("window", "today"))
+    return OpenCostClient(
+        ocean.integration_config["app_host"],
+        ocean.integration_config.get("window", "today"),
+    )
+
 
 @ocean.on_resync(ObjectKind.COST)
 async def on_cost_resync(kind: str) -> list[dict[Any, Any]]:
