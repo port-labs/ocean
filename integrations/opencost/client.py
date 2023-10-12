9747e852d098144b266b24491c2ef0e80a9e9c33
Yair Siman Tov <63305203+yairsimantov20@users.noreply.github.com>
2023-10-12 12:30:25 +0300

    [Integrations][Opencost][Improvement] Opencost modifications (#155)
    * opencost modifications
    
    * fixed the fields
    
    * fixed the fields
    
    * bump
    
    * lint

--- integrations/opencost/client.py
+++ integrations/opencost/client.py
@@ -1,47 +1,42 @@
-from typing import Any, Optional
+import typing
+from typing import Any
+
 import httpx
 from loguru import logger
 
+from integration import OpencostResourceConfig
+from port_ocean.context.event import event
+
 
 class OpenCostClient:
-    def __init__(self, app_host: str, window: str):
+    def __init__(self, app_host: str):
         self.app_host = app_host
-        self.window = window
-        self.http_client = httpx.AsyncClient(headers=self.api_auth_header)
-
-    @property
-    def api_auth_header(self) -> dict[str, Any]:
-        return {"Content-Type": "application/json"}
-
-    async def send_api_request(
-        self,
-        endpoint: str,
-        method: str = "GET",
-        query_params: Optional[dict[str, Any]] = None,
-        json_data: Optional[dict[str, Any]] = None,
-    ) -> dict[str, Any]:
-        try:
-            response = await self.http_client.request(
-                method=method,
-                url=f"{self.app_host}/{endpoint}",
-                params=query_params,
-                json=json_data,
-                headers=self.api_auth_header,
-            )
-            response.raise_for_status()
-            return response.json()
-        except httpx.HTTPStatusError as e:
-            logger.error(
-                f"HTTP error with status code: {e.response.status_code} and response text: {e.response.text}"
-            )
-            raise
+        self.http_client = httpx.AsyncClient()
 
     async def get_cost_allocation(self) -> list[dict[str, Any]]:
         """Calls the OpenCost allocation endpoint to return data for cost and usage
         https://www.opencost.io/docs/integrations/api
         """
-        endpoint = "allocation/compute"
-        parameters: dict[str, Any] = {"window": self.window}
-        return (
-            await self.send_api_request(endpoint=endpoint, query_params=parameters)
-        )["data"]
+        selector = typing.cast(OpencostResourceConfig, event.resource_config).selector
+        params: dict[str, str] = {
+            "window": selector.window,
+        }
+        if selector.aggregate:
+            params["aggregate"] = selector.aggregate
+        if selector.step:
+            params["step"] = selector.step
+        if selector.resolution:
+            params["resolution"] = selector.resolution
+
+        try:
+            response = await self.http_client.get(
+                url=f"{self.app_host}/allocation/compute",
+                params=params,
+            )
+            response.raise_for_status()
+            return response.json()["data"]
+        except httpx.HTTPStatusError as e:
+            logger.error(
+                f"HTTP error with status code: {e.response.status_code} and response text: {e.response.text}"
+            )
+            raise
--- integrations/opencost/main.py
+++ integrations/opencost/main.py
@@ -1,28 +1,11 @@
 from typing import Any
-from enum import StrEnum
-from port_ocean.context.ocean import ocean
+
 from client import OpenCostClient
+from port_ocean.context.ocean import ocean
 
 
-class ObjectKind(StrEnum):
-    COST = "cost"
-
-
-def init_client() -> OpenCostClient:
-    return OpenCostClient(
-        ocean.integration_config["app_host"],
-        ocean.integration_config.get("window", "today"),
-    )
-
-
-def process_cost_item(item: dict[str, Any]) -> list[dict[str, Any]]:
-    return [value for value in item.values()]
-
-
-@ocean.on_resync(ObjectKind.COST)
+@ocean.on_resync("cost")
 async def on_cost_resync(kind: str) -> list[dict[Any, Any]]:
-    client = init_client()
+    client = OpenCostClient(ocean.integration_config["opencost_host"])
     data = await client.get_cost_allocation()
-    processed_data = [process_cost_item(item) for item in data]
-    result = [item for sublist in processed_data for item in sublist]  ## flatten list
-    return result
+    return [value for item in data for value in item.values()]
