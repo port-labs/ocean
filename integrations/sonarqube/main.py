f55a695f7c4a834de089dd4e8961d1e6d78dcb99
yair <yairsimantov20@gmail.com>
2023-10-22 13:23:56 +0300

    removed redundant code

--- integrations/sonarqube/main.py
+++ integrations/sonarqube/main.py
@@ -17,8 +17,8 @@ def init_sonar_client() -> SonarQubeClient:
     return SonarQubeClient(
         ocean.integration_config.get("sonar_url", "https://sonarcloud.io"),
         ocean.integration_config["sonar_api_token"],
-        ocean.integration_config.get("sonar_organization_id", None),
-        ocean.integration_config.get("app_host", ""),
+        ocean.integration_config.get("sonar_organization_id"),
+        ocean.integration_config.get("app_host"),
     )
 
 
