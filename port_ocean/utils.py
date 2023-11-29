a1c26f4658bb049ef8e86b802e8148bdf1fe05b5
yair <yairsimantov20@gmail.com>
2023-11-29 12:37:49 +0200

    http_async_client comment

--- port_ocean/utils.py
+++ port_ocean/utils.py
@@ -36,6 +36,15 @@ def _get_http_client_context() -> httpx.AsyncClient:
     return client
 
 
+"""
+Utilize this client for all outbound integration requests to the third-party application. It functions as a wrapper 
+around the httpx.AsyncClient, incorporating retry logic at the transport layer for handling retries on 5xx errors and
+connection errors.
+
+The client is instantiated lazily, only coming into existence upon its initial access. It should not be closed when in
+use, as it operates as a singleton shared across all events in the thread. It also takes care of recreating the client
+in scenarios such as the creation of a new event loop, such as when initiating a new thread.
+"""
 http_async_client: httpx.AsyncClient = LocalProxy(lambda: _get_http_client_context())  # type: ignore
 
 
