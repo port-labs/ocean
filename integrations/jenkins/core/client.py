a03c9adbbad9783f3b555162b127d29f17753ddc
hpal <ahosea15@gmail.com>
2023-11-30 06:54:50 +0000

    Add error handling to client

--- integrations/jenkins/core/client.py
+++ integrations/jenkins/core/client.py
@@ -22,69 +22,87 @@ class JenkinsClient:
         page = 0
         logger.info("Getting jobs from Jenkins")
 
-        while True:
-            # Parameters for pagination
-            params = {
-                "tree": f"jobs[name,url,description,displayName,fullDisplayName,fullName]"
-            }
-            encoded_params = urlencode(params)
-
-            job_response = await self.client.get(
-                f"{self.jenkins_base_url}/api/json?{encoded_params}"
-            )
-            job_response.raise_for_status()
-            jobs = job_response.json()["jobs"]
-
-            # If there are no more jobs, exit the loop
-            if not jobs:
-                break
-
-            logger.info(f"Got {len(jobs)} jobs from Jenkins")
-
-            transformed = [
-                {
-                    "type": "item.updated",
-                    "data": job,
-                    "url": urlparse(job.get("url")).path.lstrip(
-                        "/"
-                    ),  # since blueprint expects path rather host
-                    "fullUrl": job.get("url"),
-                    "time": job.get("timestamp"),
+        try:
+            while True:
+                # Parameters for pagination
+                params = {
+                    "tree": f"jobs[name,url,description,displayName,fullDisplayName,fullName]"
                 }
-                for job in jobs
-            ]
+                encoded_params = urlencode(params)
 
-            yield transformed
-            page += 1
+                job_response = await self.client.get(
+                    f"{self.jenkins_base_url}/api/json?{encoded_params}"
+                )
+                job_response.raise_for_status()
+                jobs = job_response.json()["jobs"]
 
-            if len(jobs) < per_page:
-                break
+                # If there are no more jobs, exit the loop
+                if not jobs:
+                    break
+
+                logger.info(f"Got {len(jobs)} jobs from Jenkins")
+
+                transformed = [
+                    {
+                        "type": "item.updated",
+                        "data": job,
+                        "url": urlparse(job.get("url")).path.lstrip(
+                            "/"
+                        ),  # since blueprint expects path rather host
+                        "fullUrl": job.get("url"),
+                        "time": job.get("timestamp"),
+                    }
+                    for job in jobs
+                ]
+
+                yield transformed
+                page += 1
+
+                if len(jobs) < per_page:
+                    break
+        except httpx.HTTPStatusError as e:
+            logger.error(
+                f"HTTP error with status code: {e.response.status_code} and response text: {e.response.text}"
+            )
+            raise
+        except httpx.HTTPError as e:
+            logger.error(f"HTTP occurred while fetching Jenkins data {e}")
+            raise
 
     async def get_builds(self, job_name: str) -> list[dict[str, Any]]:
         logger.info(f"Getting builds from Jenkins for job {job_name}")
-
-        params = {
-            "tree": f"builds[id,number,url,result,duration,timestamp,displayName,fullDisplayName]"
-        }
-        encoded_params = urlencode(params)
-
-        build_response = await self.client.get(
-            f"{self.jenkins_base_url}/job/{job_name}/api/json?{encoded_params}"
-        )
-        build_response.raise_for_status()
-        builds = build_response.json().get("builds", [])
-        logger.info(f"Got {len(builds)} builds from Jenkins for job {job_name}")
-
-        transformed_builds = [
-            {
-                "type": "run.finalize",
-                "source": url_encode_str(f"job/{job_name}/"),
-                "url": build.get("url", None),
-                "data": {
-                    **build,
-                    "timestamp": convert_timestamp_to_utc_dt(build.get("timestamp")),
-                },
+        try:
+            params = {
+                "tree": f"builds[id,number,url,result,duration,timestamp,displayName,fullDisplayName]"
             }
-            for build in builds
-        ]
-        return transformed_builds
+            encoded_params = urlencode(params)
+
+            build_response = await self.client.get(
+                f"{self.jenkins_base_url}/job/{job_name}/api/json?{encoded_params}"
+            )
+            build_response.raise_for_status()
+            builds = build_response.json().get("builds", [])
+            logger.info(f"Got {len(builds)} builds from Jenkins for job {job_name}")
+
+            transformed_builds = [
+                {
+                    "type": "run.finalize",
+                    "source": url_encode_str(f"job/{job_name}/"),
+                    "url": build.get("url", None),
+                    "data": {
+                        **build,
+                        "timestamp": convert_timestamp_to_utc_dt(build.get("timestamp")),
+                    },
+                }
+                for build in builds
+            ]
+            return transformed_builds
+        except httpx.HTTPStatusError as e:
+            logger.error(
+                f"HTTP error with status code: {e.response.status_code} and response text: {e.response.text}"
+            )
+            raise
+        except httpx.HTTPError as e:
+            logger.error(f"HTTP occurred while fetching Jenkins data {e}")
+            raise
+        
