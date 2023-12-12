e9dd884fe05e16f6c4a447221d8e3f0aa14a5b89
tankilevitch <tomtankilevitch@gmail.com>
2023-12-12 10:32:43 +0200

    create SystemEventHandler

--- integrations/gitlab/gitlab_integration/events/event_handler.py
+++ integrations/gitlab/gitlab_integration/events/event_handler.py
@@ -1,9 +1,11 @@
 import asyncio
 from collections import defaultdict
-from typing import Awaitable, Callable, Any
+from typing import Awaitable, Callable, Any, Type
 
+from gitlab_integration.events.hooks.base import HookHandler
+from gitlab_integration.gitlab_service import GitlabService
 
-Observer = Callable[[str, str, dict[str, Any]], Awaitable[Any]]
+Observer = Callable[[str, dict[str, Any]], Awaitable[Any]]
 
 
 class EventHandler:
@@ -14,9 +16,32 @@ class EventHandler:
         for event in events:
             self._observers[event].append(observer)
 
-    async def notify(
-        self, event: str, group_id: str, body: dict[str, Any]
-    ) -> Awaitable[Any]:
+    async def notify(self, event: str, body: dict[str, Any]) -> Awaitable[Any]:
         return asyncio.gather(
-            *(observer(event, group_id, body) for observer in self._observers[event])
+            *(observer(event, body) for observer in self._observers[event])
+        )
+
+
+class SystemEventHandler:
+    def __init__(self) -> None:
+        self._hook_handlers: dict[str, list[Type[HookHandler]]] = defaultdict(list)
+        self._clients: list[GitlabService] = []
+
+    def on(self, hook_handler: Type[HookHandler]) -> None:
+        for system_event in hook_handler.system_events:
+            self._hook_handlers[system_event].append(hook_handler)
+
+    def add_client(self, client: GitlabService) -> None:
+        self._clients.append(client)
+
+    async def notify(self, event: str, body: dict[str, Any]) -> Awaitable[Any]:
+        # best effort to notify using all clients, as we don't know which one of the clients have the permission to
+        # access the project
+        return asyncio.gather(
+            *(
+                hook_handler(client).on_hook(event, body)
+                for client in self._clients
+                for hook_handler in self._hook_handlers[event]
+            ),
+            return_exceptions=True,
         )
