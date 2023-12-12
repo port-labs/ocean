4cca2e9f14407e20a41f02f52e91d137b66b6b1f
hpal <ahosea15@gmail.com>
2023-12-12 15:16:31 +0000

    get jobs and builds at once in resync
    Use event as trigger to call api for a jenkins resource. This allows for a single schema to target in JQ processing

--- integrations/jenkins/client.py
+++ integrations/jenkins/client.py
@@ -10,16 +10,17 @@ class JenkinsClient:
         self, jenkins_base_url: str, jenkins_user: str, jenkins_token: str
     ) -> None:
         self.jenkins_base_url = jenkins_base_url
-
         auth = (jenkins_user, jenkins_token)
 
         self.client = http_async_client
         self.client.auth = auth
 
-    async def get_jobs(self) -> AsyncGenerator[list[dict[str, Any]], None]:
+    async def get_all_jobs_and_builds(
+        self,
+    ) -> AsyncGenerator[list[dict[str, Any]], None]:
         page_size = 100
         page = 0
-        logger.info("Getting jobs from Jenkins")
+        logger.info("Getting jobs and builds from Jenkins")
 
         try:
             while True:
@@ -27,13 +28,12 @@ class JenkinsClient:
                 end_idx = start_idx + page_size
 
                 params = {
-                    "tree": f"jobs[name,url,description,displayName,fullDisplayName,fullName]{{{start_idx},{end_idx}}}"
+                    "tree": f"jobs[name,url,description,displayName,fullDisplayName,fullName,builds[id,number,url,"
+                    f"result,duration,timestamp,displayName,fullDisplayName]]{{{start_idx},{end_idx}}}"
                 }
-                encoded_params = urlencode(params)
-                logger.info(params)
 
                 job_response = await self.client.get(
-                    f"{self.jenkins_base_url}/api/json?{encoded_params}"
+                    f"{self.jenkins_base_url}/api/json?{urlencode(params)}"
                 )
                 job_response.raise_for_status()
                 jobs = job_response.json()["jobs"]
@@ -52,40 +52,15 @@ class JenkinsClient:
             logger.error(f"Unexpected error occurred: {e}")
             raise
 
-    async def get_builds(
-        self, job_name: str
-    ) -> AsyncGenerator[list[dict[str, Any]], None]:
-        page_size = 100
-        page = 0
+    async def get_single_resource(self, resource_url: str) -> dict[str, Any]:
+        """
+        Get either a job or build using the url from the event e.g.
 
-        logger.info(f"Getting builds from Jenkins for job {job_name}")
-        try:
-            while True:
-                start_idx = page_size * page
-                end_idx = start_idx + page_size
-
-                params = {
-                    "tree": f"builds[id,number,url,result,duration,timestamp,displayName,fullDisplayName]{{{start_idx},{end_idx}}}"
-                }
-                encoded_params = urlencode(params)
-                request_url = (
-                    f"{self.jenkins_base_url}/job/{job_name}/api/json?{encoded_params}"
-                )
-
-                build_response = await self.client.get(request_url)
-                build_response.raise_for_status()
-                builds = build_response.json().get("builds", [])
-
-                if not builds:
-                    break
-
-                logger.info(f"Got {len(builds)} builds from Jenkins for job {job_name}")
-
-                yield builds
-                page += 1
-
-                if len(builds) < page_size:
-                    break
-        except Exception as e:
-            logger.error(f"Unexpected error occurred: {e}")
-            raise
+        Job: job/JobName/
+        Build: job/JobName/34
+        """
+        response = await self.client.get(
+            f"{self.jenkins_base_url}/{resource_url}api/json"
+        )
+        response.raise_for_status()
+        return response.json()
--- integrations/jenkins/main.py
+++ integrations/jenkins/main.py
@@ -29,31 +29,27 @@ def init_client() -> JenkinsClient:
     )
 
 
-@ocean.on_resync(ObjectKind.JOB)
-async def on_resync_jobs(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
+@ocean.on_resync()
+async def on_resync(kind: str) -> None:
     jenkins_client = init_client()
 
-    async for jobs in jenkins_client.get_jobs():
-        logger.info(f"Received ${len(jobs)} jobs")
-        yield jobs
-
-
-@ocean.on_resync(ObjectKind.BUILD)
-async def on_resync_builds(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
-    jenkins_client = init_client()
-
-    async for jobs in jenkins_client.get_jobs():
-        logger.info(f"Received ${len(jobs)} jobs")
-        for job in jobs:
-            async for builds in jenkins_client.get_builds(job["name"]):
-                yield builds
+    async for _jobs in jenkins_client.get_all_jobs_and_builds():
+        if kind == ObjectKind.JOB:
+            yield _jobs
+        elif kind == ObjectKind.BUILD:
+            yield [build for job in _jobs for build in job["builds"]]
 
 
 @ocean.router.post("/events")
 async def handle_events(event: dict[str, Any]) -> dict[str, bool]:
+    jenkins_client = init_client()
     logger.info(f'Received {event["dataType"]} event {event["id"]} | {event["type"]}')
 
-    await ocean.register_raw(
-        ObjectKind.get_object_kind_for_event(event["type"]), [event]
-    )
+    kind = ObjectKind.get_object_kind_for_event(event["type"])
+
+    if kind:
+        resource = await jenkins_client.get_single_resource(event["url"])
+        await ocean.register_raw(kind, [resource])
+
+    logger.info("Webhook event processed")
     return {"ok": True}
