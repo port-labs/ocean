e6d60bed39d4a1ef10b3c6a8a264e5afebc3bf3e
mkarmah <mikeyarmah@gmail.com>
2023-12-22 18:49:41 +0000

    added generic function for pagination

--- integrations/terraform-cloud/client.py
+++ integrations/terraform-cloud/client.py
@@ -12,9 +12,7 @@ TERRAFORM_WEBHOOK_EVENTS = [
     "run:planning",
 ]
 
-
 PAGE_SIZE = 100
-
 
 class TerraformClient:
     def __init__(self, terraform_base_url: str, auth_token: str) -> None:
@@ -35,183 +33,122 @@ class TerraformClient:
     ) -> dict[str, Any]:
         logger.info(f"Requesting Terraform Cloud data for endpoint: {endpoint}")
         try:
+            url = f"{self.api_url}/{endpoint}"
+            logger.debug(f"URL: {url}, Method: {method}, Params: {query_params}, Body: {json_data}")
             response = await self.client.request(
                 method=method,
-                url=f"{self.api_url}/{endpoint}",
+                url=url,
                 params=query_params,
                 json=json_data,
             )
             response.raise_for_status()
 
-            logger.info(
-                f"Successfully retrieved Terraform Cloud data for endpoint: {endpoint}"
-            )
+            logger.info(f"Successfully retrieved data for endpoint: {endpoint}")
+            logger.debug(f"Response: {response.json()}")
 
             return response.json()
 
         except httpx.HTTPStatusError as e:
-            logger.error(
-                f"HTTP error with status code: {e.response.status_code} and response text: {e.response.text}"
-            )
+            logger.error(f"HTTP error on {endpoint}: {e.response.status_code} - {e.response.text}")
+            raise
+        except httpx.HTTPError as e:
+            logger.error(f"HTTP error on {endpoint}: {str(e)}")
             raise
 
-        except httpx.HTTPError:
-            logger.exception("An error occured while fetching terraform cloud data")
-            raise
+    async def get_paginated_resources(
+        self, endpoint: str, params: Optional[dict[str, Any]] = None
+    ) -> AsyncGenerator[list[dict[str, Any]], None]:
+        params = params or {}
+        params.update({"page[size]": PAGE_SIZE})
+        next_url = endpoint
 
-    async def get_organizations(self) -> list[dict[str, Any]]:
-        response = await self.send_api_request(endpoint="organizations")
-        return response.get("data", [])
+        while next_url:
+            response = await self.send_api_request(endpoint=next_url, query_params=params)
+            resources = response.get("data", [])
+
+            logger.info(f"Fetched {len(resources)} resources from {next_url}")
+            yield resources
+
+            next_url = response.get("links", {}).get("next", "")
+            if not next_url:
+                logger.debug(f"No more pages to fetch for {endpoint}")
+                break
+
+    async def get_paginated_organizations(self) -> list[dict[str, Any]]:
+        logger.info("Fetching organizations")
+        organizations = await self.get_paginated_resources("organizations")
+        return organizations
 
     async def get_single_workspace(self, workspace_id: str) -> dict[str, Any]:
-        response = await self.send_api_request(endpoint=f"workspaces/{workspace_id}")
-        return response.get("data", {})
+        logger.info(f"Fetching workspace with ID: {workspace_id}")
+        workspace = await self.send_api_request(endpoint=f"workspaces/{workspace_id}")
+        return workspace.get("data")
 
     async def get_single_run(self, run_id: str) -> dict[str, Any]:
-        response = await self.send_api_request(endpoint=f"runs/{run_id}")
-        return response.get("data", {})
+        logger.info(f"Fetching run with ID: {run_id}")
+        run = await self.send_api_request(endpoint=f"runs/{run_id}")
+        return run.get("data",{})
 
     async def get_state_version_output(self, state_version_id: str) -> dict[str, Any]:
-        response = await self.send_api_request(
-            endpoint=f"state-versions/{state_version_id}/outputs"
-        )
-        return response.get("data", {})
+        logger.info(f"Fetching state version output for ID: {state_version_id}")
+        outputs = await self.send_api_request(endpoint=f"state-versions/{state_version_id}/outputs")
+        return outputs.get("data",{})
 
     async def get_paginated_workspaces(
         self,
     ) -> AsyncGenerator[list[dict[str, Any]], None]:
-        
-        logger.info("Starting to fetch workspaces...")
         cache_key = "terraform-cloud-workspace"
         if cache := event.attributes.get(cache_key):
             logger.info("Retrieving workspaces data from cache")
             yield cache
             return
 
-        page = 1
         all_workspaces = []
-        organizations = await self.get_organizations()
-        for organization in organizations:
-            logger.info(f"Getting workspaces for {organization['id']}")
+        async for organizations in await self.get_paginated_organizations():
+            for organization in organizations:
+                organization_id = organization['id']
+                endpoint = f"organizations/{organization_id}/workspaces"
+                async for workspaces in self.get_paginated_resources(endpoint):
+                    all_workspaces.extend(workspaces)
+                    yield workspaces
 
-            while True:
-                params: dict[str, Any] = {"page[number]": page, "page[size]": PAGE_SIZE}
-                response = await self.send_api_request(
-                    endpoint=f"organizations/{organization['id']}/workspaces",
-                    query_params=params,
-                )
-                workspaces = response.get("data", [])
-                all_workspaces.extend(workspaces)
-                logger.info(f"Fetched {len(workspaces)} workspaces for organization {organization['id']} on page {page}")
+        event.attributes[cache_key] = workspaces
 
-                yield workspaces
-
-                if len(workspaces) < PAGE_SIZE:
-                    logger.debug("No more workspaces to fetch for this organization")
-                    break
-
-                page += 1
-        event.attributes[cache_key] = all_workspaces
-        logger.info("Successfully retrieved workspaces for all organizations")
 
     async def get_paginated_runs_for_workspace(
         self, workspace_id: str
     ) -> AsyncGenerator[list[dict[str, Any]], None]:
-        logger.info(f"Getting runs for workspace {workspace_id} from Terraform")
+        logger.info(f"Fetching runs for workspace ID: {workspace_id}")
 
-        page = 1
-        while True:
-            params: dict[str, Any] = {"page[number]": page, "page[size]": PAGE_SIZE}
+        endpoint = f"workspaces/{workspace_id}/runs"
 
-            logger.debug(f"Fetching page {page} of runs for workspace: {workspace_id}")
-            response = await self.send_api_request(
-                endpoint=f"workspaces/{workspace_id}/runs", query_params=params
-            )
-            runs = response.get("data", [])
-            logger.info(f"Recieved {len(runs)} runs for workspace: {workspace_id} on page {page}")
-            yield runs
+        return await self.get_paginated_resources(endpoint)
 
-            if len(runs) < PAGE_SIZE:
-                logger.debug(f"No more runs to fetch for workspace: {workspace_id} after page {page}")
-                break
-            page += 1
-
-        logger.info(f"Completed fetching all runs for workspace: {workspace_id}")
-
-    async def get_paginated_state_version(
-        self,
-    ) -> AsyncGenerator[list[dict[str, Any]], None]:
-        logger.info("Getting state versions for all workspaces")
-
-        async for workspaces in self.get_paginated_workspaces():
-            for workspace in workspaces:
-                page = 1
-
-                while True:
-                    params: dict[str, Any] = {
-                        "page[number]": page,
-                        "page[size]": PAGE_SIZE,
-                        "filter[workspace][name]": workspace["attributes"]["name"],
-                        "filter[organization][name]": workspace["relationships"][
-                            "organization"
-                        ]["data"]["id"],
-                    }
-                    response = await self.send_api_request(
-                        endpoint="state-versions", query_params=params
-                    )
-                    state_versions = response.get("data", [])
-                    logger.info(f"Received {len(state_versions)} state versions for workspace {workspace['id']} on page {page}")
-                    yield state_versions
-
-                    if len(state_versions) < PAGE_SIZE:
-                        logger.debug("No more state versions to fetch for workspace")
-                        break
-
-                    page += 1
-        logger.info("Successfully retrieved state versions for all workspaces")
 
     async def create_workspace_webhook(self, app_host: str) -> None:
         webhook_target_url = f"{app_host}/integration/webhook"
-
         async for workspaces in self.get_paginated_workspaces():
             for workspace in workspaces:
-                notification_config_url = f"{self.api_url}/workspaces/{workspace['id']}/notification-configurations"
-                logger.debug(f"Fetching existing webhooks for workspace {workspace['id']}")
-                
-                notifications_response = await self.send_api_request(
-                    endpoint=notification_config_url
-                )
+                workspace_id = workspace['id']
+                endpoint = f"workspaces/{workspace_id}/notification-configurations"
+                notifications_response = await self.send_api_request(endpoint=endpoint)
                 existing_configs = notifications_response.get("data", [])
 
-                for config in existing_configs:
-                    if config["attributes"]["url"] == webhook_target_url:
-                        logger.info(
-                            f"Skipping webhook creation: Webhook already exists for workspace {workspace['id']}"
-                        )
-                    
-                    else:
-                        webhook_body = {
-                            "data": {
-                                "type": "notification-configurations",
-                                "attributes": {
-                                    "destination-type": "generic",
-                                    "enabled": True,
-                                    "name": "port integration webhook",
-                                    "url": webhook_target_url,
-                                    "triggers": TERRAFORM_WEBHOOK_EVENTS,
-                                },
-                            }
+                webhook_exists = any(config["attributes"]["url"] == webhook_target_url for config in existing_configs)
+                if webhook_exists:
+                    logger.info(f"Webhook already exists for workspace {workspace_id}")
+                else:
+                    webhook_body = {
+                        "data": {
+                            "type": "notification-configurations",
+                            "attributes": {
+                                "destination-type": "generic",
+                                "enabled": True,
+                                "name": "port integration webhook",
+                                "url": webhook_target_url,
+                                "triggers": TERRAFORM_WEBHOOK_EVENTS,
+                            },
                         }
-
-                        logger.debug(f"Creating new webhook for workspace {workspace['id']}")
-
-                        await self.send_api_request(
-                            endpoint=notification_config_url,
-                            method="POST",
-                            json_data=webhook_body,
-                        )
-                        logger.info(
-                            f"Webhook created for Terraform workspace {workspace['id']}"
-                        )
-
-        logger.info("Completed webhook creation process for all workspaces")
+                    }
+                    await self.send_api_request(endpoint=endpoint, method="POST", json_data=webhook_body)
+                    logger.info(f"Webhook created for Terraform workspace {workspace_id}")
