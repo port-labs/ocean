79d8f7690cdd2077b4cc387452cf1da9ab741bdf
Matan Heled <matanheled@getport.io>
2024-01-17 17:58:45 +0200

    Added workflow for auto-creating sonarcloud projects on merge + added 'sonar-project.properties' to cookiecutter

--- port_ocean/cli/cookiecutter/hooks/post_gen_project.py
+++ port_ocean/cli/cookiecutter/hooks/post_gen_project.py
@@ -2,10 +2,14 @@ import os
 
 
 def delete_docker_files():
-    if "{{ cookiecutter.remove_docker_files }}" == "True":
+    if "{{ cookiecutter.remove_docker_files }}" == "true":
         os.remove("Dockerfile")
         os.remove(".dockerignore")
 
+def delete_sonarcloud_files():
+    if "{{ cookiecutter.custom_integration }}" == "true":
+        os.remove("sonar-project.properties")
 
 if __name__ == "__main__":
     delete_docker_files()
+    delete_sonarcloud_files()
