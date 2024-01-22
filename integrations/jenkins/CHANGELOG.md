b8321ad954bbddbf9c2d48620562b19cca86c800
hpal <ahosea15@gmail.com>
2024-01-22 14:26:04 +0000

    PORT-6268 Add support for ingesting users

--- integrations/jenkins/CHANGELOG.md
+++ integrations/jenkins/CHANGELOG.md
@@ -7,6 +7,13 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 <!-- towncrier release notes start -->
 
+# 0.2.0 (2024-01-22)
+
+### Features
+
+- Added support for ingesting users (0.2.0)
+
+
 # 0.1.0 (2024-01-16)
 
 ### Features
--- integrations/jenkins/client.py
+++ integrations/jenkins/client.py
@@ -119,3 +119,28 @@ class JenkinsClient:
         )
         response.raise_for_status()
         return response.json()
+
+
+    async def get_users(self) -> AsyncGenerator[list[dict[str, Any]], None]:
+        page_size = 5
+        page = 0
+
+        while True:
+            start_idx = page_size * page
+            end_idx = start_idx + page_size
+
+            params = {"tree": f"users[user[*],*]{{{start_idx},{end_idx}}}"}
+
+            response = await self.client.get(f"{self.jenkins_base_url}/people/api/json", params=params)
+            response.raise_for_status()
+            users = response.json()["users"]
+
+            if not users:
+                break
+
+            yield users
+
+            page += 1
+
+            if len(users) < page_size:
+                break
