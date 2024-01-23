b3ac5d3a955e86834129162ed9594af05e6d10b6
hpal <ahosea15@gmail.com>
2024-01-23 13:20:39 +0000

    Port 5831 Wiz integration powered by ocean (#306)

---
+++ integrations/wiz/wiz/constants.py
@@ -0,0 +1,141 @@
+PAGE_SIZE = 50
+MAX_PAGES = 5
+AUTH0_URLS = ["https://auth.wiz.io/oauth/token", "https://auth0.gov.wiz.io/oauth/token"]
+COGNITO_URLS = [
+    "https://auth.app.wiz.io/oauth/token",
+    "https://auth.gov.wiz.io/oauth/token",
+]
+
+ISSUES_GQL = """
+query IssuesTable(
+  $filterBy: IssueFilters
+  $first: Int
+  $after: String
+  $orderBy: IssueOrder
+) {
+  issues: issuesV2(
+    filterBy: $filterBy
+    first: $first
+    after: $after
+    orderBy: $orderBy
+  ) {
+    nodes {
+      id
+      sourceRule {
+        __typename
+        ... on Control {
+          id
+          name
+          controlDescription: description
+          resolutionRecommendation
+          securitySubCategories {
+            title
+            category {
+              name
+              framework {
+                name
+              }
+            }
+          }
+        }
+        ... on CloudEventRule {
+          id
+          name
+          cloudEventRuleDescription: description
+          sourceType
+          type
+        }
+        ... on CloudConfigurationRule {
+          id
+          name
+          cloudConfigurationRuleDescription: description
+          remediationInstructions
+          serviceType
+        }
+      }
+      createdAt
+      updatedAt
+      dueAt
+      type
+      resolvedAt
+      statusChangedAt
+      projects {
+        id
+        name
+        slug
+        businessUnit
+        riskProfile {
+          businessImpact
+        }
+      }
+      status
+      severity
+      entitySnapshot {
+        id
+        type
+        nativeType
+        name
+        status
+        cloudPlatform
+        cloudProviderURL
+        providerId
+        region
+        resourceGroupExternalId
+        subscriptionExternalId
+        subscriptionName
+        subscriptionTags
+        tags
+        createdAt
+        externalId
+      }
+      serviceTickets {
+        externalId
+        name
+        url
+      }
+      notes {
+        createdAt
+        updatedAt
+        text
+        user {
+          name
+          email
+        }
+        serviceAccount {
+          name
+        }
+      }
+    }
+    pageInfo {
+      hasNextPage
+      endCursor
+    }
+  }
+}
+"""
+PROJECTS_GQL = """
+query ProjectsTable(
+  $filterBy: ProjectFilters,
+  $first: Int,
+  $after: String,
+  $orderBy: ProjectOrder
+) {
+  projects(
+    filterBy: $filterBy,
+    first: $first,
+    after: $after,
+    orderBy: $orderBy
+  ) {
+    nodes {
+      id
+      name
+      isFolder
+      archived
+      businessUnit
+      description
+    }
+  }
+}
+"""
+
+GRAPH_QUERIES = {"issues": ISSUES_GQL, "projects": PROJECTS_GQL}
