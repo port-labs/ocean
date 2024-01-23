58927bb020c41bc21ce215ed1f94c3c83675a6c5
Matan Heled <matanheled@getport.io>
2024-01-23 12:02:19 +0200

    fixed cleanup.py and lint

--- port_ocean/cli/cookiecutter/hooks/post_gen_project.py
+++ port_ocean/cli/cookiecutter/hooks/post_gen_project.py
@@ -1,17 +1,13 @@
 import os
 
 
-def delete_docker_files():
-    if "{{ cookiecutter.remove_docker_files }}" == "true":
+def handle_custom_integration_flags():
+    if "{{ cookiecutter.custom_integration }}" == "False":
         os.remove("Dockerfile")
         os.remove(".dockerignore")
-
-
-def delete_sonarcloud_files():
-    if "{{ cookiecutter.custom_integration }}" == "true":
+    if "{{ cookiecutter.custom_integration }}" == "True":
         os.remove("sonar-project.properties")
 
 
 if __name__ == "__main__":
-    delete_docker_files()
-    delete_sonarcloud_files()
+    handle_custom_integration_flags()
