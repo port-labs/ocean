8d1250df9adc99c2711f793163e5d176ca4e1847
lordsarcastic <>
2024-01-29 19:20:19 +0100

    Fixed bugs on Dynatrace integration, removed needless kind

--- integrations/dynatrace/CHANGELOG.md
+++ integrations/dynatrace/CHANGELOG.md
@@ -6,3 +6,9 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 <!-- towncrier release notes start -->
+
+# Port_Ocean 1.0.0 (2024-01-29)
+
+### Features
+
+- Add Dynatrace integration (1.0.0)
--- integrations/dynatrace/client.py
+++ integrations/dynatrace/client.py
@@ -18,65 +18,59 @@ class DynatraceClient:
     def __init__(self, host_url: str, api_key: str) -> None:
         self.host_url = host_url
         self.client = http_async_client
-        self.client.headers = httpx.Headers({"Authorization": f"Api-Token {api_key}"})
+        self.client.headers.update({"Authorization": f"Api-Token {api_key}"})
+
+    async def _get_paginated_resources(
+        self, url: str, resource_key: str, params: dict[str, Any] = {}
+    ) -> AsyncGenerator[list[dict[str, Any]], None]:
+        logger.info(f"Fetching {resource_key} from {url} with params {params}")
+        try:
+            while True:
+                response = await self.client.get(url, params=params)
+                response.raise_for_status()
+                response_data = response.json()
+                resources = response_data[resource_key]
+                yield resources
+                next_page_key = response_data.get("nextPageKey")
+                if not next_page_key:
+                    break
+                params = {"nextPageKey": next_page_key}
+        except httpx.HTTPStatusError as e:
+            logger.error(
+                f"HTTP error on {url}: {e.response.status_code} - {e.response.text}"
+            )
+            raise
+        except httpx.HTTPError as e:
+            logger.error(f"HTTP error on {url}: {e}")
+            raise
 
     async def _get_problems(self) -> AsyncGenerator[list[dict[str, Any]], None]:
-        url = f"{self.host_url}/api/v2/problems"
-        logger.info(f"Fetching problems from {url}")
-        while True:
-            problems_response = await self.client.get(url)
-            problems_response.raise_for_status()
-            response_data = problems_response.json()
-            problems = response_data["problems"]
+        async for problems in self._get_paginated_resources(
+            f"{self.host_url}/api/v2/problems", "problems"
+        ):
             yield problems
-            next_page_key = response_data.get("nextPageKey")
-            if not next_page_key:
-                break
-            url = f"{self.host_url}/api/v2/problems?nextPageKey={next_page_key}"
 
     async def _get_slos(self) -> AsyncGenerator[list[dict[str, Any]], None]:
-        url = f"{self.host_url}/api/v2/slo"
-        logger.info(f"Fetching slos from {url}")
-        while True:
-            slos_response = await self.client.get(url)
-            slos_response.raise_for_status()
-            response_data = slos_response.json()
-            slos = response_data["slos"]
+        async for slos in self._get_paginated_resources(
+            f"{self.host_url}/api/v2/slo", "slo"
+        ):
             yield slos
-            next_page_key = response_data.get("nextPageKey")
-            if not next_page_key:
-                break
-            url = f"{self.host_url}/api/v2/slo?nextPageKey={next_page_key}"
 
     async def _get_entity_types(self) -> AsyncGenerator[list[dict[str, Any]], None]:
-        url = f"{self.host_url}/api/v2/entityTypes"
-        logger.info(f"Fetching entity types from {url}")
-        while True:
-            entity_types_response = await self.client.get(url)
-            entity_types_response.raise_for_status()
-            response_data = entity_types_response.json()
-            entity_types = response_data["types"]
+        async for entity_types in self._get_paginated_resources(
+            f"{self.host_url}/api/v2/entityTypes", "types"
+        ):
             yield entity_types
-            next_page_key = response_data.get("nextPageKey")
-            if not next_page_key:
-                break
-            url = f"{self.host_url}/api/v2/entityTypes?nextPageKey={next_page_key}"
 
     async def _get_entities_from_type(
         self, type: str
     ) -> AsyncGenerator[list[dict[str, Any]], None]:
-        url = f'{self.host_url}/api/v2/?entitySelector=type("{type}")'
-        logger.info(f"Fetching entities from {url}")
-        while True:
-            entities_response = await self.client.get(url)
-            entities_response.raise_for_status()
-            response_data = entities_response.json()
-            entities = response_data["entities"]
+        async for entities in self._get_paginated_resources(
+            f"{self.host_url}/api/v2/entities",
+            "entities",
+            params={"entitySelector": f'type("{type}")'},
+        ):
             yield entities
-            next_page_key = response_data.get("nextPageKey")
-            if not next_page_key:
-                break
-            url = f'{self.host_url}/api/v2/?entitySelector=type("{type}")&nextPageKey={next_page_key}'
 
     async def _get_entities(self) -> AsyncGenerator[list[dict[str, Any]], None]:
         async for entity_types in self._get_entity_types():
@@ -85,13 +79,12 @@ class DynatraceClient:
                     yield entities
 
     async def get_resource(
-        self, resource: ResourceKey, **kwargs: dict[str, Any]
+        self, resource: str, **kwargs: dict[str, Any]
     ) -> AsyncGenerator[list[dict[str, Any]], None]:
         RESOURCE_MAPPING = {
-            ResourceKey.PROBLEMS: self._get_problems,
-            ResourceKey.SLOS: self._get_slos,
-            ResourceKey.ENTITIES: self._get_entities,
-            ResourceKey.ENTITY_TYPES: self._get_entity_types,
+            ResourceKey.PROBLEM.value: self._get_problems,
+            ResourceKey.SLO.value: self._get_slos,
+            ResourceKey.ENTITY.value: self._get_entities,
         }
 
         if cache := event.attributes.get(resource):
