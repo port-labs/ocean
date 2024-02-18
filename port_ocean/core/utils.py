32fc37a628d8076e602f3d889c27818dcb933889
MatanGevaPort <matang@getport.io>
2024-02-18 09:30:18 +0200

    Added caching mechanism to async generators results

--- port_ocean/core/utils.py
+++ port_ocean/core/utils.py
@@ -1,6 +1,8 @@
-from typing import Iterable, Any, TypeVar
+import functools
+from typing import AsyncGenerator, Callable, Iterable, Any, TypeVar
 
 from pydantic import parse_obj_as, ValidationError
+from port_ocean.context.event import event
 
 from port_ocean.core.handlers.entity_processor.base import EntityPortDiff
 from port_ocean.core.models import Entity
@@ -63,3 +65,32 @@ def get_port_diff(
             ],
         ),
     )
+
+
+AsyncGeneratorCreatorType = Callable[..., AsyncGenerator[list[Any], None]]
+
+
+def cache_results(
+    cache_key: str,
+) -> Callable[[AsyncGeneratorCreatorType], AsyncGeneratorCreatorType]:
+    def decorator(method: AsyncGeneratorCreatorType) -> AsyncGeneratorCreatorType:
+        @functools.wraps(method)
+        async def wrapper(*args: Any, **kwargs: Any) -> Any:
+            # Check if the result is already in the cache
+            if cache := event.attributes.get(cache_key):
+                yield cache
+                return
+
+            # If not in cache, fetch the data
+            cached_results = list()
+            async for result in method(*args, **kwargs):
+                cached_results.extend(result)
+                yield result
+
+            # Cache the results
+            event.attributes[cache_key] = cached_results
+            return
+
+        return wrapper
+
+    return decorator
