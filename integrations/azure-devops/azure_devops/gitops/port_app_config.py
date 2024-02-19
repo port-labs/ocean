a4c3c613c7e6669f0b9c8669478233a87e5924b0
MatanGevaPort <matang@getport.io>
2024-02-19 15:43:54 +0200

    Forces file:// only to repo

--- integrations/azure-devops/azure_devops/gitops/port_app_config.py
+++ integrations/azure-devops/azure_devops/gitops/port_app_config.py
@@ -4,14 +4,15 @@ from port_ocean.core.handlers.port_app_config.models import (
 )
 from .file_entity_processor import GitSelector
 from pydantic import Field
-from typing import List
+from typing import List, Literal
 
 
 class GitResourceConfig(ResourceConfig):
+    kind: Literal['repository']
     selector: GitSelector
 
 
 class GitPortAppConfig(PortAppConfig):
     spec_path: List[str] = Field(alias="specPath", default=["port.yml"])
     branch: str = "main"
-    resources: list[GitResourceConfig] = Field(default_factory=list)  # type: ignore
+    resources: list[GitResourceConfig | ResourceConfig] = Field(default_factory=list)  # type: ignore
