c08b65106c0245280db8ec0180d29664eb2c383a
danielsinai <daniel.sinai.main@gmail.com>
2024-02-29 14:23:07 +0200

    fix(azure-devops): change the naming of the files

--- integrations/azure-devops/azure_devops/client/azure_devops_client.py
+++ integrations/azure-devops/azure_devops/client/azure_devops_client.py
@@ -4,7 +4,7 @@ from azure_devops.webhooks.webhook_event import WebhookEvent
 from port_ocean.context.event import event
 from port_ocean.context.ocean import ocean
 from loguru import logger
-from .http.http_client import HTTPBaseClient
+from client import HTTPBaseClient
 from azure_devops.utils import cache_results
 
 API_URL_PREFIX = "_apis"
