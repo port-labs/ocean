2e25705f27e639280a2d5f7f5c8976a7bf6b2108
Pages <isaac.p.coffie@gmail.com>
2024-03-07 16:50:56 +0000

    add project and issue tags

--- integrations/sentry/clients/sentry.py
+++ integrations/sentry/clients/sentry.py
@@ -1,8 +1,12 @@
 from typing import Any, AsyncGenerator
+import typing
 from loguru import logger
 
 from port_ocean.utils import http_async_client
 from port_ocean.utils.cache import cache_iterator_result
+from integration import SentryResourceConfig
+from port_ocean.context.event import event
+
 import httpx
 
 PAGE_SIZE = 100
@@ -19,8 +23,15 @@ class SentryClient:
         self.organization = sentry_organization
         self.client = http_async_client
         self.client.headers.update(self.base_headers)
+        self.selector = typing.cast(
+            SentryResourceConfig, event.resource_config
+        ).selector
 
     def get_next_link(self, link_header: str) -> str:
+        """Information about the next page of results is provided in the link header. The pagination cursors are returned for both the previous and the next page.
+        One of the URLs in the link response has rel=next, which indicates the next results page. It also has results=true, which means that there are more results.
+        You can find more information about pagination in Sentry here: https://docs.sentry.io/api/pagination/
+        """
         if link_header:
             links = link_header.split(",")
             for link in links:
@@ -36,6 +47,8 @@ class SentryClient:
         self, url: str
     ) -> AsyncGenerator[list[dict[str, Any]], None]:
         params: dict[str, Any] = {"per_page": PAGE_SIZE}
+
+        logger.info(f"Getting paginated resource from Sentry for URL: {url}")
 
         while url:
             try:
@@ -61,20 +74,40 @@ class SentryClient:
                 logger.error(f"HTTP occurred while fetching Sentry data: {e}")
                 raise
 
+    async def get_single_resource(self, url: str) -> list[dict[str, Any]]:
+        logger.info(f"Getting single resource from Sentry for URL: {url}")
+        try:
+            response = await self.client.get(url=url)
+            response.raise_for_status()
+            return response.json()
+        except httpx.HTTPStatusError as e:
+            logger.error(
+                f"HTTP error with status code: {e.response.status_code} and response text: {e.response.text}"
+            )
+            return []
+        except httpx.HTTPError as e:
+            logger.error(f"HTTP occurred while fetching Sentry data: {e}")
+            return []
+
     @cache_iterator_result("project")
     async def get_paginated_projects(
         self,
     ) -> AsyncGenerator[list[dict[str, Any]], None]:
-        logger.info("Getting paginated projects from Sentry")
         async for project in self.get_paginated_resource(f"{self.api_url}/projects/"):
             yield project
 
     async def get_paginated_issues(
         self, project_slug: str
     ) -> AsyncGenerator[list[dict[str, Any]], None]:
-        logger.info(f"Getting paginated issues from Sentry for project {project_slug}")
-
         async for issues in self.get_paginated_resource(
             f"{self.api_url}/projects/{self.organization}/{project_slug}/issues/"
         ):
             yield issues
+
+    async def get_project_tags(self, project_slug: str) -> list[dict[str, Any]]:
+        url = f"{self.api_url}/projects/{self.organization}/{project_slug}/tags/{self.selector.tag}/values/"
+        return await self.get_single_resource(url)
+
+    async def get_issue_tags(self, issue_id: str) -> list[dict[str, Any]]:
+        url = f"{self.api_url}/organizations/{self.organization}/issues/{issue_id}/tags/{self.selector.tag}/values/"
+        return await self.get_single_resource(url)
--- integrations/sentry/main.py
+++ integrations/sentry/main.py
@@ -1,4 +1,6 @@
 from enum import StrEnum
+from typing import Any
+import asyncio
 
 from port_ocean.context.ocean import ocean
 from clients.sentry import SentryClient
@@ -8,6 +10,8 @@ from port_ocean.core.ocean_types import ASYNC_GENERATOR_RESYNC_TYPE
 class ObjectKind(StrEnum):
     PROJECT = "project"
     ISSUE = "issue"
+    PROJECT_TAG = "project-tag"
+    ISSUE_TAG = "issue-tag"
 
 
 def init_client() -> SentryClient:
@@ -19,18 +23,49 @@ def init_client() -> SentryClient:
     return sentry_client
 
 
+async def add_tags_to_issue(
+    sentry_client: SentryClient, issue: dict[str, Any]
+) -> dict[str, Any]:
+    tags = await sentry_client.get_issue_tags(issue["id"])
+    return {**issue, "__tags": tags}
+
+
 @ocean.on_resync(ObjectKind.PROJECT)
-async def on_resync_projects(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
+async def on_resync_project(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
     sentry_client = init_client()
+
     async for projects in sentry_client.get_paginated_projects():
         yield projects
 
 
+@ocean.on_resync(ObjectKind.PROJECT_TAG)
+async def on_resync_project_tag(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
+    sentry_client = init_client()
+
+    async for projects in sentry_client.get_paginated_projects():
+        for project in projects:
+            tags = await sentry_client.get_project_tags(project["slug"])
+            project_tags = [{**project, "__tags": tag} for tag in tags]
+            yield project_tags
+
+
 @ocean.on_resync(ObjectKind.ISSUE)
-async def on_resync_issues(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
+async def on_resync_issue(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
     sentry_client = init_client()
 
     async for projects in sentry_client.get_paginated_projects():
         for project in projects:
             async for issues in sentry_client.get_paginated_issues(project["slug"]):
                 yield issues
+
+
+@ocean.on_resync(ObjectKind.ISSUE_TAG)
+async def on_resync_issue_tags(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
+    sentry_client = init_client()
+
+    async for projects in sentry_client.get_paginated_projects():
+        for project in projects:
+            async for issues in sentry_client.get_paginated_issues(project["slug"]):
+                tasks = [add_tags_to_issue(sentry_client, issue) for issue in issues]
+                issue_tags = await asyncio.gather(*tasks)
+                yield issue_tags
