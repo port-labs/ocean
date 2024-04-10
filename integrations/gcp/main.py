7fe2ea8e1fa2a27322a8c8701aabe9f3d13c9a27
MatanGevaPort <matang@getport.io>
2024-04-10 17:23:59 +0300

    Added case where there are not resources

--- integrations/gcp/main.py
+++ integrations/gcp/main.py
@@ -53,12 +53,18 @@ async def feed_events_callback(feed_event: Request) -> Response:
                 await ocean.unregister_raw(message.asset_type, [resource])
             else:
                 await ocean.register_raw(message.asset_type, [resource])
-        else:
+        elif len(results[0]) > 1:
             logger.warning(
                 f"Didn't find any resource named: {message.asset_name}",
                 **message.metadata,
             )
             return Response(status_code=http.HTTPStatus.NOT_FOUND)
+        else:
+            logger.warning(
+                f"Found too many resources named: {message.asset_name}",
+                **message.metadata,
+            )
+            return Response(status_code=http.HTTPStatus.CONFLICT)
     except Exception as e:
         logger.error(
             f"Got error while processing Feed event {message.message_id}: {str(e)}",
