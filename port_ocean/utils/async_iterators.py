6e3265fb736eb05460191ddac4f2deb0f9298882
tankilevitch <tomtankilevitch@gmail.com>
2024-04-12 13:00:38 +0300

    raise error

--- port_ocean/utils/async_iterators.py
+++ port_ocean/utils/async_iterators.py
@@ -36,7 +36,7 @@ async def stream_async_iterators_tasks(
     :return: A stream of results
     """
     if not tasks:
-        return
+        raise StopAsyncIteration("No tasks provided")
 
     if len(tasks) == 1:
         async for batch_items in tasks[0]:
