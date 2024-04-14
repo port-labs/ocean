94566684168bc566b7101374abaa7504e27ae418
MatanGevaPort <matang@getport.io>
2024-04-14 10:05:06 +0300

    Added docs to the schemas

--- integrations/gcp/gcp_core/utils.py
+++ integrations/gcp/gcp_core/utils.py
@@ -21,6 +21,8 @@ def parseProtobufMessages(messages: MutableSequence[T]) -> list[dict[str, Any]]:
 
 
 async def parseSubscriptionMessageFromRequest(request: Request) -> SubscriptionMessage:
+    # The message schema: https://cloud.google.com/pubsub/docs/push?_gl=1*thv8i4*_ga*NDQwMTA2MzM5LjE3MTEyNzQ2MDY.*_ga_WH2QY8WWF5*MTcxMzA3NzU3Ni40My4xLjE3MTMwNzgxMjUuMC4wLjA.&_ga=2.161162040.-440106339.1711274606&_gac=1.184150868.1711468720.CjwKCAjw5ImwBhBtEiwAFHDZx1mm-z19UdKpEARcG2-F_TXXbXw7j7_gVPKiQ9Z5KcpsvXF1fFb_MBoCUFkQAvD_BwE#receive_push
+    # The Asset schema: https://cloud.google.com/asset-inventory/docs/monitoring-asset-changes#creating_feeds
     request_json = await request.json()
     message_id = request_json["message"]["messageId"]
     try:
