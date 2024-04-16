312178639e70ac30b2375f081dd72c245d8e874b
MatanGevaPort <matang@getport.io>
2024-04-16 17:08:52 +0300

    Fixed calls to feedevent, changed call to special kind and changed log

--- integrations/gcp/main.py
+++ integrations/gcp/main.py
@@ -19,7 +19,7 @@ class AssetTypesWithSpecialHandling(enum.StrEnum):
     TOPIC = GCP_PUBSUB_TOPIC_ASSET_KIND
 
 
-@ocean.on_resync(kind=GCP_PUBSUB_TOPIC_ASSET_KIND)
+@ocean.on_resync(kind=AssetTypesWithSpecialHandling.TOPIC)
 async def resync_topics(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
     with logger.contextualize(kind=kind):
         gcp_client = create_gcp_client_from_ocean_config()
@@ -44,30 +44,30 @@ async def feed_events_callback(request: Request) -> Response:
     gcp_client = create_gcp_client_from_ocean_config()
     try:
         feed_event = await parse_feed_event_from_request(request)
-        with logger.contextualize(**feed_event.metadata):
-            resource = await gcp_client.get_resource(
-                feed_event.asset_name, feed_event.asset_type
+        with logger.contextualize(
+            message_id=feed_event["message_id"],
+            asset_name=feed_event["asset_name"],
+            asset_type=feed_event["asset_type"],
+        ):
+            resource = await gcp_client.get_single_resource(
+                feed_event["asset_name"], feed_event["asset_type"]
             )
-            if feed_event.asset_type == GCP_PUBSUB_TOPIC_ASSET_KIND:
+            if feed_event["asset_type"] == GCP_PUBSUB_TOPIC_ASSET_KIND:
                 resource = await gcp_client.get_pubsub_topic(
-                    feed_event.asset_name, resource["project"]
+                    feed_event["asset_name"], resource["project"]
                 )
             logger.info("Got Cloud Asset Inventory Feed event")
-            if (
-                "deleted" in feed_event.data.keys()
-                and feed_event.data["deleted"] is True
-            ):
-                await ocean.unregister_raw(feed_event.asset_type, [resource])
+            if feed_event["data"].get("deleted") is True:
+                await ocean.unregister_raw(feed_event["asset_type"], [resource])
             else:
-                await ocean.register_raw(feed_event.asset_type, [resource])
+                await ocean.register_raw(feed_event["asset_type"], [resource])
     except ResourceNotFoundError:
-        logger.warning(f"Didn't find any resource named: {feed_event.asset_name}")
+        logger.warning(f"Didn't find any resource named: {feed_event['asset_name']}")
         return Response(status_code=http.HTTPStatus.NOT_FOUND)
     except GotFeedCreatedSuccessfullyMessage:
         logger.info("Feed has been created successfully.")
-        return Response(status_code=http.HTTPStatus.OK)
-    except Exception as e:
-        logger.error(
-            f"Got error while processing Feed event {feed_event.message_id}: {str(e)}"
+    except Exception:
+        logger.exception(
+            f"Got error while processing Feed event {feed_event['message_id']}"
         )
     return Response(status_code=http.HTTPStatus.OK)
