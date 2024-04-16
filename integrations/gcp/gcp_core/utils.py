5cd1b8c182eb90511055476a2239210176e22389
MatanGevaPort <matang@getport.io>
2024-04-16 17:07:55 +0300

    Changed FeedEvent to be typeddict, separated b64 and json load, fixed comments

--- integrations/gcp/gcp_core/utils.py
+++ integrations/gcp/gcp_core/utils.py
@@ -1,17 +1,17 @@
 import base64
 import json
-from typing import TypeVar
-
-import proto  # type: ignore
+from typing import Any, TypedDict
 from fastapi import Request
 from port_ocean.context.event import event
 from port_ocean.context.ocean import ocean
 
 from gcp_core.gcp_client import GCPClient
-from gcp_core.types import FeedEvent
 
-T = TypeVar("T", bound=proto.Message)
-
+class FeedEvent(TypedDict):
+    message_id: str
+    asset_name: str
+    asset_type: str
+    data: dict[Any, Any]
 
 class GotFeedCreatedSuccessfullyMessage(Exception):
     pass
@@ -21,18 +21,18 @@ async def parse_feed_event_from_request(
     request: Request,
 ) -> FeedEvent:
     # The message schema: https://cloud.google.com/pubsub/docs/push?_gl=1*thv8i4*_ga*NDQwMTA2MzM5LjE3MTEyNzQ2MDY.*_ga_WH2QY8WWF5*MTcxMzA3NzU3Ni40My4xLjE3MTMwNzgxMjUuMC4wLjA.&_ga=2.161162040.-440106339.1711274606&_gac=1.184150868.1711468720.CjwKCAjw5ImwBhBtEiwAFHDZx1mm-z19UdKpEARcG2-F_TXXbXw7j7_gVPKiQ9Z5KcpsvXF1fFb_MBoCUFkQAvD_BwE#receive_push
-    # The Asset schema: https://cloud.google.com/asset-inventory/docs/monitoring-asset-changes#creating_feeds
     request_json = await request.json()
     message_id = request_json["message"]["messageId"]
     try:
-        asset_data = json.loads(base64.b64decode(request_json["message"]["data"]))
-    except json.JSONDecodeError:
-        if (
-            base64.b64decode(request_json["message"]["data"])
-            .decode("utf-8")
-            .startswith("You have successfully configured real time feed ")
+        data = base64.b64decode(request_json["message"]["data"])
+        asset_data = json.loads(data)
+    except json.JSONDecodeError as e:
+        if data.decode("utf-8").startswith(
+            "You have successfully configured real time feed"
         ):
             raise GotFeedCreatedSuccessfullyMessage()
+        raise e
+    # The Asset schema: https://cloud.google.com/asset-inventory/docs/monitoring-asset-changes#creating_feeds
     asset_type = asset_data["asset"]["assetType"]
     asset_name = asset_data["asset"]["name"]
     feed_event = FeedEvent(
