cc65fd53c8402e6fb0f24f5a30d2ab702601b8e1
MatanGevaPort <matang@getport.io>
2024-04-17 15:40:17 +0300

    used new ocean iterator capability

--- integrations/gcp/gcp_core/search/iterators.py
+++ integrations/gcp/gcp_core/search/iterators.py
@@ -1,25 +1,7 @@
-import typing
 from typing import Any, AsyncIterable, Callable
-
-import aiostream  # type: ignore
 from port_ocean.core.ocean_types import ASYNC_GENERATOR_RESYNC_TYPE
-
+from port_ocean.utils.async_iterators import stream_async_iterators_tasks
 from gcp_core.search.searches import search_all_projects
-
-
-async def stream_async_iterators_tasks(
-    tasks: typing.List[typing.AsyncIterable[typing.Any]],
-) -> typing.AsyncIterable[typing.Any]:
-    """
-    Streams the results of multiple async iterators
-
-    :param tasks: A list of async iterators
-    :return: A stream of results
-    """
-    combine = aiostream.stream.merge(tasks[0], *tasks[1:])
-    async with combine.stream() as streamer:
-        async for batch_items in streamer:
-            yield batch_items
 
 
 async def iterate_per_available_project(
@@ -31,5 +13,5 @@ async def iterate_per_available_project(
         tasks = [
             project_dependent_callable(project, *args, **kwargs) for project in projects
         ]
-        async for batch in stream_async_iterators_tasks(tasks):
+        async for batch in stream_async_iterators_tasks(*tasks):
             yield batch
