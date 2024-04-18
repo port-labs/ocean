5474784348c62a9e0746f9640de1231c1eb9ec16
MatanGevaPort <matang@getport.io>
2024-04-18 16:19:19 +0300

    Added error when got no permissions

---
+++ integrations/gcp/gcp_core/search/errors.py
@@ -0,0 +1,2 @@
+class NoGetProjectPermissionsError(Exception):
+    pass
--- integrations/gcp/gcp_core/search/iterators.py
+++ integrations/gcp/gcp_core/search/iterators.py
@@ -1,4 +1,7 @@
 from typing import Any
+
+from loguru import logger
+from gcp_core.search.errors import NoGetProjectPermissionsError
 from port_ocean.core.ocean_types import ASYNC_GENERATOR_RESYNC_TYPE
 from port_ocean.utils.async_iterators import stream_async_iterators_tasks
 from gcp_core.search.resource_searches import search_all_projects
@@ -9,9 +12,13 @@ async def iterate_per_available_project(
     *args: Any,
     **kwargs: Any,
 ) -> ASYNC_GENERATOR_RESYNC_TYPE:
-    async for projects in search_all_projects():
-        tasks = [
-            project_dependent_callable(project, *args, **kwargs) for project in projects
-        ]
-        async for batch in stream_async_iterators_tasks(*tasks):
-            yield batch
+    try:
+        async for projects in search_all_projects():
+            tasks = [
+                project_dependent_callable(project, *args, **kwargs) for project in projects
+            ]
+            async for batch in stream_async_iterators_tasks(*tasks):
+                yield batch
+    except StopAsyncIteration:
+        logger.error("Got no `resourcemanager.projects.get`` permissions to run on any project")
+        raise NoGetProjectPermissionsError()
