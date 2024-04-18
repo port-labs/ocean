0e08acf2edad730c83dc1d007dfe370b50699981
Shalev Avhar <shalev@getport.io>
2024-04-18 12:56:06 +0300

    refactor: AWS session creation to use async/await

--- integrations/aws/utils.py
+++ integrations/aws/utils.py
@@ -41,12 +41,12 @@ class AwsCredentials:
     async def createSession(self, region: Optional[str] = None):
         if self.isRole():
             if region:
-                return await aioboto3.Session(self.access_key_id, self.secret_access_key, self.session_token, region)
-            return await aioboto3.Session(self.access_key_id, self.secret_access_key, self.session_token, region)
+                return aioboto3.Session(self.access_key_id, self.secret_access_key, self.session_token, region)
+            return aioboto3.Session(self.access_key_id, self.secret_access_key, self.session_token, region)
         else:
             if region:
-                return  await aioboto3.Session(aws_access_key_id=self.access_key_id, aws_secret_access_key=self.secret_access_key, region_name=region)
-            return await aioboto3.Session(aws_access_key_id=self.access_key_id, aws_secret_access_key=self.secret_access_key)
+                return aioboto3.Session(aws_access_key_id=self.access_key_id, aws_secret_access_key=self.secret_access_key, region_name=region)
+            return aioboto3.Session(aws_access_key_id=self.access_key_id, aws_secret_access_key=self.secret_access_key)
 
 
 _aws_credentials: list[AwsCredentials] = []
@@ -141,12 +141,12 @@ async def _get_sessions(custom_account_id: Optional[str] = None, custom_region: 
     """
     if custom_account_id:
         credentials = find_credentials_by_account_id(custom_account_id)
-        yield credentials.createSession(custom_region if custom_region else None)
+        yield await credentials.createSession(custom_region if custom_region else None)
         return
     
     
     for credentials in _aws_credentials:
-        yield credentials.createSession(custom_region if custom_region else None)
+        yield await credentials.createSession(custom_region if custom_region else None)
     
 def _fix_unserializable_date_properties(obj: Any) -> Any:
     """
