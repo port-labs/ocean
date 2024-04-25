42998c0b1de834a971bdf8b852b995fadf7cdf1c
omribarouch <omrib@getport.io>
2024-04-25 18:39:04 +0300

    webhook processing queue

--- integrations/gitlab/gitlab_integration/events/event_handler.py
+++ integrations/gitlab/gitlab_integration/events/event_handler.py
@@ -1,4 +1,5 @@
 import asyncio
+from abc import abstractmethod, ABC
 from collections import defaultdict
 from loguru import logger
 from typing import Awaitable, Callable, Any, Type
@@ -9,15 +10,40 @@ from gitlab_integration.gitlab_service import GitlabService
 Observer = Callable[[str, dict[str, Any]], Awaitable[Any]]
 
 
-class EventHandler:
+class BaseEventHandler(ABC):
     def __init__(self) -> None:
+        self._webhook_handling_concurrency = asyncio.Semaphore(20)
+        self.webhook_tasks_queue: asyncio.Queue = asyncio.Queue()
+
+    async def start_event_processor(self) -> None:
+        logger.error(f"Started {self.__class__.__name__} worker")
+        while True:
+            event_id, body = await self.webhook_tasks_queue.get()
+            await asyncio.sleep(3)
+            try:
+                async with self._webhook_handling_concurrency:
+                    await self._notify(event_id, body)
+            finally:
+                self.webhook_tasks_queue.task_done()
+
+    @abstractmethod
+    async def _notify(self, event: str, body: dict[str, Any]) -> None:
+        pass
+
+    async def notify(self, event: str, body: dict[str, Any]) -> None:
+        await self.webhook_tasks_queue.put((event, body))
+
+
+class EventHandler(BaseEventHandler):
+    def __init__(self) -> None:
+        super().__init__()
         self._observers: dict[str, list[Observer]] = defaultdict(list)
 
     def on(self, events: list[str], observer: Observer) -> None:
         for event in events:
             self._observers[event].append(observer)
 
-    async def notify(self, event: str, body: dict[str, Any]) -> Awaitable[Any]:
+    async def _notify(self, event: str, body: dict[str, Any]) -> None:
         observers = asyncio.gather(
             *(observer(event, body) for observer in self._observers.get(event, []))
         )
@@ -27,11 +53,10 @@ class EventHandler:
                 f"event: {event} has no matching handler. the handlers available are for events: {self._observers.keys()}"
             )
 
-        return observers
 
-
-class SystemEventHandler:
+class SystemEventHandler(BaseEventHandler):
     def __init__(self) -> None:
+        super().__init__()
         self._hook_handlers: dict[str, list[Type[HookHandler]]] = defaultdict(list)
         self._clients: list[GitlabService] = []
 
@@ -42,10 +67,10 @@ class SystemEventHandler:
     def add_client(self, client: GitlabService) -> None:
         self._clients.append(client)
 
-    async def notify(self, event: str, body: dict[str, Any]) -> Awaitable[Any]:
+    async def _notify(self, event: str, body: dict[str, Any]) -> None:
         # best effort to notify using all clients, as we don't know which one of the clients have the permission to
         # access the project
-        return asyncio.gather(
+        asyncio.gather(
             *(
                 hook_handler(client).on_hook(event, body)
                 for client in self._clients
--- integrations/gitlab/gitlab_integration/ocean.py
+++ integrations/gitlab/gitlab_integration/ocean.py
@@ -4,10 +4,8 @@ from datetime import datetime, timedelta
 from itertools import islice
 from typing import Any
 
-from fastapi import BackgroundTasks
 from loguru import logger
 from starlette.requests import Request
-from port_ocean.context.event import event
 
 from gitlab_integration.events.setup import event_handler, system_event_handler
 from gitlab_integration.models.webhook_groups_override_config import (
@@ -24,40 +22,25 @@ NO_WEBHOOK_WARNING = "Without setting up the webhook, the integration will not e
 PROJECT_RESYNC_BATCH_SIZE = 10
 
 
-async def _handle_webhook(
-    group_id: str, request_body: dict[str, Any], gitlab_event_name: str
-) -> None:
-    event_id = f"{gitlab_event_name}:{group_id}"
+@ocean.router.post("/hook/{group_id}")
+async def handle_webhook_request(group_id: str, request: Request) -> dict[str, Any]:
+    body: dict[str, Any] = await request.json()
+    event_id = f"{request.headers.get('X-Gitlab-Event')}:{group_id}"
     with logger.contextualize(event_id=event_id):
         logger.debug(f"Received webhook event {event_id} from Gitlab")
-        await event_handler.notify(event_id, request_body)
-
-
-@ocean.router.post("/hook/{group_id}")
-async def handle_webhook_request(
-    group_id: str, request: Request, background_tasks: BackgroundTasks
-) -> dict[str, Any]:
-    gitlab_event_name: str = request.headers.get("X-Gitlab-Event")
-    body: dict[str, Any] = await request.json()
-    background_tasks.add_task(_handle_webhook, group_id, body, gitlab_event_name)
+        await event_handler.webhook_tasks_queue.put((event_id, body))
 
     return {"ok": True}
 
 
-async def _handle_system_webhook(request_body: dict[str, Any]) -> None:
+@ocean.router.post("/system/hook")
+async def handle_system_webhook_request(request: Request) -> dict[str, Any]:
+    body: dict[str, Any] = await request.json()
     # some system hooks have event_type instead of event_name in the body, such as merge_request events
-    event_name: str = request_body.get("event_name") or request_body.get("event_type")
+    event_name: str = body.get("event_name") or body.get("event_type")
     with logger.contextualize(event_name=event_name):
         logger.debug(f"Received system webhook event {event_name} from Gitlab")
-        await system_event_handler.notify(event_name, request_body)
-
-
-@ocean.router.post("/system/hook")
-async def handle_system_webhook_request(
-    request: Request, background_tasks: BackgroundTasks
-) -> dict[str, Any]:
-    body: dict[str, Any] = await request.json()
-    background_tasks.add_task(_handle_system_webhook, body)
+        await system_event_handler.notify(event_name, body)
 
     return {"ok": True}
 
@@ -99,6 +82,9 @@ async def on_start() -> None:
             integration_config["use_system_hook"],
             token_webhook_mapping,
         )
+
+        asyncio.create_task(event_handler.start_event_processor())
+        asyncio.create_task(system_event_handler.start_event_processor())
     except Exception as e:
         logger.warning(
             f"Failed to setup webhook: {e}. {NO_WEBHOOK_WARNING}",
