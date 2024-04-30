39bbdca08d80f59ddc11f1c414638bfaac161fd2
omribarouch <omrib@getport.io>
2024-04-30 15:21:53 +0300

    cr fixes

--- integrations/gitlab/gitlab_integration/events/event_handler.py
+++ integrations/gitlab/gitlab_integration/events/event_handler.py
@@ -2,34 +2,48 @@ import asyncio
 from abc import abstractmethod, ABC
 from asyncio import Queue
 from collections import defaultdict
+from copy import deepcopy
+
 from loguru import logger
 from typing import Awaitable, Callable, Any, Type
 
 from gitlab_integration.events.hooks.base import HookHandler
 from gitlab_integration.gitlab_service import GitlabService
+from port_ocean.context.event import event, event_context
 
 Observer = Callable[[str, dict[str, Any]], Awaitable[Any]]
 
 
 class BaseEventHandler(ABC):
     def __init__(self) -> None:
-        self.webhook_tasks_queue: Queue = Queue()
+        self.webhook_tasks_queue = Queue()
 
     async def start_event_processor(self) -> None:
         logger.info(f"Started {self.__class__.__name__} worker")
         while True:
-            event_id, body = await self.webhook_tasks_queue.get()
+            event_ctx, event_id, body = await self.webhook_tasks_queue.get()
             try:
-                await self._notify(event_id, body)
+                async with event_context(
+                    event_type="http_event_async_worker", parent_override=event_ctx
+                ):
+                    await self._notify(event_id, body)
+            except:
+                logger.exception(f"")
             finally:
                 self.webhook_tasks_queue.task_done()
 
     @abstractmethod
-    async def _notify(self, event: str, body: dict[str, Any]) -> None:
+    async def _notify(self, event_id: str, body: dict[str, Any]) -> None:
         pass
 
-    async def notify(self, event: str, body: dict[str, Any]) -> None:
-        await self.webhook_tasks_queue.put((event, body))
+    async def notify(self, event_id: str, body: dict[str, Any]) -> None:
+        await self.webhook_tasks_queue.put(
+            (
+                deepcopy(event),
+                event_id,
+                body,
+            )
+        )
 
 
 class EventHandler(BaseEventHandler):
@@ -41,14 +55,17 @@ class EventHandler(BaseEventHandler):
         for event in events:
             self._observers[event].append(observer)
 
-    async def _notify(self, event: str, body: dict[str, Any]) -> None:
+    async def _notify(self, event_id: str, body: dict[str, Any]) -> None:
         observers = asyncio.gather(
-            *(observer(event, body) for observer in self._observers.get(event, []))
+            *(
+                observer(event_id, body)
+                for observer in self._observers.get(event_id, [])
+            )
         )
 
         if not observers:
             logger.debug(
-                f"event: {event} has no matching handler. the handlers available are for events: {self._observers.keys()}"
+                f"event: {event_id} has no matching handler. the handlers available are for events: {self._observers.keys()}"
             )
 
 
@@ -65,14 +82,14 @@ class SystemEventHandler(BaseEventHandler):
     def add_client(self, client: GitlabService) -> None:
         self._clients.append(client)
 
-    async def _notify(self, event: str, body: dict[str, Any]) -> None:
+    async def _notify(self, event_id: str, body: dict[str, Any]) -> None:
         # best effort to notify using all clients, as we don't know which one of the clients have the permission to
         # access the project
         asyncio.gather(
             *(
-                hook_handler(client).on_hook(event, body)
+                hook_handler(client).on_hook(event_id, body)
                 for client in self._clients
-                for hook_handler in self._hook_handlers.get(event, [])
+                for hook_handler in self._hook_handlers.get(event_id, [])
             ),
             return_exceptions=True,
         )
