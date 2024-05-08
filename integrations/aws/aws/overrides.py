fa86b1d5a7d9d80013e48c8f632dedbba0f7db66
Shalev Avhar <shalev@getport.io>
2024-05-08 11:22:05 +0300

    fix: use pydantic validations

--- integrations/aws/aws/overrides.py
+++ integrations/aws/aws/overrides.py
@@ -4,17 +4,11 @@ from port_ocean.core.handlers.port_app_config.models import (
     PortAppConfig,
     Selector,
 )
-from pydantic import Field, validator
+from pydantic import Field
 
 
 class AWSSpecificKindsResourceConfig(Selector):
-    resource_kinds: list[str] = Field(alias="resourceKinds", default=[])
-
-    @validator("resource_kinds")
-    def validate_resource_kinds_min_size(cls, v: dict[str, str]) -> dict[str, str]:
-        if len(v) < 1:
-            raise ValueError("resourceKinds must have at least one item")
-        return v
+    resource_kinds: list[str] = Field(alias="resourceKinds", default=[], min_items=1)
 
 
 class AWSResourceConfig(ResourceConfig):
