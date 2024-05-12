e545b8ea14ad7a0f5ea3dc1b6520dd2adbdf8d28
Shalev Avhar <shalev@getport.io>
2024-05-12 10:40:44 +0300

    fix: remove try-catch

--- integrations/aws/utils/resources.py
+++ integrations/aws/utils/resources.py
@@ -55,38 +55,33 @@ async def resync_cloudcontrol(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
         account_id = await _session_manager.find_account_id_by_session(session)
         next_token = None
         while True:
-            try:
-                async with session.client("cloudcontrol") as cloudcontrol:
-                    params = {
-                        "TypeName": kind,
-                    }
-                    if next_token:
-                        params["NextToken"] = next_token
+            async with session.client("cloudcontrol") as cloudcontrol:
+                params = {
+                    "TypeName": kind,
+                }
+                if next_token:
+                    params["NextToken"] = next_token
 
-                    response = await cloudcontrol.list_resources(**params)
-                    next_token = response.get("NextToken")
-                    resources = response.get("ResourceDescriptions", [])
-                    if not resources:
-                        break
-                    page_resources = []
-                    for instance in resources:
-                        serialized = instance.copy()
-                        serialized.update(
-                            {
-                                CustomProperties.KIND: kind,
-                                CustomProperties.ACCOUNT_ID: account_id,
-                                CustomProperties.REGION: region,
-                                "Properties": json.loads(instance.get("Properties")),
-                            }
-                        )
-                        page_resources.append(
-                            fix_unserializable_date_properties(serialized)
-                        )
-                    yield page_resources
-            except Exception:
-                logger.exception(
-                    f"Failed to list CloudControl Instance in account {account_id} kind {kind} region: {region}"
-                )
-                break
-            if not next_token:
-                break
+                response = await cloudcontrol.list_resources(**params)
+                next_token = response.get("NextToken")
+                resources = response.get("ResourceDescriptions", [])
+                if not resources:
+                    break
+                page_resources = []
+                for instance in resources:
+                    serialized = instance.copy()
+                    serialized.update(
+                        {
+                            CustomProperties.KIND: kind,
+                            CustomProperties.ACCOUNT_ID: account_id,
+                            CustomProperties.REGION: region,
+                            "Properties": json.loads(instance.get("Properties")),
+                        }
+                    )
+                    page_resources.append(
+                        fix_unserializable_date_properties(serialized)
+                    )
+                yield page_resources
+
+                if not next_token:
+                    break
