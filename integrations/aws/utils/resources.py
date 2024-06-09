a0f4af1cbc81dbcccfeaf8384c145753fe2ed7bb
Shalev Avhar <shalev@getport.io>
2024-06-09 23:48:21 +0300

    feat: Add batch resource syncing for ACM certificates and AMI images
    The code changes introduce the following modifications:
    - Added `batch_resources` function in `utils.resources.py` to handle batch syncing of ACM certificates and AMI images.
    - Created `resync_acm` and `resync_ami` functions in `main.py` to yield batches of resources using `batch_resources`.
    - Updated imports and function calls to accommodate the new changes.
    
    Recent user commits and repository commits indicate bug fixes, feature additions, and version updates. However, there are no specific conventions observed in the commit messages.

--- integrations/aws/utils/resources.py
+++ integrations/aws/utils/resources.py
@@ -1,6 +1,7 @@
 import json
-from typing import Any
+from typing import Any, Literal
 
+import aioboto3
 from loguru import logger
 from utils.misc import (
     CustomProperties,
@@ -49,6 +50,41 @@ async def describe_single_resource(
             )
             return serialized
     return {}
+
+
+async def batch_resources(
+    kind: str,
+    session: aioboto3.Session,
+    service_name: Literal["acm", "elbv2", "cloudformation", "ec2"],
+    describe_method: str,
+    list_param: str,
+    marker_param: str = "NextToken",
+) -> ASYNC_GENERATOR_RESYNC_TYPE:
+    region = session.region_name
+    account_id = await _session_manager.find_account_id_by_session(session)
+    next_token = None
+    while True:
+        async with session.client(service_name) as client:
+            params: dict[str, Any] = {}
+            if next_token:
+                pointer_param = (
+                    marker_param if marker_param == "NextToken" else "Marker"
+                )
+                params[pointer_param] = next_token
+            response = await getattr(client, describe_method)(**params)
+            next_token = response.get(marker_param)
+            if results := response.get(list_param, []):
+                yield [
+                    {
+                        CustomProperties.KIND: kind,
+                        CustomProperties.ACCOUNT_ID: account_id,
+                        CustomProperties.REGION: region,
+                        "Properties": fix_unserializable_date_properties(resource),
+                    }
+                    for resource in results
+                ]
+        if not next_token:
+            break
 
 
 async def resync_cloudcontrol(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
