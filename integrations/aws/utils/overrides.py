14161d8252e4232b4d8d59f6e6a5467f44464d63
Shalev Avhar <shalev@getport.io>
2024-06-16 12:04:45 +0300

    fix: lint error

--- integrations/aws/utils/overrides.py
+++ integrations/aws/utils/overrides.py
@@ -3,7 +3,7 @@ from port_ocean.core.handlers.port_app_config.models import (
     PortAppConfig,
     Selector,
 )
-from pydantic import Field, validator
+from pydantic import Field
 
 
 class AWSDescribeResourcesSelector(Selector):
