aa81ea1f43599ade33d62c66eebb4eecefb05705
Katamba Martin <martinkatamba@gmail.com>
2024-07-05 21:41:05 +0300

    Update client.py

--- integrations/clickup/clickup/client.py
+++ integrations/clickup/clickup/client.py
@@ -7,7 +7,6 @@ from port_ocean.context.ocean import ocean
 
 class ClickUpClient:
     def __init__(self, clickup_url: str, clickup_api_key: str) -> None:
-        self.clickup_url = clickup_url
         self.api_url = f"{self.clickup_url}/api/v2"
         self.api_auth_header = {"Authorization": clickup_api_key}
         self.client = http_async_client
