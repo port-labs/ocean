9a3cc263943777ce9f04e75e27ba0a1ececf11b2
hpal <ahosea15@gmail.com>
2024-07-25 19:07:02 +0300

    [Wiz] Add parameters to issue kind

--- integrations/wiz/main.py
+++ integrations/wiz/main.py
@@ -1,13 +1,14 @@
 from enum import StrEnum
-from typing import Any
+from typing import Any, cast
 
 from fastapi import Depends, HTTPException
 from fastapi.security import HTTPBearer
 from loguru import logger
 from port_ocean.context.ocean import ocean
 from port_ocean.core.ocean_types import ASYNC_GENERATOR_RESYNC_TYPE
-
+from port_ocean.context.event import event
 from wiz.client import WizClient
+from overrides import IssueResourceConfig
 
 
 class ObjectKind(StrEnum):
@@ -26,31 +27,53 @@ def init_client() -> WizClient:
     )
 
 
-@ocean.on_resync()
-async def on_resync(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
+@ocean.on_resync(kind=ObjectKind.PROJECT)
+async def on_resync_projects(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
+    """Resync projects from Wiz."""
+    wiz_client = init_client()
+    async for projects in wiz_client.get_projects():
+        logger.info(f"Received {len(projects)} projects")
+        yield projects
+
+
+@ocean.on_resync(kind=ObjectKind.ISSUE)
+async def on_resync_issues(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
+    logger.info(f"Resyncing {kind} kind")
     wiz_client = init_client()
 
-    if kind == ObjectKind.PROJECT:
-        async for projects in wiz_client.get_projects():
-            logger.info(f"Received {len(projects)} projects")
-            yield projects
-    else:
-        async for _issues in wiz_client.get_issues():
-            logger.info(f"Received {len(_issues)} issues")
-            if kind == ObjectKind.ISSUE:
-                yield _issues
-            elif kind == ObjectKind.CONTROL:
-                yield [
-                    issue["sourceRule"]
-                    for issue in _issues
-                    if issue.get("sourceRule") is not None
-                ]
-            elif kind == ObjectKind.SERVICE_TICKET:
-                yield [
-                    ticket
-                    for issue in _issues
-                    for ticket in issue.get("serviceTickets", [])
-                ]
+    max_pages = cast(IssueResourceConfig, event.resource_config).selector.max_pages
+    status_list = cast(IssueResourceConfig, event.resource_config).selector.status_list
+
+    async for _issues in wiz_client.get_issues(
+        max_pages=max_pages, status_list=status_list
+    ):
+        logger.info(f"Received {len(_issues)} issues")
+        yield _issues
+
+
+@ocean.on_resync(kind=ObjectKind.CONTROL)
+async def on_resync_controls(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
+    logger.info(f"Resyncing {kind} kind")
+    wiz_client = init_client()
+
+    async for _issues in wiz_client.get_issues():
+        yield [
+            issue["sourceRule"]
+            for issue in _issues
+            if issue.get("sourceRule") is not None
+        ]
+
+
+@ocean.on_resync(kind=ObjectKind.SERVICE_TICKET)
+async def on_resync_tickets(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
+    logger.info(f"Resyncing {kind} kind")
+    wiz_client = init_client()
+
+    async for _issues in wiz_client.get_issues():
+        logger.info(f"Received {len(_issues)} tickets")
+        yield [
+            ticket for issue in _issues for ticket in issue.get("serviceTickets", [])
+        ]
 
 
 @ocean.router.post("/webhook")
