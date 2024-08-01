3bd995cd0dfc3b67748cefc55c4c748157c362f7
mkarmah <mikeyarmah@gmail.com>
2024-08-01 09:42:49 +0000

    Fixed unpacking error in unregister raw

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,14 @@ this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.htm
 
 <!-- towncrier release notes start -->
 
+## 0.9.8 (2024-08-01)
+
+
+### Bug Fixes
+
+- Fix unpacking error in unregister raw
+
+
 ## 0.9.7 (2024-07-31)
 
 ### Bug Fixes
--- port_ocean/core/integrations/mixins/sync_raw.py
+++ port_ocean/core/integrations/mixins/sync_raw.py
@@ -311,12 +311,16 @@ class SyncRawMixin(HandlerMixin, EventsMixin):
             resource for resource in config.resources if resource.kind == kind
         ]
 
-        entities, errors = await asyncio.gather(
+        entities, errors = zip(*await asyncio.gather(
             *(
-                self._unregister_resource_raw(resource, results, user_agent_type)
-                for resource in resource_mappings
+            self._unregister_resource_raw(resource, results, user_agent_type) 
+            for resource in resource_mappings
             )
         )
+        )
+
+        entities = sum(entities, [])
+        errors = sum(errors, [])
 
         if errors:
             message = f"Failed to unregister all entities with {len(errors)} errors"
