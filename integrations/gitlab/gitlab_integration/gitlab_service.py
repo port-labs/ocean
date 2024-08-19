a67d99e47c870ba0a8bd04b80a9cc6554d0ff5d3
tankilevitch <tomtankilevitch@gmail.com>
2024-08-19 12:17:50 +0300

    fix

--- integrations/gitlab/gitlab_integration/gitlab_service.py
+++ integrations/gitlab/gitlab_integration/gitlab_service.py
@@ -610,16 +610,19 @@ class GitlabService:
             project_file = await AsyncFetcher.fetch_single(
                 project.files.get, file_path, branch
             )
-            logger.debug(
+            logger.info(
                 f"Fetched file {file_path} in project {project.path_with_namespace}"
             )
             project_file = typing.cast(ProjectFile, project_file)
             parsed_file = self._parse_file_content(project_file)
             project_file_dict = project_file.asdict()
 
-            if parsed_file:
-                # Update the content with the parsed content. Useful for JSON and YAML files that can be further processed using itemsToParse
-                project_file_dict["content"] = parsed_file
+            if not parsed_file:
+                # if the file is too large to be processed, we return None
+                return None
+
+            # Update the content with the parsed content. Useful for JSON and YAML files that can be further processed using itemsToParse
+            project_file_dict["content"] = parsed_file
 
             return {"file": project_file_dict, "repo": project.asdict()}
         except Exception as e:
@@ -632,12 +635,12 @@ class GitlabService:
         self, project: Project, path: str
     ) -> typing.AsyncIterator[List[dict[str, Any]]]:
         branch = project.default_branch
+        files = []
         try:
             file_paths = await self._get_file_paths(project, path, branch, True)
             logger.info(
                 f"Found {len(file_paths)} files in project {project.path_with_namespace} files: {file_paths}"
             )
-            files = []
             for file_path in file_paths:
                 file = await self.get_and_parse_single_file(project, file_path, branch)
                 if file:
