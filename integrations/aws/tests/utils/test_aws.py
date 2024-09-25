6c27fc31a733ef465f600e215c92949d23b73a59
mkarmah <mikeyarmah@gmail.com>
2024-09-25 09:45:24 +0000

    added tests

---
+++ integrations/aws/tests/utils/test_aws.py
@@ -0,0 +1,35 @@
+import unittest
+from unittest.mock import AsyncMock, patch, MagicMock
+from starlette.requests import Request
+import asyncio
+from utils.aws import update_available_access_credentials
+
+
+
+@patch('aws.session_manager.SessionManager.reset', new_callable=AsyncMock)
+@patch('utils.aws.lock', new_callable=AsyncMock)
+async def test_update_available_access_credentials(mock_lock, mock_reset):
+    """
+    Test to ensure that thundering herd problem is avoided and multiple
+    concurrent calls only trigger one session reset.
+    """
+
+    result = await update_available_access_credentials()
+    mock_reset.assert_called_once()
+    assert result
+
+    result_1 = await update_available_access_credentials()
+    assert result_1 is True
+    mock_reset.assert_called_once()  # No additional calls should be made
+
+    # Thundering herd test
+    mock_reset.reset_mock()
+    mock_reset.return_value = asyncio.sleep(0.1)
+
+    await asyncio.gather(
+        update_available_access_credentials(),
+        update_available_access_credentials(),
+        update_available_access_credentials()
+    )
+
+    mock_reset.assert_called_once()
