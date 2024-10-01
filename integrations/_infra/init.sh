89036054bd6f73a142ca6d4d70b552f1dfc2af30
yair <yairsimantov20@gmail.com>
2024-10-01 14:39:54 +0300

    revert init

--- integrations/_infra/init.sh
+++ integrations/_infra/init.sh
@@ -2,9 +2,4 @@ if test -e /usr/local/share/ca-certificates/cert.crt; then
   update-ca-certificates
 fi
 
-# delete output file if exists
-MEMRAY_OUTPUT_FILE=/var/memray/output.bin
-
-rm -f $MEMRAY_OUTPUT_FILE
-
-memray run --trace-python-allocators -o $MEMRAY_OUTPUT_FILE debug.py
+python ./debug.py
