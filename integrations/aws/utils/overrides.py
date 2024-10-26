e8fea8f8565125b9caa57b9dcad55f7f1e68d12d
mkarmah <mikeyarmah@gmail.com>
2024-10-26 19:05:53 +0000

    updated repolicy logic

--- integrations/aws/utils/overrides.py
+++ integrations/aws/utils/overrides.py
@@ -30,7 +30,9 @@ class AWSDescribeResourcesSelector(Selector):
         - If the region is listed in the "deny" list of `region_policy`, the method returns False.
         - If the region is listed in the "allow" list of `region_policy`, the method returns True.
         - If the region is not listed in either "allow" or "deny" lists, the method returns False.
-
+        - If the region is listed in both "allow" and "deny" lists, the method returns False.
+        - If the policy denies regions but does not explicitly allow any, and the specific region is not in the deny list, then the region is considered allowed.
+        - If the policy allows regions but does not explicitly deny any, and the specific region is not in the allow list, then the region is considered denied.
         Args:
             region (str): The region to be checked.
 
@@ -43,7 +45,10 @@ class AWSDescribeResourcesSelector(Selector):
             return False
         if region in self.region_policy.allow:
             return True
-
+        if self.region_policy.deny and not self.region_policy.allow:
+            return True
+        if self.region_policy.allow and not self.region_policy.deny:
+            return False
         return False
 
 
