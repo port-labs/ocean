1b85c216e741d4f4749a35cd2cee97b3479aee8d
talsabagport <tal@getport.io>
2024-10-30 15:55:05 +0200

    Return back jira main

--- integrations/jira/main.py
+++ integrations/jira/main.py
@@ -12,38 +12,38 @@ class ObjectKind(StrEnum):
     ISSUE = "issue"
 
 
-# async def setup_application() -> None:
-#     logic_settings = ocean.integration_config
-#     app_host = logic_settings.get("app_host")
-#     if not app_host:
-#         logger.warning(
-#             "No app host provided, skipping webhook creation. "
-#             "Without setting up the webhook, the integration will not export live changes from Jira"
-#         )
-#         return
-#
-#     jira_client = JiraClient(
-#         logic_settings["jira_host"],
-#         logic_settings["atlassian_user_email"],
-#         logic_settings["atlassian_user_token"],
-#     )
-#
-#     await jira_client.create_events_webhook(
-#         logic_settings["app_host"],
-#     )
+async def setup_application() -> None:
+    logic_settings = ocean.integration_config
+    app_host = logic_settings.get("app_host")
+    if not app_host:
+        logger.warning(
+            "No app host provided, skipping webhook creation. "
+            "Without setting up the webhook, the integration will not export live changes from Jira"
+        )
+        return
+
+    jira_client = JiraClient(
+        logic_settings["jira_host"],
+        logic_settings["atlassian_user_email"],
+        logic_settings["atlassian_user_token"],
+    )
+
+    await jira_client.create_events_webhook(
+        logic_settings["app_host"],
+    )
 
 
-# @ocean.on_resync(ObjectKind.PROJECT)
-# async def on_resync_projects(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
-#     client = JiraClient(
-#         ocean.integration_config["jira_host"],
-#         ocean.integration_config["atlassian_user_email"],
-#         ocean.integration_config["atlassian_user_token"],
-#     )
-#
-#     async for projects in client.get_paginated_projects():
-#         logger.info(f"Received project batch with {len(projects)} issues")
-#         yield projects
+@ocean.on_resync(ObjectKind.PROJECT)
+async def on_resync_projects(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
+    client = JiraClient(
+        ocean.integration_config["jira_host"],
+        ocean.integration_config["atlassian_user_email"],
+        ocean.integration_config["atlassian_user_token"],
+    )
+
+    async for projects in client.get_paginated_projects():
+        logger.info(f"Received project batch with {len(projects)} issues")
+        yield projects
 
 
 @ocean.on_resync(ObjectKind.ISSUE)
@@ -59,33 +59,33 @@ async def on_resync_issues(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
         yield issues
 
 
-# @ocean.router.post("/webhook")
-# async def handle_webhook_request(data: dict[str, Any]) -> dict[str, Any]:
-#     client = JiraClient(
-#         ocean.integration_config["jira_host"],
-#         ocean.integration_config["atlassian_user_email"],
-#         ocean.integration_config["atlassian_user_token"],
-#     )
-#     logger.info(f'Received webhook event of type: {data.get("webhookEvent")}')
-#     if "project" in data:
-#         logger.info(f'Received webhook event for project: {data["project"]["key"]}')
-#         project = await client.get_single_project(data["project"]["key"])
-#         await ocean.register_raw(ObjectKind.PROJECT, [project])
-#     elif "issue" in data:
-#         logger.info(f'Received webhook event for issue: {data["issue"]["key"]}')
-#         issue = await client.get_single_issue(data["issue"]["key"])
-#         await ocean.register_raw(ObjectKind.ISSUE, [issue])
-#     logger.info("Webhook event processed")
-#     return {"ok": True}
+@ocean.router.post("/webhook")
+async def handle_webhook_request(data: dict[str, Any]) -> dict[str, Any]:
+    client = JiraClient(
+        ocean.integration_config["jira_host"],
+        ocean.integration_config["atlassian_user_email"],
+        ocean.integration_config["atlassian_user_token"],
+    )
+    logger.info(f'Received webhook event of type: {data.get("webhookEvent")}')
+    if "project" in data:
+        logger.info(f'Received webhook event for project: {data["project"]["key"]}')
+        project = await client.get_single_project(data["project"]["key"])
+        await ocean.register_raw(ObjectKind.PROJECT, [project])
+    elif "issue" in data:
+        logger.info(f'Received webhook event for issue: {data["issue"]["key"]}')
+        issue = await client.get_single_issue(data["issue"]["key"])
+        await ocean.register_raw(ObjectKind.ISSUE, [issue])
+    logger.info("Webhook event processed")
+    return {"ok": True}
 
 
 # Called once when the integration starts.
-# @ocean.on_start()
-# async def on_start() -> None:
-#     logger.info("Starting Port Ocean Jira integration")
-#
-#     if ocean.event_listener_type == "ONCE":
-#         logger.info("Skipping webhook creation because the event listener is ONCE")
-#         return
-#
-#     await setup_application()
+@ocean.on_start()
+async def on_start() -> None:
+    logger.info("Starting Port Ocean Jira integration")
+
+    if ocean.event_listener_type == "ONCE":
+        logger.info("Skipping webhook creation because the event listener is ONCE")
+        return
+
+    await setup_application()
