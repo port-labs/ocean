c01eb08ccb912fc19f527b1e638e1b4dbf5163be
lordsarcastic <adeoti.15.jude@gmail.com>
2024-11-05 14:39:04 +0100

    Fixed all failing tests

--- integrations/jira/tests/test_client.py
+++ integrations/jira/tests/test_client.py
@@ -1,5 +1,5 @@
 from typing import Any, Generator, TypedDict
-from unittest.mock import AsyncMock, MagicMock, patch
+from unittest.mock import MagicMock, patch
 
 import httpx
 import pytest
@@ -47,21 +47,23 @@ class HttpxResponses(TypedDict):
 
 class MockHttpxClient:
     def __init__(self, responses: list[HttpxResponses] = []) -> None:
-        self.responses = [httpx.Response(
-            status_code=response["status_code"],
-            json=response["json"]
-        ) for response in responses]
+        self.responses = [
+            httpx.Response(
+                status_code=response["status_code"],
+                json=response["json"],
+                request=httpx.Request("GET", "https://myorg.atlassian.net"),
+            )
+            for response in responses
+        ]
         self._current_response_index = 0
 
     async def get(self, *args: tuple[Any], **kwargs: dict[str, Any]) -> httpx.Response:
         if self._current_response_index >= len(self.responses):
             raise httpx.HTTPError("No more responses")
-        return self.responses[self._current_response_index]
 
-    async def post(self, *args: tuple[Any], **kwargs: dict[str, Any]) -> httpx.Response:
-        if self._current_response_index >= len(self.responses):
-            raise httpx.HTTPError("No more responses")
-        return self.responses[self._current_response_index]
+        response = self.responses[self._current_response_index]
+        self._current_response_index += 1
+        return response
 
 
 @pytest.mark.parametrize(
@@ -87,9 +89,15 @@ def test_base_required_params_are_generated_correctly(
 
 
 @pytest.mark.asyncio
-@patch(
-    "client.http_async_client",
-    side_effect=MockHttpxClient(
+async def test_make_paginated_request_will_hit_api_till_no_response_left() -> None:
+    client = JiraClient(
+        "https://myorg.atlassian.net",
+        "mail@email.com",
+        "token",
+    )
+
+    # we can't monkeypatch the client because it's an instance attribute
+    client.client = MockHttpxClient(  # type: ignore
         responses=[
             {
                 "status_code": 200,
@@ -99,6 +107,8 @@ def test_base_required_params_are_generated_correctly(
                         {"id": 1},
                         {"id": 2},
                     ],
+                    "startAt": 0,
+                    "maxResults": 2,
                 },
             },
             {
@@ -109,53 +119,58 @@ def test_base_required_params_are_generated_correctly(
                         {"id": 3},
                         {"id": 4},
                     ],
+                    "startAt": 2,
+                    "maxResults": 2,
                 },
             },
         ]
-    ),
-)
-async def test_make_paginated_request_will_hit_api_till_no_response_left(
-    mock_httpx_client: MockHttpxClient,
-) -> None:
-    client = JiraClient(
-        "https://myorg.atlassian.net",
-        "mail@email.com",
-        "token",
     )
 
-    assert (
-        sum(
-            1
-            async for _ in client._make_paginated_request("https://myorg.atlassian.net")
-        )
-        == 2
-    )
+    count = 0
+
+    async for _ in client._make_paginated_request("https://myorg.atlassian.net"):
+        count += 1
+
+    assert count == 2
 
 
 @pytest.mark.asyncio
 async def test_get_all_projects_will_compose_correct_url(monkeypatch: Any) -> None:
+    mock_paginated_request = MagicMock()
+    mock_paginated_request.__aiter__.return_value = ()
+
+    monkeypatch.setattr(JiraClient, "_make_paginated_request", mock_paginated_request)
+
     client = JiraClient(
         "https://myorg.atlassian.net",
         "mail@email.com",
         "token",
     )
 
-    client._make_paginated_request = AsyncMock()
-    await client.get_all_projects()
-    client._make_paginated_request.assert_called_with(
+    async for _ in client.get_all_projects():
+        pass
+
+    client._make_paginated_request.assert_called_with(  # type: ignore
         "https://myorg.atlassian.net/rest/api/3/project/search"
     )
 
 
 @pytest.mark.asyncio
 async def test_get_all_issues_will_compose_correct_url(monkeypatch: Any) -> None:
+    mock_paginated_request = MagicMock()
+    mock_paginated_request.__aiter__.return_value = ()
+
+    monkeypatch.setattr(JiraClient, "_make_paginated_request", mock_paginated_request)
+
     client = JiraClient("https://myorg.atlassian.net", "mail.email.com", "token")
 
-    client._make_paginated_request = AsyncMock()
-    await client.get_all_issues()
-    client._make_paginated_request.assert_called_with(
-        "https://myorg.atlassian.net/rest/api/3/search"
+    async for _ in client.get_all_issues():
+        pass
+
+    # we can't assert the exact call because one of the params is a lambda
+    # function, so we'll just check the url
+    assert client._make_paginated_request.call_args_list[0][0] == (  # type: ignore
+        "https://myorg.atlassian.net/rest/api/3/search",
     )
 
-
-# @pytest.mark.asyncio
+    assert client._make_paginated_request.call_args_list[0][1]["params"] == {}  # type: ignore
