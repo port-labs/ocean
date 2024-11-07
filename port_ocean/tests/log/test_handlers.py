db55df7c426a9d7c3d460f6a66bd5b3492ed84b7
Ivan Kalinovski <ivankalinovski@Ivans-MacBook-Pro.local>
2024-11-07 16:12:24 +0200

    Integration logs not being ingested
    1.Extend method "_serialize_record" to stringify exceptions in "exc_info".
    2.Await logger on thread exit.
    3.Await logger writing exception on exit.

--- port_ocean/tests/log/test_handlers.py
+++ port_ocean/tests/log/test_handlers.py
@@ -3,7 +3,7 @@ from loguru import logger
 from logging import LogRecord
 from queue import Queue
 from logging.handlers import QueueHandler
-from typing import Callable
+from typing import Callable, Any
 
 
 log_message = "This is a test log message."
@@ -12,7 +12,7 @@ exception_message = "Test Exception"
 expected_keys = ["message", "level", "timestamp", "extra"]
 
 
-def test_serialize_record_log_shape():
+def test_serialize_record_log_shape() -> None:
     record = log_record(
         lambda: logger.exception(
             log_message,
@@ -21,10 +21,10 @@ def test_serialize_record_log_shape():
     )
     serialized_record = _serialize_record(record)
     assert all(key in serialized_record for key in expected_keys)
-    assert log_message in serialized_record.get("message")
+    assert log_message in serialized_record.get("message", None)
 
 
-def test_serialize_record_exc_info_single_exception():
+def test_serialize_record_exc_info_single_exception() -> None:
     record = log_record(
         lambda: logger.exception(
             log_message,
@@ -34,27 +34,27 @@ def test_serialize_record_exc_info_single_exception():
         )
     )
     serialized_record = _serialize_record(record)
-    exc_info = assert_extra(serialized_record.get("extra"))
+    exc_info = assert_extra(serialized_record.get("extra", {}))
     assert exception_grouop_message in exc_info
     assert exception_message in exc_info
 
 
-def test_serialize_record_exc_info_group_exception():
+def test_serialize_record_exc_info_group_exception() -> None:
     record = log_record(
         lambda: logger.exception(log_message, exc_info=Exception(exception_message))
     )
     serialized_record = _serialize_record(record)
-    exc_info = assert_extra(serialized_record.get("extra"))
+    exc_info = assert_extra(serialized_record.get("extra", {}))
     assert exception_message in exc_info
 
 
-def assert_extra(extra: dict):
+def assert_extra(extra: dict[str, Any]) -> str:
     exc_info = extra.get("exc_info", None)
     assert type(exc_info) is str
     return exc_info
 
 
-def log_record(cb: Callable[[None], None]) -> LogRecord:
+def log_record(cb: Callable[[], None]) -> LogRecord:
     queue = Queue[LogRecord]()
     queue_handler = QueueHandler(queue)
     logger_id = logger.add(
