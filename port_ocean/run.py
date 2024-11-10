d5cec8881a060d41da1461504c879166b35156c4
MatanGevaPort <matang@getport.io>
2024-11-10 17:32:14 +0200

    alla huakbar

--- port_ocean/run.py
+++ port_ocean/run.py
@@ -57,7 +57,7 @@ def run(
     # Override config with arguments
     if initialize_port_resources is not None:
         app.config.initialize_port_resources = initialize_port_resources
-
+    # aaa
     initialize_defaults(app.integration.AppConfigHandlerClass.CONFIG_CLASS, app.config)
 
     uvicorn.run(app, host="0.0.0.0", port=application_settings.port)
