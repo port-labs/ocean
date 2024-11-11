06196d1d0042ffd5f992de4a91b55bdf1953b3f2
Shalev Avhar <shalev@getport.io>
2024-11-11 17:58:37 +0200

    fix: remove test file added

--- port_ocean/config/settings.py
+++ port_ocean/config/settings.py
@@ -1,7 +1,6 @@
-from pathlib import Path
 from typing import Any, Literal, Type, cast
 
-from pydantic import DirectoryPath, Extra, AnyHttpUrl, parse_obj_as, parse_raw_as
+from pydantic import Extra, AnyHttpUrl, parse_obj_as, parse_raw_as
 from pydantic.class_validators import root_validator, validator
 from pydantic.env_settings import InitSettingsSource, EnvSettingsSource, BaseSettings
 from pydantic.fields import Field
@@ -79,7 +78,7 @@ class IntegrationConfiguration(BaseOceanSettings, extra=Extra.allow):
         default_factory=lambda: IntegrationSettings(type="", identifier="")
     )
     runtime: Runtime = Runtime.OnPrem
-    resources_path: DirectoryPath = Field(default=Path(".port/resources"))
+    resources_path: str = Field(default=".port/resources")
 
     @root_validator()
     def validate_integration_config(cls, values: dict[str, Any]) -> dict[str, Any]:
