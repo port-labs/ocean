e1807a87f3165a7114add3849b4ea6c198b3851c
Victor Ikuomola <vheecthurikuomola@gmail.com>
2024-11-11 09:34:55 +0100

    ref(async-gitlab): uses single resync func for resources

--- integrations/async-gitlab/main.py
+++ integrations/async-gitlab/main.py
@@ -10,16 +10,15 @@ from gitlab.helpers.utils import ObjectKind, ResourceKindsHandledViaWebhooks
 @ocean.on_resync()
 async def on_resources_resync(kind: str) -> None:
     logger.info(f"Received re-sync for kind {kind}")
-    return
 
-@ocean.on_resync(ObjectKind.GROUP)
-async def on_group_resync(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
-    logger.info("Group Re-sync req received")
+    if kind == ObjectKind.PROJECT:
+        return
+
     gitlab_client = GitLabClient.create_from_ocean_config()
 
-    async for groups in gitlab_client.get_resources(ObjectKind.GROUP):
-        logger.info(f"Re-syncing {len(groups)} groups")
-        yield groups
+    async for resources in gitlab_client.get_resources(kind):
+        logger.info(f"Re-syncing {len(resources)} {kind}")
+        yield resources
         return
 
 @ocean.on_resync(ObjectKind.PROJECT)
@@ -30,24 +29,6 @@ async def on_project_resync(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
     async for projects in gitlab_client.get_resources(ObjectKind.PROJECT, {"owned": "yes"}):
         logger.info(f"Re-syncing {len(projects)} projects")
         yield projects
-        return
-
-@ocean.on_resync(ObjectKind.MERGE_REQUEST)
-async def on_merge_request_resync(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
-    gitlab_client = GitLabClient.create_from_ocean_config()
-
-    async for merge_requests in gitlab_client.get_resources(ObjectKind.MERGE_REQUEST):
-        logger.info(f"Re-syncing {len(merge_requests)} merge requests")
-        yield merge_requests
-        return
-
-@ocean.on_resync(ObjectKind.ISSUE)
-async def on_issue_resync(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
-    gitlab_client = GitLabClient.create_from_ocean_config()
-
-    async for issues in gitlab_client.get_resources(ObjectKind.ISSUE):
-        logger.info(f"Re-syncing {len(issues)} issues")
-        yield issues
         return
 
 @ocean.router.post("/webhook")
