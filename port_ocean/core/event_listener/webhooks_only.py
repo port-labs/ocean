48e6f0414f22bf3ff2a19e96f8a2e86f4f52188e
MatanGevaPort <matang@getport.io>
2024-12-15 11:52:51 +0200

    added types

--- port_ocean/core/event_listener/webhooks_only.py
+++ port_ocean/core/event_listener/webhooks_only.py
@@ -15,8 +15,8 @@ class WebhooksOnlyEventListenerSettings(EventListenerSettings):
     """
 
     type: Literal["WEBHOOKS_ONLY"]
-    should_resync = False
-    should_create_resources_on_start = False
+    should_resync: bool = False
+    should_create_resources_on_start: bool = False
 
     def to_request(self) -> dict[str, Any]:
         return {}
