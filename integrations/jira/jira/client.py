80e2764f79b091aa751f709a55025550d27e2a7e
shariff-6 <mohammed.s@getport.io>
2024-12-17 13:36:02 +0300

    Resets page size to 50

--- integrations/jira/jira/client.py
+++ integrations/jira/jira/client.py
@@ -7,7 +7,7 @@ from port_ocean.context.ocean import ocean
 from port_ocean.utils import http_async_client
 import asyncio
 
-PAGE_SIZE = 100
+PAGE_SIZE = 50
 WEBHOOK_NAME = "Port-Ocean-Events-Webhook"
 MAX_CONCURRENT_REQUESTS = 10
 
