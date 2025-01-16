f3ee1ffe23c333cbf546ecd2a09e03323c011694
shariff-6 <mohammed.s@getport.io>
2025-01-16 16:12:38 +0300

    Fixes lint

--- integrations/gitlab/gitlab_integration/core/utils.py
+++ integrations/gitlab/gitlab_integration/core/utils.py
@@ -1,8 +1,8 @@
 from glob2 import fnmatch
 from typing import Union, List
 
+
 def does_pattern_apply(pattern: Union[str, List[str]], string: str) -> bool:
     if isinstance(pattern, list):
         return any(does_pattern_apply(p, string) for p in pattern)
     return fnmatch.fnmatch(string, pattern)
-
