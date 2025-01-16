0c94537b1075070bf50d0a3d39e581dc0cac8380
shariff-6 <mohammed.s@getport.io>
2025-01-16 16:10:15 +0300

    refactor(gitlab): simplify glob pattern matching using glob2.fnmatch
    - Replace custom pattern matching implementation with glob2.fnmatch

--- integrations/gitlab/gitlab_integration/core/utils.py
+++ integrations/gitlab/gitlab_integration/core/utils.py
@@ -1,35 +1,8 @@
-import fnmatch
+from glob2 import fnmatch
+from typing import Union, List
 
-
-def _match(pattern_parts: list[str], string_parts: list[str]) -> bool:
-    if not pattern_parts:  # Reached the end of the pattern
-        return not string_parts
-    if not string_parts:  # Reached the end of the string
-        return pattern_parts == ["**"]
-
-    if pattern_parts[0] == "**":
-        if len(pattern_parts) == 1:
-            return True
-        else:  # Recursive matching
-            return _match(pattern_parts[1:], string_parts) or _match(
-                pattern_parts, string_parts[1:]
-            )
-
-    if fnmatch.fnmatch(string_parts[0], pattern_parts[0]):  # Regular matching
-        return _match(pattern_parts[1:], string_parts[1:])
-    return False
-
-
-def does_pattern_apply(pattern: str | list[str], string: str) -> bool:
+def does_pattern_apply(pattern: Union[str, List[str]], string: str) -> bool:
     if isinstance(pattern, list):
-        return any(
-            does_pattern_apply(single_pattern, string) for single_pattern in pattern
-        )
+        return any(does_pattern_apply(p, string) for p in pattern)
+    return fnmatch.fnmatch(string, pattern)
 
-    pattern_parts = pattern.split("/")
-    string_parts = string.split("/")
-    return _match(pattern_parts, string_parts)
-
-
-def generate_ref(branch_name: str) -> str:
-    return f"refs/heads/{branch_name}"
