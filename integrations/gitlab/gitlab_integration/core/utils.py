b105ea38abcfe670ef2644041b8331aa1a923833
shariff-6 <mohammed.s@getport.io>
2025-01-16 16:15:24 +0300

    Returns generate_ref function

--- integrations/gitlab/gitlab_integration/core/utils.py
+++ integrations/gitlab/gitlab_integration/core/utils.py
@@ -6,3 +6,6 @@ def does_pattern_apply(pattern: Union[str, List[str]], string: str) -> bool:
     if isinstance(pattern, list):
         return any(does_pattern_apply(p, string) for p in pattern)
     return fnmatch.fnmatch(string, pattern)
+
+def generate_ref(branch_name: str) -> str:
+    return f"refs/heads/{branch_name}"
