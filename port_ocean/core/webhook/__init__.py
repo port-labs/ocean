bdcbe74b1d516978377cf470bcb0ac1f3f01ff81
Shalev Avhar <shalev@getport.io>
2025-01-19 15:18:10 +0200

    feat: empty __init__.py file to handle imports

--- port_ocean/core/webhook/__init__.py
+++
@@ -1,4 +0,0 @@
-from port_ocean.core.webhook.abstract_webhook_handler import AbstractWebhookHandler
-from port_ocean.core.webhook.handler_manager import WebhookHandlerManager
-
-__all__ = ["AbstractWebhookHandler", "WebhookHandlerManager"]
