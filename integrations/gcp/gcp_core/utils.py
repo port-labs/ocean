b7b37534a2ca4b7375f6a6d2dadc54458b8a90ca
Iyanuoluwa Adebayo <ioluwadunsinadebayo@gmail.com>
2025-01-20 10:39:27 +0100

    Fix for CI

--- integrations/gcp/gcp_core/utils.py
+++ integrations/gcp/gcp_core/utils.py
@@ -253,7 +253,7 @@ async def get_quotas_for_project(
 
 
 async def resolve_request_controllers(
-    kind: str, **kwargs: Dict[str, Any]
+    kind: str, **kwargs: dict[str, Any]
 ) -> Tuple["AsyncLimiter", "BoundedSemaphore"]:
     service_account_project_id = get_service_account_project_id()
     return await get_quotas_for_project(
