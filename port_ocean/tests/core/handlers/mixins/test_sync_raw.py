60b891177477af6f06f6bd72bd29f6ec666f2aeb
yaelibarg <yaelig@getport.io>
2025-01-20 10:54:22 +0200

    removed saas

--- port_ocean/tests/core/handlers/mixins/test_sync_raw.py
+++ port_ocean/tests/core/handlers/mixins/test_sync_raw.py
@@ -436,6 +436,7 @@ async def test_register_raw(
     mock_ocean: Ocean,
     mock_context: PortOceanContext,
     monkeypatch: pytest.MonkeyPatch,
+    mock_resource_config: ResourceConfig,
 ) -> None:
     kind = "service"
     user_agent_type = UserAgentType.exporter
@@ -702,14 +703,10 @@ class CalculationResult:
 
 
 @pytest.mark.asyncio
-async def test_register_resource_raw_saas_no_changes_upsert_not_called_entitiy_is_returned(
+async def test_register_resource_raw_no_changes_upsert_not_called_entitiy_is_returned(
     mock_sync_raw_mixin: SyncRawMixin,
     mock_port_app_config: PortAppConfig,
-    mock_context: PortOceanContext,
-    monkeypatch: pytest.MonkeyPatch,
 ) -> None:
-    # Mock ocean.app.is_saas()
-    monkeypatch.setattr(mock_context.app, "is_saas", lambda: True)
 
     # Mock dependencies
     entity = Entity(identifier="1", blueprint="service")
@@ -735,14 +732,10 @@ async def test_register_resource_raw_saas_no_changes_upsert_not_called_entitiy_i
 
 
 @pytest.mark.asyncio
-async def test_register_resource_raw_saas_with_changes_upsert_called_and_entities_are_mapped(
+async def test_register_resource_raw_with_changes_upsert_called_and_entities_are_mapped(
     mock_sync_raw_mixin: SyncRawMixin,
     mock_port_app_config: PortAppConfig,
-    mock_context: PortOceanContext,
-    monkeypatch: pytest.MonkeyPatch,
 ) -> None:
-    # Mock ocean.app.is_saas()
-    monkeypatch.setattr(mock_context.app, "is_saas", lambda: True)
 
     # Mock dependencies
     entity = Entity(identifier="1", blueprint="service")
@@ -768,53 +761,9 @@ async def test_register_resource_raw_saas_with_changes_upsert_called_and_entitie
 
 
 @pytest.mark.asyncio
-async def test_register_resource_raw_non_saas_upsert_called_and_no_entitites_diff_calculation(
-    mock_sync_raw_mixin: SyncRawMixin,
-    mock_port_app_config: PortAppConfig,
-    mock_context: PortOceanContext,
-    monkeypatch: pytest.MonkeyPatch,
+async def test_register_resource_raw_with_errors(
+    mock_sync_raw_mixin: SyncRawMixin, mock_port_app_config: PortAppConfig
 ) -> None:
-    # Mock ocean.app.is_saas()
-    monkeypatch.setattr(mock_context.app, "is_saas", lambda: False)
-
-    # Mock dependencies
-    entity = Entity(identifier="1", blueprint="service")
-    calculation_result = CalculationResult(
-        entity_selector_diff=EntitySelectorDiff(passed=[entity], failed=[]),
-        errors=[],
-        misconfigurations=[],
-        misonfigured_entity_keys=[],
-    )
-    mock_sync_raw_mixin._calculate_raw = AsyncMock(return_value=[calculation_result])  # type: ignore
-    mock_sync_raw_mixin._map_entities_compared_with_port = AsyncMock()  # type: ignore
-    mock_sync_raw_mixin.entities_state_applier.upsert = AsyncMock(return_value=[entity])  # type: ignore
-
-    async with event_context(EventType.RESYNC, trigger_type="machine") as event:
-        event.port_app_config = mock_port_app_config
-
-        # Test execution
-        result = await mock_sync_raw_mixin._register_resource_raw(
-            mock_port_app_config.resources[0],
-            [{"some": "data"}],
-            UserAgentType.exporter,
-        )
-
-        # Assertions
-        assert len(result.entity_selector_diff.passed) == 1
-        mock_sync_raw_mixin._calculate_raw.assert_called_once()
-        mock_sync_raw_mixin._map_entities_compared_with_port.assert_not_called()
-        mock_sync_raw_mixin.entities_state_applier.upsert.assert_called_once()
-
-
-@pytest.mark.asyncio
-async def test_register_resource_raw_saas_with_errors(
-    mock_sync_raw_mixin: SyncRawMixin,
-    mock_port_app_config: PortAppConfig,
-    mock_context: PortOceanContext,
-    monkeypatch: pytest.MonkeyPatch,
-) -> None:
-    # Mock ocean.app.is_saas()
-    monkeypatch.setattr(mock_context.app, "is_saas", lambda: True)
 
     # Mock dependencies
     failed_entity = Entity(identifier="1", blueprint="service")
