f01351aa21d678d134f072fe9c8170e8b24f6681
yaelibarg <yaelig@getport.io>
2025-01-23 12:50:41 +0200

    removed reduce load from not saas apps

--- port_ocean/tests/core/handlers/mixins/test_sync_raw.py
+++ port_ocean/tests/core/handlers/mixins/test_sync_raw.py
@@ -500,9 +500,9 @@ async def test_register_raw(
 @pytest.mark.asyncio
 async def test_unregister_raw(
     mock_sync_raw_mixin_with_jq_processor: SyncRawMixin,
-    mock_ocean: Ocean,
     mock_context: PortOceanContext,
     monkeypatch: pytest.MonkeyPatch,
+    mock_resource_config: ResourceConfig,
 ) -> None:
     kind = "service"
     user_agent_type = UserAgentType.exporter
@@ -530,23 +530,7 @@ async def test_unregister_raw(
                 enable_merge_entity=True,
                 delete_dependent_entities=True,
                 create_missing_related_entities=False,
-                resources=[
-                    ResourceConfig(
-                        kind=kind,
-                        selector=Selector(query="true"),
-                        port=PortResourceConfig(
-                            entity=MappingsConfig(
-                                mappings=EntityMapping(
-                                    identifier=".id | tostring",
-                                    title=".name",
-                                    blueprint='"service"',
-                                    properties={"url": ".web_url"},
-                                    relations={},
-                                )
-                            )
-                        ),
-                    )
-                ],
+                resources=[mock_resource_config],
             ),
         ):
             # Ensure the event.port_app_config is set correctly
@@ -790,41 +774,3 @@ async def test_register_resource_raw_with_errors(
             mock_sync_raw_mixin._calculate_raw.assert_called_once()
             mock_sync_raw_mixin._map_entities_compared_with_port.assert_called_once()
             mock_sync_raw_mixin.entities_state_applier.upsert.assert_not_called()
-
-
-@pytest.mark.asyncio
-async def test_register_resource_raw_skip_check_diff_upsert_called_and_no_entitites_diff_calculation(
-    mock_sync_raw_mixin: SyncRawMixin,
-    mock_port_app_config: PortAppConfig,
-    mock_context: PortOceanContext,
-    monkeypatch: pytest.MonkeyPatch,
-) -> None:
-    # Mock ocean.app.is_saas()
-    with patch.object(ocean.config.integration, "skip_check_diff", True):
-        # Mock dependencies
-        entity = Entity(identifier="1", blueprint="service")
-        calculation_result = CalculationResult(
-            entity_selector_diff=EntitySelectorDiff(passed=[entity], failed=[]),
-            errors=[],
-            misconfigurations=[],
-            misonfigured_entity_keys=[],
-        )
-        mock_sync_raw_mixin._calculate_raw = AsyncMock(return_value=[calculation_result])  # type: ignore
-        mock_sync_raw_mixin._map_entities_compared_with_port = AsyncMock()  # type: ignore
-        mock_sync_raw_mixin.entities_state_applier.upsert = AsyncMock(return_value=[entity])  # type: ignore
-
-        async with event_context(EventType.RESYNC, trigger_type="machine") as event:
-            event.port_app_config = mock_port_app_config
-
-            # Test execution
-            result = await mock_sync_raw_mixin._register_resource_raw(
-                mock_port_app_config.resources[0],
-                [{"some": "data"}],
-                UserAgentType.exporter,
-            )
-
-            # Assertions
-            assert len(result.entity_selector_diff.passed) == 1
-            mock_sync_raw_mixin._calculate_raw.assert_called_once()
-            mock_sync_raw_mixin._map_entities_compared_with_port.assert_not_called()
-            mock_sync_raw_mixin.entities_state_applier.upsert.assert_called_once()
