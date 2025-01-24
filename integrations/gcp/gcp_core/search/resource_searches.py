1ad4675bb3d1c7a0e19ad86aa87936e779a8093d
Iyanuoluwa Adebayo <ioluwadunsinadebayo@gmail.com>
2025-01-24 18:56:40 +0100

    Update resource_searches.py

--- integrations/gcp/gcp_core/search/resource_searches.py
+++ integrations/gcp/gcp_core/search/resource_searches.py
@@ -178,7 +178,7 @@ async def list_all_subscriptions_per_project(
 
 
 @cache_iterator_result()
-async def search_all_projects(rate_limiter) -> ASYNC_GENERATOR_RESYNC_TYPE:
+async def search_all_projects(rate_limiter: AsyncLimiter) -> ASYNC_GENERATOR_RESYNC_TYPE:
     logger.info("Searching projects")
     async with ProjectsAsyncClient() as projects_client:
         async for projects in paginated_query(
