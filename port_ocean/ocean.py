f56ca7a282ac775aadd71633ceec7a6183c0d3c5
Shalev Avhar <shalev@getport.io>
2025-01-27 09:56:43 +0200

    refactor: rename to processor

--- port_ocean/ocean.py
+++ port_ocean/ocean.py
@@ -26,7 +26,7 @@ from port_ocean.utils.repeat import repeat_every
 from port_ocean.utils.signal import signal_handler
 from port_ocean.version import __integration_version__
 from port_ocean.utils.misc import IntegrationStateStatus
-from port_ocean.core.handlers.webhook.handler_manager import WebhookHandlerManager
+from port_ocean.core.handlers.webhook.processor_manager import WebhookProcessorManager
 
 
 class Ocean:
@@ -54,7 +54,7 @@ class Ocean:
         )
         self.integration_router = integration_router or APIRouter()
 
-        self.webhook_manager = WebhookHandlerManager(
+        self.webhook_manager = WebhookProcessorManager(
             self.integration_router, signal_handler
         )
 
