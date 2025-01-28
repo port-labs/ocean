75ee1594ce411790e0fc4227b823c50a426a0594
yaelibarg <yaelig@getport.io>
2025-01-28 11:41:58 +0200

    fix lint

--- port_ocean/exceptions/api.py
+++ port_ocean/exceptions/api.py
@@ -18,5 +18,5 @@ class InternalServerException(BaseAPIException):
 class EmptyPortAppConfigError(Exception):
     """Exception raised when the Port app configuration is empty."""
 
-    def __init__(self, message="Port app config is empty"):
+    def __init__(self, message: str = "Port app config is empty") -> None:
         super().__init__(message)
