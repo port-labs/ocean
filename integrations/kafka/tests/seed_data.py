0a259026b8b87d9bd90a456fefcf1ab292b1562f
hpal <ahosea15@gmail.com>
2025-01-31 10:11:00 +0000

    Update seed_data.py

--- integrations/kafka/tests/seed_data.py
+++ integrations/kafka/tests/seed_data.py
@@ -104,9 +104,9 @@ class KafkaLoadTest:
 
 KAFKA_SERVERS_ARRAY = [
     "localhost:19092",
-    # "localhost:9092",
-    # "localhost:9093",
-    # "localhost:9094"
+    "localhost:9092",
+    "localhost:9093",
+    "localhost:9094"
 ]
 # Configuration
 NUM_TOPICS = 50
