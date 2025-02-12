67a41d21ace23d0ceaa9904a17fc9899853c0f5d
yaelibarg <yaelig@getport.io>
2025-02-12 09:23:06 +0200

    deleted not needed change

--- port_ocean/core/integrations/mixins/__init__.py
+++ port_ocean/core/integrations/mixins/__init__.py
@@ -1,13 +1,11 @@
 from .events import EventsMixin
 from .handler import HandlerMixin
-from .live_events import LiveEventsMixin
 from .sync import SyncMixin
 from .sync_raw import SyncRawMixin
 
 __all__ = [
     "EventsMixin",
     "HandlerMixin",
-    "LiveEventsMixin",
     "SyncRawMixin",
     "SyncMixin",
 ]
