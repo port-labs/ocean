ef8c21bfc36368fc6f97986d2ad8d4f09c8c11b5
Sergei Pristavski <sergei.pristavski@cyberark.com>
2025-02-26 14:01:10 +0200

    add scheduled_resync_interval option

--- deployment/terraform/aws/ecs/modules/ecs_service/variables.tf
+++ deployment/terraform/aws/ecs/modules/ecs_service/variables.tf
@@ -94,6 +94,12 @@ variable "initialize_port_resources" {
   default = false
 }
 
+variable "scheduled_resync_interval" {
+  type        = number
+  default     = 1440
+  description = "The interval to resync the integration (in minutes)"
+}
+
 variable "integration" {
   type = object({
     identifier = optional(string)
@@ -106,6 +112,7 @@ variable "lb_targ_group_arn" {
   type    = string
   default = ""
 }
+
 variable "additional_policy_statements" {
   type = list(object({
     actions   = list(string)
