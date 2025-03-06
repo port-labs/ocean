a7441f421629bce7cb2419d85a919be8e9a3b248
mkarmah <mikeyarmah@gmail.com>
2025-03-06 23:06:09 +0000

    lint and clean code

---
+++ integrations/bitbucket/bitbucket_integration/webhook/processors/_base.py
@@ -0,0 +1,15 @@
+from port_ocean.core.handlers.webhook.abstract_webhook_processor import (
+    AbstractWebhookProcessor,
+)
+from initialize_client import init_webhook_client
+from port_ocean.core.handlers.webhook.webhook_event import EventHeaders, EventPayload
+
+
+class _BaseWebhookProcessorConfig(AbstractWebhookProcessor):
+
+    _webhook_client = init_webhook_client()
+
+    async def authenticate(self, payload: EventPayload, headers: EventHeaders) -> bool:
+        return await self._webhook_client.authenticate_incoming_webhook(
+            payload, headers
+        )
