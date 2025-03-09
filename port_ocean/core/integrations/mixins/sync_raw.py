cda3c654567d1d3cdab6a242d3ab3276b9a068da
Shalev Avhar <shalev@getport.io>
2025-03-09 17:59:13 +0200

    feat: add log

--- port_ocean/core/integrations/mixins/sync_raw.py
+++ port_ocean/core/integrations/mixins/sync_raw.py
@@ -169,6 +169,7 @@ class SyncRawMixin(HandlerMixin, EventsMixin):
 
         MIN_ENTITIES_TO_MAP = 10
         if len(entities) <= MIN_ENTITIES_TO_MAP:
+            logger.info("Avoid searching in port since entities amount is low", port_entities=len(entities))
             return entities
 
         BATCH_SIZE = 50
