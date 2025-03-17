f73660a43c042ac9ca4208c526d47ad4c17caf59
shariff-6 <mohammed.s@getport.io>
2025-03-17 13:13:32 +0300

    Fixes lint

--- integrations/gitlab-v2/main.py
+++ integrations/gitlab-v2/main.py
@@ -37,7 +37,11 @@ async def on_resync_projects(kind: str) -> ASYNC_GENERATOR_RESYNC_TYPE:
         logger.info(f"Received project batch with {len(projects_batch)} projects")
         if selector.include_labels:
             for project in projects_batch:
-                project["__labels"] = project["labels"]["nodes"] if "labels" in project and "nodes" in project["labels"] else []
+                project["__labels"] = (
+                    project["labels"]["nodes"]
+                    if "labels" in project and "nodes" in project["labels"]
+                    else []
+                )
         yield projects_batch
 
 
