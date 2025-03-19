07f423acb6611204088144a97c580058ce6c8148
Iyanuoluwa Adebayo <ioluwadunsinadebayo@gmail.com>
2025-03-19 14:02:36 +0100

    Fix test

--- integrations/bitbucket-cloud/tests/test_client.py
+++ integrations/bitbucket-cloud/tests/test_client.py
@@ -274,5 +274,5 @@ async def test_get_pull_requests(mock_client: BitbucketClient) -> None:
                 assert prs == mock_data["values"]
             mock_paginated.assert_called_once_with(
                 f"{mock_client.base_url}/repositories/{mock_client.workspace}/test-repo/pullrequests",
-                params={"state": "OPEN", "pagelen": 100},
+                params={"state": "OPEN", "pagelen": 50},
             )
