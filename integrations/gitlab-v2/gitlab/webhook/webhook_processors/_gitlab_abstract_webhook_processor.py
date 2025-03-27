9c92e3c2ad6c3863ef120fe0d6bc559de63b0876
mkarmah <mikeyarmah@gmail.com>
2025-03-27 16:11:55 +0000

    Bitbucket Live Events

---
+++ integrations/gitlab-v2/gitlab/webhook/webhook_processors/_gitlab_abstract_webhook_processor.py
@@ -0,0 +1,27 @@
+from port_ocean.core.handlers.webhook.abstract_webhook_processor import (
+    AbstractWebhookProcessor,
+)
+from port_ocean.core.handlers.webhook.webhook_event import (
+    EventHeaders,
+    EventPayload,
+    WebhookEvent,
+)
+from gitlab.clients.client_factory import create_gitlab_client
+
+
+class _GitlabAbstractWebhookProcessor(AbstractWebhookProcessor):
+    events: list[str]
+    hook: str
+
+    _gitlab_webhook_client = create_gitlab_client()
+
+    async def authenticate(self, payload: EventPayload, headers: EventHeaders) -> bool:
+        return True
+
+    async def should_process_event(self, event: WebhookEvent) -> bool:
+        event_name = str(
+            event.payload.get("event_name") or event.payload.get("event_type")
+        )
+        return bool(
+            self.hook == event.headers["x-gitlab-event"] and event_name in self.events
+        )
