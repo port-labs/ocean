6bb663ad87c5afd1f7aa8e5f5e4b08b143c04658
Niv Meshorer <nivm@getport.io>
2025-04-02 15:17:06 +0300

    return the metrics array without assign elements to another array

--- integrations/github-copilot/parsers/copilot_metrics_parser.py
+++ integrations/github-copilot/parsers/copilot_metrics_parser.py
@@ -1,9 +1,7 @@
 from typing import Any
 
 def parse(metrics: list[dict[str, Any]], teamSlug: str, organization: str) -> list[dict[Any, Any]]:
-    parsed_data = []
     for metric in metrics:
         metric["team"] = teamSlug
         metric["org"] = organization
-        parsed_data.append(metric)
-    return parsed_data
+    return metrics
