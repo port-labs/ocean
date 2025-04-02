6e264be422c76ca7dd362dd19f5c9e2a0bd26f32
Niv Meshorer <nivm@getport.io>
2025-04-02 14:04:27 +0300

    integration + configurations + blueprints

--- integrations/github-copilot/main.py
+++ integrations/github-copilot/main.py
@@ -2,61 +2,40 @@ from typing import Any
 from enum import StrEnum
 
 from port_ocean.context.ocean import ocean
+from clients.client_factory import create_github_client
+from clients.github_endpoints import GithubEndpoints
+from parsers.copilot_metrics_parser import parse
+from port_ocean.context.ocean import ocean
 
 class ObjectKind(StrEnum):
     COPILOT_TEAM_METRICS = "copilot-team-metrics"
     COPILOT_ORGANIZATION_METRICS = "copilot-organization-metrics"
 
-# Required
-# Listen to the resync event of all the kinds specified in the mapping inside port.
-# Called each time with a different kind that should be returned from the source system.
 @ocean.on_resync(ObjectKind.COPILOT_TEAM_METRICS)
 async def on_resync_copilot_team_metrics(kind: str) -> list[dict[Any, Any]]:
-    # 1. Get all data from the source system
-    # 2. Return a list of dictionaries with the raw data of the state to run the core logic of the framework for
-    # Example:
-    # if kind == "project":
-    #     return [{"some_project_key": "someProjectValue", ...}]
-    # if kind == "issues":
-    #     return [{"some_issue_key": "someIssueValue", ...}]
-
-    # Initial stub to show complete flow, replace this with your own logic
-    if kind == "github-copilot-example-kind":
-        return [
-            {
-                "my_custom_id": f"id_{x}",
-                "my_custom_text": f"very long text with {x} in it",
-                "my_special_score": x * 32 % 3,
-                "my_component": f"component-{x}",
-                "my_service": f"service-{x %2}",
-                "my_enum": "VALID" if x % 2 == 0 else "FAILED",
-            }
-            for x in range(25)
-        ]
-
-    return []
+   github_client = create_github_client()
+   result = []
+   orgs = ocean.integration_config["github_orgs"].split(",") if ocean.integration_config["github_orgs"] else await github_client.get_paginated_data(GithubEndpoints.LIST_TOKEN_ORGS)
+   for org in orgs:
+       teams = await github_client.get_paginated_data(GithubEndpoints.LIST_TEAMS, {"org": org["login"]}, ignore_status_code=[403])
+       if teams:
+        for team in teams:
+           metrics = await github_client.send_api_request_with_route_params('get', GithubEndpoints.COPILOT_TEAM_METRICS, {"org": org["login"], "team": team["slug"]}, ignore_status_code=[422,403])
+           parsed_data = parse(metrics, team["slug"], org["login"])
+           result.extend(parsed_data)
+   return result
 
 @ocean.on_resync(ObjectKind.COPILOT_ORGANIZATION_METRICS)
 async def on_resync_copilot_organization_metrics(kind: str) -> list[dict[Any, Any]]:
-    if kind == "github-copilot-example-kind":
-        return [
-            {
-                "my_custom_id": f"id_{x}",
-                "my_custom_text": f"very long text with {x} in it",
-                "my_special_score": x * 32 % 3,
-                "my_component": f"component-{x}",
-                "my_service": f"service-{x %2}",
-                "my_enum": "VALID" if x % 2 == 0 else "FAILED",
-            }
-            for x in range(25)
-        ]
+   github_client = create_github_client()
+   result = []
+   orgs = ocean.integration_config["github_orgs"].split(",") if ocean.integration_config["github_orgs"] else await github_client.get_paginated_data(GithubEndpoints.LIST_TOKEN_ORGS)
+   for org in orgs:
+       metrics = await github_client.send_api_request_with_route_params('get', GithubEndpoints.COPILOT_ORGANIZATION_METRICS, {"org": org["login"]}, ignore_status_code=[422,403])
+       parsed_data = parse(metrics, None, org["login"])
+       result.extend(parsed_data)
+   return result
 
-    return []
-
-# Optional
-# Listen to the start event of the integration. Called once when the integration starts.
 @ocean.on_start()
 async def on_start() -> None:
-    # Something to do when the integration starts
-    # For example create a client to query 3rd party services - GitHub, Jira, etc...
     print("Starting github-copilot integration")
