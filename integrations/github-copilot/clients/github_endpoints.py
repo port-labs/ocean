dae6bced4e8fd01cda4e18cbaac6cd8792b11c82
Niv Meshorer <nivm@getport.io>
2025-04-03 16:36:37 +0300

    change organizations endpoint name to be more indicative

--- integrations/github-copilot/clients/github_endpoints.py
+++ integrations/github-copilot/clients/github_endpoints.py
@@ -3,5 +3,5 @@ from enum import Enum
 class GithubEndpoints(Enum):
     COPILOT_TEAM_METRICS = "orgs/{org}/team/{team}/copilot/metrics"
     COPILOT_ORGANIZATION_METRICS = "orgs/{org}/copilot/metrics"
-    LIST_TOKEN_ORGS = "user/orgs"
+    LIST_ACCESSIBLE_ORGS = "user/orgs"
     LIST_TEAMS = "orgs/{org}/teams"
