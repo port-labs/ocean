fedf9dc42a936d1de8e3c5721f0a7c188feef398
yotamdekel <yotam@komodor.io>
2025-04-07 12:20:04 +0300

    Removed unused

--- integrations/komodor/client.py
+++ integrations/komodor/client.py
@@ -3,8 +3,6 @@ from typing import Optional, Any, AsyncGenerator
 from loguru import logger
 from port_ocean.utils import http_async_client
 
-from models import IssueRequestBody, IssueScope
-
 # The page size may differ between entities base on their potential size
 DEFAULT_PAGE_SIZE = 100
 SERVICES_PAGE_SIZE = 25 # The service page size is smaller due to the potential extra data in labels and annotations
@@ -67,66 +65,3 @@ class KomodorClient:
                 logger.debug("No more health risks pages, breaking")
                 break
             offset += RISKS_PAGE_SIZE
-
-    async def _get_clusters(self) -> AsyncGenerator[list[dict[str, Any]], Any]:
-        response = await self._send_request(url=f"{self.api_url}/clusters")
-        yield response.get("data", {}).get("clusters", [])
-
-
-    async def _get_risks(self) -> AsyncGenerator[list[dict[str, Any]], None]:
-        offset = 0
-        while True:
-            response = await self._send_request(url=f"{self.api_url}/health/risks",
-                                                params={"pageSize": RISKS_PAGE_SIZE, "offset": offset,
-                                                        "checkCategory": ["workload",
-                                                                          "infrastructure"],
-                                                        "impactGroupType": ["dynamic"]})
-            yield response.get("violations", [])
-
-            if not response.get("hasMoreResults"):
-                logger.debug("No more health risks pages, breaking")
-                break
-            offset += RISKS_PAGE_SIZE
-
-    async def _get_issues(self) -> AsyncGenerator[list[dict[str, Any]], Any]:
-        offset = 0
-        while True:
-            response = await self._send_request(url=f"{self.api_url}/health/risks",
-                                                params={"pageSize:": RISKS_PAGE_SIZE, "offset": offset,
-                                                        "checkType": ["unhealthyService"],
-                                                        "impactGroupType": ["realtime"],
-                                                        "checkCategory": ["workload"]})
-            yield response.get("violations", [])
-            if not response.get("hasMoreResults"):
-                logger.debug("No more health issues pages, breaking")
-                break
-            offset += RISKS_PAGE_SIZE
-
-    async def _get_issues_from_cluster(self, clusters: list[dict[str, Any]]) -> AsyncGenerator[
-        list[dict[str, Any]] | None, Any]:
-        for cluster in clusters:
-            cluster_name = cluster.get("name", None)
-            if cluster_name is None:
-                logger.error("Required cluster name parameter is missing")
-                yield None
-                continue
-
-            body = IssueRequestBody(scope=IssueScope(cluster=cluster_name)).dict()
-            current_page = 0
-            while True:
-                body["pagination"] = {"pageSize": DEFAULT_PAGE_SIZE, "page": current_page}
-                response = await self._send_request(url=f"{self.api_url}/clusters/issues/search", data=body,
-                                               method="POST")
-                issues = response.get("data", {}).get("issues", [])
-                if issues is None:
-                    yield None
-                    continue
-
-                # enrich issue with cluster name
-                for issue in issues:
-                    issue["_clusterName"] = cluster_name
-                yield issues
-                current_page = response.get("meta", {}).get("nextPage", None)
-                if not current_page:
-                    logger.debug("No more issues pages, breaking")
-                    break
--- integrations/komodor/models.py
+++ integrations/komodor/models.py
@@ -2,20 +2,6 @@ from enum import StrEnum
 
 from pydantic import BaseModel
 
-
-class IssueScope(BaseModel):
-    cluster: str
-
-
-class IssueProps(BaseModel):
-    type: str = "availability"
-    statuses: list[str] = ["open", "closed"]
-
-
-class IssueRequestBody(BaseModel):
-    scope: IssueScope
-    props: IssueProps = IssueProps()
-
 class KomoObjectKind(StrEnum):
     SERVICE = "komodorService"
     HEALTH_MONITOR = "komodorHealthMonitoring"
