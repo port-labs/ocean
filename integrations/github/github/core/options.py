6f3bccc4a338e376fbfaba5ee155fd59341d8614
Melody Daniel <melodyogonna@gmail.com>
2025-05-21 13:28:01 +0100

    Don't use the same option for workflow and workflow runs

--- integrations/github/github/core/options.py
+++ integrations/github/github/core/options.py
@@ -7,9 +7,29 @@ class SingleRepositoryOptions(TypedDict):
     name: str
 
 
-class ListWorkflowOptions(TypedDict):
+class RepoOptions(TypedDict):
+    """Base options requiring a repository name."""
+
     repo: str
 
 
-class SingleWorkflowOptions(ListWorkflowOptions):
+class SingleRepoResourceOptions(RepoOptions):
+    """Base options requiring a repository and a resource ID."""
+
     resource_id: str
+
+
+class ListWorkflowOptions(RepoOptions):
+    """Options for listing workflows within a repository."""
+
+
+class SingleWorkflowOptions(SingleRepoResourceOptions):
+    """Options for fetching a single workflow within a repository."""
+
+
+class ListWorkflowRunOptions(RepoOptions):
+    """Options for listing workflow runs within a repository."""
+
+
+class SingleWorkflowRunOptions(SingleRepoResourceOptions):
+    """Options for fetching a single workflow run within a repository."""
