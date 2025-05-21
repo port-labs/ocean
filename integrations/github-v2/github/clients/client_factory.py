cb4742954592cb4b9338bae122f856edab9521e9
Melody Daniel <melodyogonna@gmail.com>
2025-05-21 12:05:14 +0100

    Clean client factory

--- integrations/github-v2/github/clients/client_factory.py
+++ integrations/github-v2/github/clients/client_factory.py
@@ -1,4 +1,5 @@
 import base64
+from typing import Any
 
 from github.clients.app_client import GithubAppRestClient
 from github.clients.rest_client import GithubRestClient
@@ -16,6 +17,27 @@ class GithubClientFactory:
             cls._instance = super(GithubClientFactory, cls).__new__(cls)
         return cls._instance
 
+    @property
+    def ocean_config(self) -> dict[str, Any]:
+        app_id = ocean.integration_config.get("app_id")
+        app_private_key = ocean.integration_config.get("app_private_key")
+        token = ocean.integration_config.get("github_token")
+        config = {
+            "organization": ocean.integration_config["github_organization"],
+            "github_host": ocean.integration_config["github_host"],
+        }
+        if app_id:
+            config["app_id"] = app_id
+        if app_private_key:
+            config["app_private_key"] = app_private_key
+        if token:
+            config["token"] = token
+
+        return config
+
+    def app_configured(self) -> bool:
+        return "app_id" in self.ocean_config and "app_private_key" in self.ocean_config
+
     async def get_client(self, client_type: str) -> AbstractGithubClient:
         """Get or create a client instance from Ocean configuration.
 
@@ -28,44 +50,50 @@ class GithubClientFactory:
         Raises:
             ValueError: If client_type is invalid
         """
-        token = ocean.integration_config.get("github_token")
-        app_id = ocean.integration_config.get("app_id")
-        app_private_key = ocean.integration_config.get("app_private_key")
-        organization = ocean.integration_config["github_organization"]
-        github_host = ocean.integration_config["github_host"]
 
         match client_type:
             case "rest":
-                if app_id is not None and app_private_key is not None:
+                if self.app_configured():
                     logger.info("app_id and private_key detected, using Github App")
-                    if "rest_app" not in self._clients:
-                        decoded_private_key = base64.b64decode(app_private_key)
-                        rest_app = self._clients[
-                            "rest_app"
-                        ] = await GithubAppRestClient(
-                            organization, github_host, app_id, decoded_private_key
-                        ).set_up()
-                        return rest_app
-                    else:
-                        return self._clients["rest_app"]
-                elif token is not None:
+                    return await self._get_app_client()
+                elif "token" in self.ocean_config:
                     logger.info(
                         "Github token found, using Rest Client with Token authentication"
                     )
-                    if "rest_token" not in self._clients:
-                        token_app = self._clients["rest_token"] = GithubRestClient(
-                            token,
-                            organization=organization,
-                            github_host=organization,
-                        )
-                        return token_app
-                    else:
-                        return self._clients["rest_token"]
+                    return self._get_rest_client()
+
                 raise ValueError(
-                    "app_id and app_private_key must be passed in if github_token is not being used."
+                    "app_id and app_private_key must be passed if github_token is not being used."
                 )
             case _:
                 raise ValueError("Unknown client type")
+
+    def _get_rest_client(self) -> AbstractGithubClient:
+        if "rest_token" not in self._clients:
+            token_app = self._clients["rest_token"] = GithubRestClient(
+                self.ocean_config["token"],
+                organization=self.ocean_config["organization"],
+                github_host=self.ocean_config["github_host"],
+            )
+
+            return token_app
+        else:
+            return self._clients["rest_token"]
+
+    async def _get_app_client(self) -> AbstractGithubClient:
+        if "rest_app" not in self._clients:
+            decoded_private_key = base64.b64decode(self.ocean_config["app_private_key"])
+
+            rest_app = self._clients["rest_app"] = await GithubAppRestClient(
+                organization=self.ocean_config["organization"],
+                github_host=self.ocean_config["github_host"],
+                app_id=self.ocean_config["app_id"],
+                private_key=decoded_private_key,
+            ).set_up()
+
+            return rest_app
+        else:
+            return self._clients["rest_app"]
 
 
 async def create_github_client(client_type: str = "rest") -> AbstractGithubClient:
