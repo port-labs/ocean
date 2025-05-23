84d7ac73cf2493779de649a073ede6362723c836
Michael Kofi Armah <mikeyarmah@gmail.com>
2025-05-23 09:32:41 +0000

    Construct headers with just token string

--- integrations/github/github/clients/auth/github_app_authenticator.py
+++ integrations/github/github/clients/auth/github_app_authenticator.py
@@ -39,9 +39,9 @@ class GitHubAppAuthenticator(AbstractGitHubAuthenticator):
         return self.cached_token
 
     async def get_headers(self) -> GitHubHeaders:
-        token = await self.get_token()
+        token_response = await self.get_token()
         return GitHubHeaders(
-            Authorization=f"Bearer {token}",
+            Authorization=f"Bearer {token_response.token}",
             Accept="application/vnd.github+json",
             X_GitHub_Api_Version="2022-11-28",
         )
