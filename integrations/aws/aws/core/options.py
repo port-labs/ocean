79881f793c8c38cc7a39d9fc8d3d3b34494fa823
Michael Kofi Armah <mikeyarmah@gmail.com>
2025-07-03 08:50:55 +0000

    Lint code

--- integrations/aws/aws/core/options.py
+++ integrations/aws/aws/core/options.py
@@ -34,4 +34,16 @@ class ListSQSOptions(ExporterOptions):
     queue_name_prefix: Optional[str]
 
 
-SupportedServices: TypeAlias = Literal["sqs",]
+class SingleResourceGroupOptions(ExporterOptions):
+    group_name: str
+    method_name: str = "get_group"
+    include_resources: bool = True
+
+
+class ListResourceGroupOptions(ExporterOptions):
+    list_param: str = "Groups"
+    method_name: str = "list_groups"
+    include_resources: bool = True
+
+
+SupportedServices: TypeAlias = Literal["sqs", "resource-groups"]
