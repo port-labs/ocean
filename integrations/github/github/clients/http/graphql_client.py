7dd3309341279692793abbce8495768b2ce8db4f
Melody Daniel <melodyogonna@gmail.com>
2025-07-09 17:58:13 +0100

    parse error path with better join key

--- integrations/github/github/clients/http/graphql_client.py
+++ integrations/github/github/clients/http/graphql_client.py
@@ -34,7 +34,7 @@ class GithubGraphQLClient(AbstractGithubClient):
         for error in response["errors"]:
             error_type = error.get("type")
             if error_type in ignored_types:
-                log_message = f"{ignored_types[error_type]} due to {error['message']} for {error['path']} "
+                log_message = f"{ignored_types[error_type]} due to {error['message']} for {'::'.join(error['path'])} "
                 logger.warning(log_message)
                 continue
             non_ignored_exceptions.append(GraphQLClientError(error["message"]))
