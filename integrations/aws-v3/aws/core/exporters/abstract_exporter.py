2e1e1fcecb37710c06cd0304c9c2d8f426845d53
Michael Kofi Armah <mikeyarmah@gmail.com>
2025-07-15 16:27:47 +0000

    Add Declarative Exporters

---
+++ integrations/aws-v3/aws/core/exporters/abstract_exporter.py
@@ -0,0 +1,39 @@
+from abc import ABC, abstractmethod
+from typing import Any, Optional, Self, AsyncGenerator, TYPE_CHECKING
+from aiobotocore.session import AioSession
+from aws.core.options import SupportedServices
+
+if TYPE_CHECKING:
+    from aiobotocore.client import AioBaseClient
+
+
+class AbstractResourceExporter(ABC):
+
+    SERVICE_NAME: SupportedServices
+
+    def __init__(self, session: AioSession, region: str) -> None:
+        self.session = session
+        self.region = region
+        self._client: AioBaseClient | None = None
+
+    async def __aenter__(self) -> Self:
+        self._client = await self.session.create_client(
+            service_name=self.SERVICE_NAME, region_name=self.region
+        ).__aenter__()
+        return self
+
+    async def __aexit__(self, exc_type: Any, exc: Any, tb: Any) -> None:
+        if self._client:
+            await self._client.__aexit__(exc_type, exc, tb)
+
+    @abstractmethod
+    async def get_resource(self, options: Any) -> dict[str, Any]:
+        """Fetch a single resource's detailed data by ID (e.g., ARN, URL, Name)."""
+        ...
+
+    @abstractmethod
+    def get_paginated_resources(
+        self, options: Any
+    ) -> AsyncGenerator[list[dict[str, Any]], None]:
+        """Fetch paginated number of pages of resources."""
+        ...
