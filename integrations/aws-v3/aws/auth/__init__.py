21bbb62658e1a3a9f1578f95c81cb22dc5cf68d6
Shalev Avhar <shalev@getport.io>
2025-07-28 16:42:01 +0300

    feat: add assume role with web identity

--- integrations/aws-v3/aws/auth/__init__.py
+++ integrations/aws-v3/aws/auth/__init__.py
@@ -1,6 +1,9 @@
 from aws.auth.providers.base import CredentialProvider
 from aws.auth.providers.static_provider import StaticCredentialProvider
 from aws.auth.providers.assume_role_provider import AssumeRoleProvider
+from aws.auth.providers.assume_role_with_web_identity_provider import (
+    AssumeRoleWithWebIdentityProvider,
+)
 from aws.auth.session_factory import ResyncStrategyFactory
 from aws.auth.strategies.base import AWSSessionStrategy
 from aws.auth.strategies.single_account_strategy import SingleAccountStrategy
@@ -12,6 +15,7 @@ __all__ = [
     "CredentialProvider",
     "StaticCredentialProvider",
     "AssumeRoleProvider",
+    "AssumeRoleWithWebIdentityProvider",
     "CredentialsProviderError",
     "ResyncStrategyFactory",
     "AWSSessionStrategy",
