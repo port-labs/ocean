a0f3ec283b2b653d06dbd5d7178b9bed70c19bbd
Shalev Avhar <shalev@getport.io>
2025-08-05 08:38:46 +0300

    fix: linting errors

--- integrations/aws-v3/aws/auth/session_factory.py
+++ integrations/aws-v3/aws/auth/session_factory.py
@@ -68,8 +68,6 @@ class ResyncStrategyFactory:
         if cls._cached_strategy is not None:
             return cls._cached_strategy
         config = ocean.integration_config or {}
-        account_role_arn = config.get("account_role_arn")
-        is_multi_account = bool(account_role_arn and len(account_role_arn) > 0)
 
         provider: CredentialProvider
         strategy_cls: type[StrategyType]
