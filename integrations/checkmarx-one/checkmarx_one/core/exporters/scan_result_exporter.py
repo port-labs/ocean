ae87e7970eeb53e217484a673f544c96c7a2f8f8
Michael Kofi Armah <mikeyarmah@gmail.com>
2025-09-04 15:06:14 +0000

    Avoid sending type to the type to the API

--- integrations/checkmarx-one/checkmarx_one/core/exporters/scan_result_exporter.py
+++ integrations/checkmarx-one/checkmarx_one/core/exporters/scan_result_exporter.py
@@ -46,7 +46,7 @@ class CheckmarxScanResultExporter(AbstractCheckmarxExporter):
 
     async def _get_paginated_scan_results(
         self,
-        options: ListScanResultOptions,
+        params: Dict[str, Any],
     ) -> ASYNC_GENERATOR_RESYNC_TYPE:
         """
         Get scan results from Checkmarx One.
@@ -63,13 +63,11 @@ class CheckmarxScanResultExporter(AbstractCheckmarxExporter):
             Batches of scan results
         """
 
-        params: dict[str, Any] = self._get_params(options)
-
         async for results in self.client.send_paginated_request(
             "/results", "results", params
         ):
             logger.info(
-                f"Fetched batch of {len(results)} scan results for scan {options['scan_id']}"
+                f"Fetched batch of {len(results)} scan results for scan {params['scan-id']}"
             )
             yield results
 
@@ -79,7 +77,8 @@ class CheckmarxScanResultExporter(AbstractCheckmarxExporter):
         """
         Get paginated scan results from Checkmarx One.
         """
-        async for results in self._get_paginated_scan_results(options):
+        params: dict[str, Any] = self._get_params(options)
+        async for results in self._get_paginated_scan_results(params):
             batch = [
                 self._enrich_scan_result_with_scan_id(
                     result,
