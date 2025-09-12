87f9e44001d3bbc555c4bc9099bacf5e000ec92b
Melody Daniel <melodyogonna@gmail.com>
2025-09-12 12:05:19 +0100

    fix: Gracefully handle errors in parallel SLO entity fetching

--- integrations/dynatrace/client.py
+++ integrations/dynatrace/client.py
@@ -121,9 +121,15 @@ class DynatraceClient:
         related_slo_tasks = [
             self._get_slo_related_entities(slo["filter"]) for slo in slos
         ]
-        results = await asyncio.gather(*related_slo_tasks)
+        results = await asyncio.gather(*related_slo_tasks, return_exceptions=True)
 
         for slo, entities in zip(slos, results):
+            # AI! can this be improved?
+            if isinstance(entities, Exception):
+                logger.warning(
+                    f"Error {entities} occurred while fetching related entitles for slo {slo['id']}"
+                )
+                continue
             slo["__entities"] = entities
         return slos
 
