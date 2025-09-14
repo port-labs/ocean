edfd5e9ed80643c5ffa7a46aedfd2a323a9d2bbb
50-Course <erioluwaadeodu@my.uopeople.edu>
2025-09-14 20:14:19 +0100

    chore(exceptions): implement custom exceptions for our sailpoint client

--- integrations/sailpoint/sailpoint/exceptions.py
+++ integrations/sailpoint/sailpoint/exceptions.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 class ThirdPartyAPIError(Exception):
     """Base exception for third-party API errors."""
 
@@ -6,6 +9,24 @@ class ThirdPartyAPIError(Exception):
         self.response_code = response_code
         self.response_data = response_data
 
+
 def is_success(status_code: int) -> bool:
     """Check if the status code indicates a successful response."""
     return 200 <= status_code < 300
+
+
+class SailPointAuthError(ThirdPartyAPIError):
+    """Raised for authentication errors with SailPoint API"""
+
+    def __init__(self, response_code: int, response_data: Any) -> None:
+        super().__init__(
+            "Authentication error with SailPoint API", response_code, response_data
+        )
+
+
+class SailPointTokenExpiredError(SailPointAuthError):
+    """Raised when the SailPoint API token has expired"""
+
+    def __init__(self, response_code: int, response_data: Any) -> None:
+        super().__init__(response_code, response_data)
+        self.message = "SailPoint API token has expired"
