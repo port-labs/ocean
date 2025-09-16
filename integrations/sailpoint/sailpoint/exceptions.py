7b9a6ab09d96c34be631ed6e09e2e6f9de2a18cd
50-Course <erioluwaadeodu@my.uopeople.edu>
2025-09-16 03:02:04 +0100

    refactor(exceptions): improve custom error defintitions

--- integrations/sailpoint/sailpoint/exceptions.py
+++ integrations/sailpoint/sailpoint/exceptions.py
@@ -1,11 +1,16 @@
-from typing import Any
+from typing import Any, Optional
 
 
 class ThirdPartyAPIError(Exception):
     """Base exception for third-party API errors."""
-
-    def __init__(self, message: str, response_code: int, response_data: Any) -> None:
+    def __init__(
+        self,
+        message: str,
+        response_code: Optional[int] = None,
+        response_data: Optional[Any] = None,
+    ) -> None:
         super().__init__(message)
+        self.message = message
         self.response_code = response_code
         self.response_data = response_data
 
@@ -18,15 +23,25 @@ def is_success(status_code: int) -> bool:
 class SailPointAuthError(ThirdPartyAPIError):
     """Raised for authentication errors with SailPoint API"""
 
-    def __init__(self, response_code: int, response_data: Any) -> None:
-        super().__init__(
-            "Authentication error with SailPoint API", response_code, response_data
-        )
+    def __init__(
+        self,
+        message: str = "Authentication error with SailPoint API",
+        response_code: Optional[int] = None,
+        response_data: Optional[Any] = None,
+    ) -> None:
+        super().__init__(message, response_code, response_data)
 
 
 class SailPointTokenExpiredError(SailPointAuthError):
     """Raised when the SailPoint API token has expired"""
 
-    def __init__(self, response_code: int, response_data: Any) -> None:
-        super().__init__(response_code, response_data)
-        self.message = "SailPoint API token has expired"
+    def __init__(
+        self,
+        response_code: Optional[int] = None,
+        response_data: Optional[Any] = None,
+    ) -> None:
+        super().__init__(
+            message="SailPoint API token has expired",
+            response_code=response_code,
+            response_data=response_data,
+        )
