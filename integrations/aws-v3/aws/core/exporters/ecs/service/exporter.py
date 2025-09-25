2b2db95feecf26e53c49f49609e7abed2f9b2d3f
shariff-6 <mohammed.s@getport.io>
2025-09-25 13:28:50 +0300

    Refactor EcsServiceExporter to streamline ECS services fetching
    - Removed dependency on EcsClusterExporter and integrated cluster fetching directly within EcsServiceExporter.

--- integrations/aws-v3/aws/core/exporters/ecs/service/exporter.py
+++ integrations/aws-v3/aws/core/exporters/ecs/service/exporter.py
@@ -7,8 +7,6 @@ from aws.core.exporters.ecs.service.models import (
     SingleServiceRequest,
     PaginatedServiceRequest,
 )
-from aws.core.exporters.ecs.cluster.exporter import EcsClusterExporter
-from aws.core.exporters.ecs.cluster.models import PaginatedClusterRequest
 from aws.core.helpers.types import SupportedServices
 from aws.core.interfaces.exporter import IResourceExporter
 from aws.core.modeling.resource_inspector import ResourceInspector
@@ -49,36 +47,29 @@ class EcsServiceExporter(IResourceExporter):
     ) -> AsyncGenerator[list[dict[str, Any]], None]:
         """Fetch all ECS services across all clusters in the region."""
 
-        cluster_exporter = EcsClusterExporter(self.session)
-        cluster_options = PaginatedClusterRequest(
-            region=options.region, account_id=options.account_id, include=[]
-        )
-
         async with AioBaseClientProxy(
             self.session, options.region, self._service_name
         ) as proxy:
             inspector = ResourceInspector(
                 proxy.client, self._actions_map(), lambda: self._model_cls()
             )
-            paginator = proxy.get_paginator("list_services", "serviceArns")
+            services_paginator = proxy.get_paginator("list_services", "serviceArns")
+            clusters_paginator = proxy.get_paginator("list_clusters", "clusterArns")
 
-            async for clusters_batch in cluster_exporter.get_paginated_resources(
-                cluster_options
-            ):
-                for cluster in clusters_batch:
-                    cluster_arn = cluster["Properties"]["ClusterArn"]
-
-                    async for services in paginator.paginate(
-                        cluster=cluster_arn, batch_size=10
-                    ):
-                        if services:
-                            action_result = await inspector.inspect(
-                                services,
-                                options.include,
-                                extra_context={
-                                    "AccountId": options.account_id,
-                                    "Region": options.region,
-                                    "ClusterArn": cluster_arn,
-                                },
-                            )
-                            yield action_result
+            async for cluster_arns in clusters_paginator.paginate():
+                if cluster_arns:
+                    for cluster_arn in cluster_arns:
+                        async for services in services_paginator.paginate(
+                            cluster=cluster_arn, batch_size=10
+                        ):
+                            if services:
+                                action_result = await inspector.inspect(
+                                    services,
+                                    options.include,
+                                    extra_context={
+                                        "AccountId": options.account_id,
+                                        "Region": options.region,
+                                        "ClusterArn": cluster_arn,
+                                    },
+                                )
+                                yield action_result
