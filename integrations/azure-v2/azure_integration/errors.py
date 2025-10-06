bc315e90e0fa719cd2d8f4be8cf28afc22b3079c
Melody Daniel <melodyogonna@gmail.com>
2025-10-06 10:10:48 +0100

    Add error models

---
+++ integrations/azure-v2/azure_integration/errors.py
@@ -0,0 +1,40 @@
+import asyncio
+import random
+from datetime import datetime
+from typing import cast
+
+from azure.core.exceptions import HttpResponseError
+from azure.core.rest import AsyncHttpResponse
+from loguru import logger
+
+
+class SubscriptionLimitReacheached(Exception):
+    pass
+
+
+class AzureRequestThrottled(HttpResponseError):
+    async def handle_delay(self) -> None:
+        if not self.response:
+            return
+        response = cast(AsyncHttpResponse, self.response)
+        self._check_for_subscription_limit(response)
+
+        remaining_quota = response.headers["x-ms-user-quota-remaining"]
+        resets_after = response.headers["x-ms-user-quota-resets-after"]
+
+        if int(remaining_quota) < 1:
+            time_obj = datetime.strptime(resets_after, "%H:%M:%S").time()
+            sleep_duration = (
+                time_obj.hour * 3600 + time_obj.minute * 60 + time_obj.second
+            ) + random.uniform(1, 5)
+            logger.info(
+                f"Azure API quota depleted. Waiting for {sleep_duration:.2f} seconds before retrying."
+            )
+            await asyncio.sleep(sleep_duration)
+
+    def _check_for_subscription_limit(self, response: AsyncHttpResponse) -> None:
+        subscription_limit = response.headers.get("x-ms-tenant-subscription-limit-hit")
+        if subscription_limit and subscription_limit == "true":
+            raise SubscriptionLimitReacheached(
+                "Principal has reached a maximum subsciption limit of 10000"
+            )
