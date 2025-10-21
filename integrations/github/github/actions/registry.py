d1f69ce36ba7eea603c4d9f9adfcc9832ac78ac3
omribarouch <omrib@getport.io>
2025-10-21 15:07:03 +0300

    Removed github integration changes

--- integrations/github/github/actions/registry.py
+++
@@ -1,9 +0,0 @@
-from port_ocean.context.ocean import ocean
-from integrations.github.github.actions.dispatch_workflow_executor import (
-    DispatchWorkflowExecutor,
-)
-
-
-def register_actions_executors():
-    """Register all actions executors."""
-    ocean.register_action_executor(DispatchWorkflowExecutor())
--- integrations/github/github/actions/utils.py
+++
@@ -1,5 +0,0 @@
-from typing import Any
-
-
-def build_external_id(workflow_run: dict[str, Any]) -> str:
-    return f'gh_{workflow_run["repository"]["owner"]["id"]}_{workflow_run["repository"]["id"]}_{workflow_run["id"]}'
