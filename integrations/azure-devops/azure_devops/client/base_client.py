5ac7bc72247ce68979c3b41c71154904e1a2b8bc
Idan Shani <idans@port.io>
2025-10-22 16:58:24 +0300

    fix: fix linter format

--- integrations/azure-devops/azure_devops/client/base_client.py
+++ integrations/azure-devops/azure_devops/client/base_client.py
@@ -37,7 +37,7 @@ class HTTPBaseClient:
         data: Optional[Any] = None,
         params: Optional[dict[str, Any]] = None,
         headers: Optional[dict[str, Any]] = None,
-        timeout : int = 5
+        timeout: int = 5,
     ) -> Response | None:
         self._client.auth = BasicAuth("", self._personal_access_token)
         self._client.follow_redirects = True
@@ -50,7 +50,7 @@ class HTTPBaseClient:
                     data=data,
                     params=params,
                     headers=headers,
-                    timeout=timeout
+                    timeout=timeout,
                 )
                 response.raise_for_status()
         except httpx.HTTPStatusError as e:
