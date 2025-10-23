a73fe2d156bdc65226e0a665699d6215c5a7b3b3
Melody Daniel <melodyogonna@gmail.com>
2025-10-23 07:21:54 +0100

    Fix formatting

--- integrations/github/github/core/exporters/repository_exporter.py
+++ integrations/github/github/core/exporters/repository_exporter.py
@@ -21,9 +21,9 @@ class RestRepositoryExporter(AbstractGithubExporter[GithubRestClient]):
         "teams": "_enrich_repository_with_teams",
     }
 
-    async def get_resource[ExporterOptionsT: SingleRepositoryOptions](
-        self, options: ExporterOptionsT
-    ) -> RAW_ITEM:
+    async def get_resource[
+        ExporterOptionsT: SingleRepositoryOptions
+    ](self, options: ExporterOptionsT) -> RAW_ITEM:
         name = options["name"]
         organization = options["organization"]
         included_relationships = options.get("included_relationships")
@@ -43,9 +43,9 @@ class RestRepositoryExporter(AbstractGithubExporter[GithubRestClient]):
         )
 
     @cache_iterator_result()
-    async def get_paginated_resources[ExporterOptionsT: ListRepositoryOptions](
-        self, options: ExporterOptionsT
-    ) -> ASYNC_GENERATOR_RESYNC_TYPE:
+    async def get_paginated_resources[
+        ExporterOptionsT: ListRepositoryOptions
+    ](self, options: ExporterOptionsT) -> ASYNC_GENERATOR_RESYNC_TYPE:
         """Get all repositories in the organization with pagination."""
         organization = options["organization"]
         included_relationships = options.get("included_relationships")
