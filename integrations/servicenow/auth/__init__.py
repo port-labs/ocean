8083bd8e76290705923e26d38bfaefd45b2be174
Erioluwa Asiru <eri.asiru@port.io>
2025-11-27 17:39:35 +0100

    lint fixes

--- integrations/servicenow/auth/__init__.py
+++ integrations/servicenow/auth/__init__.py
@@ -1,8 +1,10 @@
-from integrations.servicenow.auth.abstract_authenticator import \
-    AbstractServiceNowAuthenticator
+from integrations.servicenow.auth.abstract_authenticator import (
+    AbstractServiceNowAuthenticator,
+)
 from integrations.servicenow.auth.basic_authenticator import BasicAuthenticator
-from integrations.servicenow.auth.oauth_authenticator import \
-    OAuthClientCredentialsAuthenticator
+from integrations.servicenow.auth.oauth_authenticator import (
+    OAuthClientCredentialsAuthenticator,
+)
 
 __all__ = [
     "AbstractServiceNowAuthenticator",
