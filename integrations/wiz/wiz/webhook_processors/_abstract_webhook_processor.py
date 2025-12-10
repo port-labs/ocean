2d8ff60a661a3715f406ac85f4f8d04d9cbd97c3
Dennis Bilson <dennis.bilson@port.io>
2025-12-10 16:08:50 +0000

    Updated issue webhook to handle new selector fields

---
+++ integrations/wiz/wiz/webhook_processors/_abstract_webhook_processor.py
@@ -0,0 +1,39 @@
+from port_ocean.core.handlers.webhook.abstract_webhook_processor import (
+    AbstractWebhookProcessor,
+)
+from port_ocean.core.handlers.webhook.webhook_event import (
+    EventPayload,
+    EventHeaders,
+    WebhookEvent,
+)
+from port_ocean.context.ocean import ocean
+from initialize_client import init_client
+from fastapi import HTTPException
+from typing import Optional
+
+
+class _WizAbstractWebhookProcessor(AbstractWebhookProcessor):
+
+    _client = init_client()
+    _verification_token: Optional[str] = ocean.integration_config[
+        "wiz_webhook_verification_token"
+    ]
+
+    async def authenticate(self, payload: EventPayload, headers: EventHeaders) -> bool:
+        credentials = headers.get("Authorization")
+        if (
+            not credentials
+            or credentials.split(" ")[0] != "Bearer"
+            or self._verification_token != credentials.split(" ")[1]
+        ):
+            raise HTTPException(
+                status_code=401,
+                detail={
+                    "ok": False,
+                    "message": "Wiz webhook token verification failed, ignoring request",
+                },
+            )
+        return True
+
+    async def should_process_event(self, event: WebhookEvent) -> bool:
+        return True
