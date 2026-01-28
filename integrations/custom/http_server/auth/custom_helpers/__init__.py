90371a03b981978040c2136274c224211d6e26f0
Shalev Avhar <shalev@getport.io>
2026-01-28 19:29:55 +0200

    refactor(auth): format import statement for TokenExpirationTracker

--- integrations/custom/http_server/auth/custom_helpers/__init__.py
+++ integrations/custom/http_server/auth/custom_helpers/__init__.py
@@ -2,7 +2,9 @@
 
 from http_server.auth.custom_helpers.lock_manager import LockManager
 from http_server.auth.custom_helpers.template_cache import TemplateCache
-from http_server.auth.custom_helpers.token_expiration_tracker import TokenExpirationTracker
+from http_server.auth.custom_helpers.token_expiration_tracker import (
+    TokenExpirationTracker,
+)
 from http_server.auth.custom_helpers.auth_flow import AuthFlowManager
 
 __all__ = [
