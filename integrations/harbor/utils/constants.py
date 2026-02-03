9cea07e38e02391e9fa2071bc8b5731f723251d3
Emeka Allison <emekaallison4@gmail.com>
2026-02-03 14:40:20 +0100

    [Integration] [Harbor] - generic fixes

--- integrations/harbor/utils/constants.py
+++ integrations/harbor/utils/constants.py
@@ -1,8 +1,8 @@
 from dataclasses import dataclass
 
 
-HARBOUR_API_VERSION = "v2.0"
-HARBOUR_API_BASE_PATH = f"/api/{HARBOUR_API_VERSION}"
+HARBOR_API_VERSION = "v2.0"
+HARBOR_API_BASE_PATH = f"/api/{HARBOR_API_VERSION}"
 
 
 MAX_CONCURRENT_REQUESTS = 10
@@ -12,10 +12,10 @@ REQUEST_TIMEOUT = 2 * 60  # 2 minutes
 
 @dataclass(frozen=True)
 class HarborEndpoints:
-    projects = f"{HARBOUR_API_BASE_PATH}/projects"
-    users = f"{HARBOUR_API_BASE_PATH}/users"
-    repositories = f"{HARBOUR_API_BASE_PATH}/projects/{{project_name}}/repositories"
-    artifacts = f"{HARBOUR_API_BASE_PATH}/projects/{{project_name}}/repositories/{{repository_name}}/artifacts"
+    projects = f"{HARBOR_API_BASE_PATH}/projects"
+    users = f"{HARBOR_API_BASE_PATH}/users"
+    repositories = f"{HARBOR_API_BASE_PATH}/projects/{{project_name}}/repositories"
+    artifacts = f"{HARBOR_API_BASE_PATH}/projects/{{project_name}}/repositories/{{repository_name}}/artifacts"
 
 
 harbor_endpoints = HarborEndpoints()
