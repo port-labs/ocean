30b253b1b4fe4e190c74c4b32c1334dff318a266
jason.nwakaeze <jason.nwakaeze@port.io>
2026-02-08 13:59:59 +0100

    removed from init

--- integrations/gitlab-v2/gitlab/clients/rate_limiter/__init__.py
+++ integrations/gitlab-v2/gitlab/clients/rate_limiter/__init__.py
@@ -4,10 +4,3 @@ from gitlab.clients.rate_limiter.utils import (
     RateLimitInfo,
     RateLimiterRequiredHeaders,
 )
-
-__all__ = [
-    "GitLabRateLimiter",
-    "GitLabRateLimiterConfig",
-    "RateLimitInfo",
-    "RateLimiterRequiredHeaders",
-]
