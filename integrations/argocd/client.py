69179adff40c738e506fa0e0f0395c740c93007b
Quabynah Bilson <dennis.bilson@port.io>
2026-02-11 15:27:23 +0000

    Test out CI impact

--- integrations/argocd/client.py
+++ integrations/argocd/client.py
@@ -56,9 +56,20 @@ class ArgocdClient:
             self.http_client = http_async_client  # type: ignore
         self.http_client.headers.update(self.api_auth_header)
         if custom_http_headers:
-            parsed_headers: dict[str, str] = json.loads(custom_http_headers)
-            logger.debug(f"Applying custom HTTP headers: {list(parsed_headers.keys())}")
-            self.http_client.headers.update(parsed_headers)
+            try:
+                parsed_headers: dict[str, str] = json.loads(custom_http_headers)
+                logger.debug(
+                    f"Applying custom HTTP headers: {list(parsed_headers.keys())}"
+                )
+                self.http_client.headers.update(parsed_headers)
+            except TypeError as e:
+                logger.error(f"Failed to parse custom HTTP headers: {e}")
+                if not self.ignore_server_error:
+                    raise
+            except json.JSONDecodeError as e:
+                logger.error(f"Failed to parse custom HTTP headers: {e}")
+                if not self.ignore_server_error:
+                    raise
         self.streaming_client = StreamingClientWrapper(self.http_client)
         self.use_streaming = use_streaming
 
