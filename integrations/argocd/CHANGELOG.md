e0306d03b321f1b9c9d1a903af0b0240a2a52fc3
Quabynah Bilson <dennis.bilson@port.io>
2026-02-11 00:25:39 +0000

    Bumped version

--- integrations/argocd/CHANGELOG.md
+++ integrations/argocd/CHANGELOG.md
@@ -7,6 +7,14 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 <!-- towncrier release notes start -->
 
+## 0.1.287 (2026-02-11)
+
+
+### Features
+
+- Added support for custom HTTP headers for ArgoCD integration requests
+
+
 ## 0.1.286 (2026-02-02)
 
 
--- integrations/argocd/client.py
+++ integrations/argocd/client.py
@@ -55,6 +55,7 @@ class ArgocdClient:
             self.http_client = http_async_client  # type: ignore
         self.http_client.headers.update(self.api_auth_header)
         if custom_http_headers:
+            logger.debug(f"Applying custom HTTP headers: {custom_http_headers.keys()}")
             self.http_client.headers.update(custom_http_headers)
         self.streaming_client = StreamingClientWrapper(self.http_client)
         self.use_streaming = use_streaming
