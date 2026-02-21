ec73443846ad647198e16db6bf3a5e0c2e43e0c8
omribarouch <omrib@getport.io>
2026-02-21 20:20:56 +0200

    bumped version

--- port_ocean/cli/commands/port_app_config.py
+++ port_ocean/cli/commands/port_app_config.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from typing import Any, Type
 
 import click
-from jsonref import replace_refs
+from jsonref import replace_refs  # type: ignore[import-untyped]
 
 from port_ocean.cli.commands.main import cli_start
 from port_ocean.core.handlers import BasePortAppConfig
