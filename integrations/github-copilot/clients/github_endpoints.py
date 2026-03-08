67169283e37192ca8f1a63666bf0caac05418f21
Eri A. <eri.adeodu@port.io>
2026-03-08 18:40:51 +0100

    Update integrations/github-copilot/clients/github_endpoints.py
    Co-authored-by: Michael Kofi Armah <mikeyarmah@gmail.com>

--- integrations/github-copilot/clients/github_endpoints.py
+++ integrations/github-copilot/clients/github_endpoints.py
@@ -5,7 +5,6 @@ class GithubEndpoints(Enum):
     COPILOT_ORGANIZATION_METRICS_28_DAY = (
         "orgs/{org}/copilot/metrics/reports/organization-28-day/latest"
     )
-
     LIST_ACCESSIBLE_ORGS = "user/orgs"
 
     # NOTE: Sunset April 2026, see https://github.blog/changelog/2026-01-29-closing-down-notice-of-legacy-copilot-metrics-apis/
