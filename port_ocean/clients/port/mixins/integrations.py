8b9b3c4ad01fe2dbb93a61e39b4851e956ecb475
yaelibarg <yaelig@getport.io>
2026-03-08 15:50:15 +0200

    added url encode

--- port_ocean/clients/port/mixins/integrations.py
+++ port_ocean/clients/port/mixins/integrations.py
@@ -283,7 +283,7 @@ class IntegrationClientMixin:
         logger.debug("starting POST raw data request", raw_data=raw_data)
         headers = await self.auth.headers()
         response = await self.client.post(
-            f"{self.auth.ingest_url}/lake/write/integration-type/{self.auth.integration_type}/integration/{self.integration_identifier}/sync/{sync_id}/kind/{kind}",
+            f"{self.auth.ingest_url}/lake/write/integration-type/{quote_plus(self.auth.integration_type)}/integration/{quote_plus(self.integration_identifier)}/sync/{quote_plus(sync_id)}/kind/{quote_plus(kind)}",
             headers=headers,
             json={
                 "items": raw_data,
