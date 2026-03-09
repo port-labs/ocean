e5048b12b5b6c6ab555f73613428bb211fd0726c
Eri A. <eri.adeodu@port.io>
2026-03-09 13:02:39 +0100

    Apply suggestions from code review
    Co-authored-by: Michael Kofi Armah <mikeyarmah@gmail.com>

---
+++ integrations/github-copilot/utils.py
@@ -0,0 +1,32 @@
+from loguru import logger
+from clients.github_client import GitHubClient
+from strategies import (
+    LegacyOrganizationMetricsStrategy,
+    OrganizationUsageMetricsStrategy,
+)
+from port_ocean.core.ocean_types import ASYNC_GENERATOR_RESYNC_TYPE
+
+
+async def resync_organization_metrics(
+    github_client: GitHubClient,
+    strategy: OrganizationUsageMetricsStrategy | LegacyOrganizationMetricsStrategy,
+) -> ASYNC_GENERATOR_RESYNC_TYPE:
+    async for organizations_batch in github_client.get_organizations():
+        for organization in organizations_batch:
+            organization_metrics = await strategy.fetch_metrics(
+                github_client, organization
+            )
+
+            if not organization_metrics:
+                continue
+
+            strategy._enrich_metrics_with_organization(
+                organization_metrics, organization
+            )
+
+            for metrics in organization_metrics:
+                record_date = strategy._get_record_date_key(metrics)
+                logger.info(
+                    f"Received metrics of day {record_date} for organization {organization['login']}"
+                )
+            yield organization_metrics
