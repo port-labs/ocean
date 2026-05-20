3ca0688f61409c52eadd9c6002b06fede5f67f9c
jason.nwakaeze <jason.nwakaeze@port.io>
2026-05-20 10:19:16 +0100

    [Integration][Github] Fix asyncio event loop error when repoManagedMapping is enabled

--- integrations/github/github/clients/client_factory.py
+++ integrations/github/github/clients/client_factory.py
@@ -1,3 +1,4 @@
+import os
 from typing import Dict, Type, overload, Literal, Optional
 
 from port_ocean.context.ocean import ocean
@@ -14,6 +15,16 @@ from github.clients.auth.personal_access_token_authenticator import (
 )
 from github.clients.auth.github_app_authenticator import GitHubAppAuthenticator
 from github.helpers.exceptions import MissingCredentials
+
+
+def _reset_clients_after_fork() -> None:
+    """Clear cached clients after fork so children create fresh httpx connections."""
+    for client in GithubClientFactory._instances.values():
+        client.authenticator._http_client = None
+    GithubClientFactory._instances.clear()
+
+
+os.register_at_fork(after_in_child=_reset_clients_after_fork)
 
 
 class GitHubAuthenticatorFactory:
