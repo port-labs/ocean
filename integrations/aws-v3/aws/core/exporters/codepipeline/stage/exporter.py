dc7e2d1c1db80ec5cb3e1751ff6f8b82032bd237
Venfi Oranai <venfioranai@gmail.com>
2026-06-03 11:55:49 +0300

    Paginate pipelines externally rather than bulking them inside action

--- integrations/aws-v3/aws/core/exporters/codepipeline/stage/exporter.py
+++ integrations/aws-v3/aws/core/exporters/codepipeline/stage/exporter.py
@@ -26,7 +26,7 @@ class CodePipelineStageExporter(IResourceExporter):
             inspector = ResourceInspector(
                 proxy.client, self._actions_map(), lambda: self._model_cls()
             )
-            
+
             # Create a resource identifier for the specific stage
             resource_id = {
                 "PipelineName": options.pipeline_name,
@@ -34,7 +34,7 @@ class CodePipelineStageExporter(IResourceExporter):
                 "pipeline_name": options.pipeline_name,
                 "stage_name": options.stage_name,
             }
-            
+
             response = await inspector.inspect(
                 [resource_id], options.include
             )
@@ -50,30 +50,18 @@ class CodePipelineStageExporter(IResourceExporter):
             inspector = ResourceInspector(
                 proxy.client, self._actions_map(), lambda: self._model_cls()
             )
-            
-            # CodePipeline doesn't have built-in pagination for stages since stages are part of pipelines
-            # We need to list all pipelines first, then extract stages
-            try:
-                logger.info(f"Fetching CodePipeline stages from region {options.region}")
-                
-                # Pass an empty list to trigger the ListPipelinesAction
-                # which will discover all pipelines and their stages
-                action_result = await inspector.inspect(
-                    [],  # Empty list triggers pipeline discovery
-                    options.include,
-                    extra_context={
-                        "AccountId": options.account_id,
-                        "Region": options.region,
-                    },
-                )
-                
-                if action_result:
-                    logger.info(f"Found {len(action_result)} CodePipeline stages in region {options.region}")
+
+            paginator = proxy.get_paginator("list_pipelines", "pipelines")
+            async for pipelines in paginator.paginate():
+                if pipelines:
+                    action_result = await inspector.inspect(
+                        pipelines,
+                        options.include,
+                        extra_context={
+                            "AccountId": options.account_id,
+                            "Region": options.region,
+                        },
+                    )
                     yield action_result
                 else:
-                    logger.info(f"No CodePipeline stages found in region {options.region}")
                     yield []
-                    
-            except Exception as e:
-                logger.error(f"Error fetching CodePipeline stages from region {options.region}: {e}")
-                yield []
