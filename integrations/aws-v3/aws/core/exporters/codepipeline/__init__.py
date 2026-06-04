cfd6897542006f038695564ef2bbfef29f92da6a
Venfi Oranai <venfioranai@gmail.com>
2026-06-04 15:16:41 +0300

    Fixed broken syntax

--- integrations/aws-v3/aws/core/exporters/codepipeline/__init__.py
+++ integrations/aws-v3/aws/core/exporters/codepipeline/__init__.py
@@ -2,6 +2,7 @@ from aws.core.exporters.codepipeline.stage import CodePipelineStageExporter
 from aws.core.exporters.codepipeline.stage.models import (
     SingleCodePipelineStageRequest,
     PaginatedCodePipelineStageRequest,
+)
 from aws.core.exporters.codepipeline.pipeline import (
     PipelineExporter,
     SinglePipelineRequest,
