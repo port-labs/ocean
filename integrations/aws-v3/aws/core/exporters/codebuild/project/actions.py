c6d5dd98a7d21fe7dde870dc9108eabc3e9d5889
Venfi Oranai <venfioranai@gmail.com>
2026-06-10 10:26:29 +0300

    Used aliases instead of passing through a giant Dict

--- integrations/aws-v3/aws/core/exporters/codebuild/project/actions.py
+++ integrations/aws-v3/aws/core/exporters/codebuild/project/actions.py
@@ -20,40 +20,7 @@ class GetProjectDetailsAction(Action):
             f"Successfully fetched details for {len(projects)} CodeBuild projects"
         )
 
-        return [
-            {
-                "Arn": project.get("arn", ""),
-                "Artifacts": project.get("artifacts"),
-                "AutoRetryLimit": project.get("autoRetryLimit"),
-                "Badge": project.get("badge"),
-                "BuildBatchConfig": project.get("buildBatchConfig"),
-                "Cache": project.get("cache"),
-                "ConcurrentBuildLimit": project.get("concurrentBuildLimit"),
-                "Created": project.get("created"),
-                "Description": project.get("description"),
-                "EncryptionKey": project.get("encryptionKey"),
-                "Environment": project.get("environment"),
-                "FileSystemLocations": project.get("fileSystemLocations", []),
-                "LastModified": project.get("lastModified"),
-                "LogsConfig": project.get("logsConfig"),
-                "Name": project.get("name", ""),
-                "ProjectVisibility": project.get("projectVisibility"),
-                "PublicProjectAlias": project.get("publicProjectAlias"),
-                "QueuedTimeoutInMinutes": project.get("queuedTimeoutInMinutes"),
-                "ResourceAccessRole": project.get("resourceAccessRole"),
-                "SecondaryArtifacts": project.get("secondaryArtifacts", []),
-                "SecondarySources": project.get("secondarySources", []),
-                "SecondarySourceVersions": project.get("secondarySourceVersions", []),
-                "ServiceRole": project.get("serviceRole"),
-                "Source": project.get("source"),
-                "SourceVersion": project.get("sourceVersion"),
-                "Tags": project.get("tags", []),
-                "TimeoutInMinutes": project.get("timeoutInMinutes"),
-                "VpcConfig": project.get("vpcConfig"),
-                "Webhook": project.get("webhook"),
-            }
-            for project in projects
-        ]
+        return projects
 
 
 class CodeBuildProjectActionsMap(ActionMap):
