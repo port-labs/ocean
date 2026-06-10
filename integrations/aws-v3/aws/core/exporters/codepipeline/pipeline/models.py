3337ab7d0a3395a268c175ba3c03b4a39254ee28
Venfi Oranai <venfioranai@gmail.com>
2026-06-10 11:18:38 +0300

    Alphabetized keys

--- integrations/aws-v3/aws/core/exporters/codepipeline/pipeline/models.py
+++ integrations/aws-v3/aws/core/exporters/codepipeline/pipeline/models.py
@@ -5,20 +5,20 @@ from aws.core.modeling.resource_models import ResourceModel, ResourceRequestMode
 
 
 class PipelineProperties(BaseModel):
-    Name: str = Field(default_factory=str)
     Arn: Optional[str] = None
-    RoleArn: Optional[str] = None
     ArtifactStore: Optional[dict[str, Any]] = None
     ArtifactStores: Dict[str, dict[str, Any]] = Field(default_factory=dict)
-    Stages: List[dict[str, Any]] = Field(default_factory=list)
-    Version: Optional[int] = None
-    ExecutionMode: Optional[str] = None
-    PipelineType: Optional[str] = None
-    Variables: List[Dict[str, Any]] = Field(default_factory=list)
-    Triggers: List[Dict[str, Any]] = Field(default_factory=list)
     Created: Optional[datetime] = None
-    Updated: Optional[datetime] = None
+    ExecutionMode: Optional[str] = None
+    Name: str = Field(default_factory=str)
+    PipelineType: Optional[str] = None
+    RoleArn: Optional[str] = None
+    Stages: List[dict[str, Any]] = Field(default_factory=list)
     Tags: Dict[str, str] = Field(default_factory=dict)
+    Triggers: List[Dict[str, Any]] = Field(default_factory=list)
+    Updated: Optional[datetime] = None
+    Variables: List[Dict[str, Any]] = Field(default_factory=list)
+    Version: Optional[int] = None
 
     class Config:
         extra = "ignore"
