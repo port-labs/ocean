02e492d63eece5da6204c94efc2a3d78fa2fcf41
Venfi Oranai <venfioranai@gmail.com>
2026-06-11 15:57:16 +0300

    Removed redundant action

--- integrations/aws-v3/aws/core/exporters/codebuild/project/actions.py
+++ integrations/aws-v3/aws/core/exporters/codebuild/project/actions.py
@@ -3,13 +3,6 @@ from aws.core.interfaces.action import Action, ActionMap
 from loguru import logger
 
 
-class ListProjectsAction(Action):
-    """Processes the initial list of projects from AWS."""
-
-    async def _execute(self, resources: List[str]) -> List[Dict[str, Any]]:
-        return [{"name": project, "id": project} for project in resources]
-
-
 class GetProjectDetailsAction(Action):
     """Fetches detailed information about CodeBuild projects."""
 
@@ -25,7 +18,6 @@ class CodeBuildProjectActionsMap(ActionMap):
     """Groups all actions for CodeBuild project resource type."""
 
     defaults: List[Type[Action]] = [
-        ListProjectsAction,
         GetProjectDetailsAction,
     ]
     options: List[Type[Action]] = []
