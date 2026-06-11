4ad90675059e854adc7a5faa0b62430d9aad3ad1
Venfi Oranai <venfioranai@gmail.com>
2026-06-11 15:34:54 +0300

    Added action to get tags

--- integrations/aws-v3/aws/core/exporters/codedeploy/deployment_group/actions.py
+++ integrations/aws-v3/aws/core/exporters/codedeploy/deployment_group/actions.py
@@ -21,10 +21,39 @@ class GetDeploymentGroupDetailsAction(Action):
         return results
 
 
+class GetDeploymentGroupTags(Action):
+    async def _execute(self, groups_data: DeploymentGroupActionInput) -> List[Dict[str, Any]]:
+        tags = await asyncio.gather(
+            *(self._fetch_tags(
+                app_name=groups_data['app_name'],
+                group_name=group,
+                region=groups_data['extras']['region'],
+                account_id=groups_data['extras']['account_id'],
+            ) for group in groups_data['groups']),
+            return_exceptions=True,
+        )
+
+        results: List[Dict[str, Any]] = []
+        for idx, tag_result in enumerate(tags):
+            if isinstance(tag_result, Exception):
+                logger.error(
+                    f"Error fetching tags for DeploymentGroup '{groups_data['app_name']}/{groups_data['groups'][idx]}': {tag_result}"
+                )
+                results.append({})
+                continue
+            results.append(cast(Dict[str, Any], tag_result))
+        return results
+
+    async def _fetch_tags(self, app_name: str, group_name: str, region: str, account_id: str) -> List[Dict[str, Any]]:
+        arn = f"arn:aws:codedeploy:{region}:{account_id}:deploymentgroup:{app_name}/{group_name}"
+        return await self.client.list_tags_for_resource(ResourceArn=arn)
+
+
 class CodeDeployDeploymentGroupActionsMap(ActionMap):
     """Groups all actions for CodeDeploy deployment groups."""
 
     defaults: List[Type[Action]] = [
         GetDeploymentGroupDetailsAction,
+        GetDeploymentGroupTags,
     ]
     options: List[Type[Action]] = []
