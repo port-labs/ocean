d14bbece64350c7a858e7bc95c84ae4bfebae124
Venfi Oranai <venfioranai@gmail.com>
2026-06-15 14:12:49 +0300

    Made batch size a const

--- integrations/aws-v3/aws/core/exporters/codebuild/project/exporter.py
+++ integrations/aws-v3/aws/core/exporters/codebuild/project/exporter.py
@@ -15,6 +15,7 @@ class CodeBuildProjectExporter(IResourceExporter):
     _service_name: SupportedServices = "codebuild"
     _model_cls: Type[CodeBuildProject] = CodeBuildProject
     _actions_map: Type[CodeBuildProjectActionsMap] = CodeBuildProjectActionsMap
+    _pagination_batch_size: int = 100  # Limit defined by AWS's API
 
     async def get_resource(
         self, options: SingleCodeBuildProjectRequest
@@ -39,7 +40,7 @@ class CodeBuildProjectExporter(IResourceExporter):
             )
             paginator = proxy.get_paginator("list_projects", "projects")
 
-            async for projects in paginator.paginate(batch_size=100):
+            async for projects in paginator.paginate(batch_size=self._pagination_batch_size):
                 if projects:
                     action_result = await inspector.inspect(
                         projects,
