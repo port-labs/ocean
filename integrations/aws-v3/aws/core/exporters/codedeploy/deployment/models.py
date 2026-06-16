c44f1f5c7775e86d89c1e7c351a9eb0ea1cf5ede
Venfi Oranai <venfioranai@gmail.com>
2026-06-16 12:09:30 +0300

    Matched to api

--- integrations/aws-v3/aws/core/exporters/codedeploy/deployment/models.py
+++ integrations/aws-v3/aws/core/exporters/codedeploy/deployment/models.py
@@ -7,34 +7,40 @@ from aws.core.modeling.resource_models import ResourceModel, ResourceRequestMode
 class CodeDeployDeploymentProperties(BaseModel):
     """Properties for a CodeDeploy Deployment resource."""
 
+    AdditionalDeploymentStatusInfo: Optional[str] = None
     ApplicationName: Optional[str] = None
+    AutoRollbackConfiguration: Optional[Dict[str, Any]] = None
+    BlueGreenDeploymentConfiguration: Optional[Dict[str, Any]] = None
+    CompleteTime: Optional[datetime] = None
+    ComputePlatform: Optional[str] = None
+    CreateTime: Optional[datetime] = None
+    Creator: Optional[str] = None
+    DeploymentConfigName: Optional[str] = None
     DeploymentGroupName: Optional[str] = None
     DeploymentId: str = Field(default_factory=str)
-    Status: Optional[str] = None
-    ErrorInformation: Optional[Dict[str, Any]] = None
-    CreateTime: Optional[datetime] = None
-    StartTime: Optional[datetime] = None
-    CompleteTime: Optional[datetime] = None
     DeploymentOverview: Optional[Dict[str, Any]] = None
-    Description: Optional[str] = None
-    Creator: Optional[str] = None
-    IgnoreApplicationStopFailures: Optional[bool] = None
-    AutoRollbackConfiguration: Optional[Dict[str, Any]] = None
-    UpdateOutdatedInstancesOnly: Optional[bool] = None
-    RollbackInfo: Optional[Dict[str, Any]] = None
+    DeploymentStatusMessages: Optional[List[str]] = None
     DeploymentStyle: Optional[Dict[str, Any]] = None
-    TargetInstances: Optional[Dict[str, Any]] = None
-    InstanceTerminationWaitTimeStarted: Optional[bool] = None
-    BlueGreenDeploymentConfiguration: Optional[Dict[str, Any]] = None
-    LoadBalancerInfo: Optional[Dict[str, Any]] = None
-    AdditionalDeploymentStatusInfo: Optional[str] = None
-    FileExistsBehavior: Optional[str] = None
+    Description: Optional[str] = None
+    ErrorInformation: Optional[Dict[str, Any]] = None
     ExternalId: Optional[str] = None
+    FileExistsBehavior: Optional[str] = None
+    IgnoreApplicationStopFailures: Optional[bool] = None
+    InstanceTerminationWaitTimeStarted: Optional[bool] = None
+    LoadBalancerInfo: Optional[Dict[str, Any]] = None
+    OverrideAlarmConfiguration: Optional[Dict[str, Any]] = None
+    PreviousRevision: Optional[Dict[str, Any]] = None
+    RelatedDeployments: Optional[Dict[str, Any]] = None
     Revision: Optional[Dict[str, Any]] = None
+    RollbackInfo: Optional[Dict[str, Any]] = None
+    StartTime: Optional[datetime] = None
+    Status: Optional[str] = None
+    TargetInstances: Optional[Dict[str, Any]] = None
+    UpdateOutdatedInstancesOnly: Optional[bool] = None
 
     class Config:
         extra = "ignore"
-        allow_population_by_name = True
+        allow_population_by_field_name = True
 
 
 class CodeDeployDeployment(ResourceModel[CodeDeployDeploymentProperties]):
