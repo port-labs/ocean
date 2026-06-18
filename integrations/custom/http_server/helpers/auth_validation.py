a5f714a61b8c4e0eb8943cf0bd81e850cf916d4e
Venfi Oranai <venfioranai@gmail.com>
2026-06-18 15:15:36 +0300

    Revert comment change

--- integrations/custom/http_server/helpers/auth_validation.py
+++ integrations/custom/http_server/helpers/auth_validation.py
@@ -80,7 +80,7 @@ def validate_custom_auth_request_template_config(
                 CustomAuthRequestTemplateConfig, custom_auth_request_template_config
             )
     except CustomAuthRequestTemplateError:
-        # Re-raise CustomAuthRequestTemplateError as-is (from pydantic.v1 validator)
+        # Re-raise CustomAuthRequestTemplateError as-is (from pydantic validator)
         raise
     except Exception as e:
         # Wrap other parsing errors
