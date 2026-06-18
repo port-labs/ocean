1de87410590b6e49d20696455a26fd82d0552715
Venfi Oranai <venfioranai@gmail.com>
2026-06-18 13:45:12 +0300

    Inspector can take optional kwarg to match action results by key rather than index

--- integrations/aws-v3/aws/core/modeling/resource_inspector.py
+++ integrations/aws-v3/aws/core/modeling/resource_inspector.py
@@ -36,6 +36,7 @@ class ResourceInspector[
         client: Any,
         actions_map: ActionMap[ActionInput],
         model_factory: Callable[[], ResourceModelT],
+        action_id_key: str | None = None,
     ) -> None:
         """
         Initialize the ResourceInspector.
@@ -48,6 +49,7 @@ class ResourceInspector[
         self.client = client
         self.actions_map = actions_map
         self.model_factory = model_factory
+        self.action_id_key = action_id_key
 
     async def inspect(
         self,
@@ -77,16 +79,24 @@ class ResourceInspector[
         if not action_results or not any(action_results):
             return []
 
-        resource_data: dict[int, dict[str, Any]] = defaultdict(dict)
-        for action_result in action_results:
-            if isinstance(action_result, Exception):
-                logger.warning(
-                    f"{action_result.__class__.__name__} failed: {action_result}, skipping ..."
-                )
-                continue
-            for idx, item in enumerate(cast(List[Dict[str, Any]], action_result)):
-                if item:
-                    resource_data[idx] |= item
+        resource_data: dict[int | str, dict[str, Any]] = defaultdict(dict)
+        if self.action_id_key:
+            for result_group in action_results:
+                for result in result_group:
+                    if self.action_id_key in result:
+                        resource_data[result[self.action_id_key]] |= result
+                    else:
+                        logger.warning(f"Missing key '{self.action_id_key}' in result: {result}, skipping ...")
+        else:
+            for action_result in action_results:
+                if isinstance(action_result, Exception):
+                    logger.warning(
+                        f"{action_result.__class__.__name__} failed: {action_result}, skipping ..."
+                    )
+                    continue
+                for idx, item in enumerate(cast(List[Dict[str, Any]], action_result)):
+                    if item:
+                        resource_data[idx] |= item
 
         resources = []
         for resource_props in resource_data.values():
