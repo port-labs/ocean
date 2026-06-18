90daee75dca1cec5b63216f9a7ef87e142cce480
Venfi Oranai <venfioranai@gmail.com>
2026-06-18 11:14:49 +0300

    Matched API

--- integrations/aws-v3/aws/core/exporters/codepipeline/action_execution/models.py
+++ integrations/aws-v3/aws/core/exporters/codepipeline/action_execution/models.py
@@ -1,93 +1,21 @@
 from datetime import datetime
-from typing import Optional, Dict, Any, List
+from typing import Any
 from pydantic import BaseModel, Field
 from aws.core.modeling.resource_models import ResourceModel, ResourceRequestModel
 
 
-class ActionTypeIdProperties(BaseModel):
-    category: Optional[str] = Field(default=None, alias="Category")
-    owner: Optional[str] = Field(default=None, alias="Owner")
-    provider: Optional[str] = Field(default=None, alias="Provider")
-    version: Optional[str] = Field(default=None, alias="Version")
-
-    class Config:
-        extra = "ignore"
-        allow_population_by_field_name = True
-
-
-class ActionExecutionInputProperties(BaseModel):
-    actionTypeId: Optional[ActionTypeIdProperties] = Field(
-        default=None, alias="ActionTypeId"
-    )
-    configuration: Optional[Dict[str, str]] = Field(default=None, alias="Configuration")
-    resolvedConfiguration: Optional[Dict[str, str]] = Field(
-        default=None, alias="ResolvedConfiguration"
-    )
-    roleArn: Optional[str] = Field(default=None, alias="RoleArn")
-    region: Optional[str] = Field(default=None, alias="Region")
-    inputArtifacts: Optional[List[Dict[str, Any]]] = Field(
-        default=None, alias="InputArtifacts"
-    )
-    namespace: Optional[str] = Field(default=None, alias="Namespace")
-
-    class Config:
-        extra = "ignore"
-        allow_population_by_field_name = True
-
-
-class ActionExecutionResultProperties(BaseModel):
-    externalExecutionId: Optional[str] = Field(
-        default=None, alias="ExternalExecutionId"
-    )
-    externalExecutionSummary: Optional[str] = Field(
-        default=None, alias="ExternalExecutionSummary"
-    )
-    externalExecutionUrl: Optional[str] = Field(
-        default=None, alias="ExternalExecutionUrl"
-    )
-    errorDetails: Optional[Dict[str, str]] = Field(default=None, alias="ErrorDetails")
-    logStreamARN: Optional[str] = Field(default=None, alias="LogStreamARN")
-
-    class Config:
-        extra = "ignore"
-        allow_population_by_field_name = True
-
-
-class ActionExecutionOutputProperties(BaseModel):
-    outputArtifacts: Optional[List[Dict[str, Any]]] = Field(
-        default=None, alias="OutputArtifacts"
-    )
-    executionResult: Optional[ActionExecutionResultProperties] = Field(
-        default=None, alias="ExecutionResult"
-    )
-    outputVariables: Optional[Dict[str, str]] = Field(
-        default=None, alias="OutputVariables"
-    )
-
-    class Config:
-        extra = "ignore"
-        allow_population_by_field_name = True
-
-
 class CodePipelineActionExecutionProperties(BaseModel):
-    actionExecutionId: Optional[str] = Field(default=None, alias="ActionExecutionId")
-    actionName: Optional[str] = Field(default=None, alias="ActionName")
-    pipelineExecutionId: Optional[str] = Field(
-        default=None, alias="PipelineExecutionId"
-    )
-    pipelineVersion: Optional[int] = Field(default=None, alias="PipelineVersion")
-    pipelineName: Optional[str] = Field(default=None, alias="PipelineName")
-    stageName: Optional[str] = Field(default=None, alias="StageName")
-    startTime: Optional[datetime] = Field(default=None, alias="StartTime")
-    lastUpdateTime: Optional[datetime] = Field(default=None, alias="LastUpdateTime")
-    updatedBy: Optional[str] = Field(default=None, alias="UpdatedBy")
-    status: Optional[str] = Field(default=None, alias="Status")
-    input: Optional[ActionExecutionInputProperties] = Field(
-        default=None, alias="Input"
-    )
-    output: Optional[ActionExecutionOutputProperties] = Field(
-        default=None, alias="Output"
-    )
+    actionExecutionId: str | None = Field(default=None, alias="ActionExecutionId")
+    actionName: str | None = Field(default=None, alias="ActionName")
+    input: dict[str, Any] | None = Field(default=None, alias="Input")
+    lastUpdateTime: datetime | None = Field(default=None, alias="LastUpdateTime")
+    output: dict[str, Any] | None = Field(default=None, alias="Output")
+    pipelineExecutionId: str | None = Field(default=None, alias="PipelineExecutionId")
+    pipelineVersion: int | None = Field(default=None, alias="PipelineVersion")
+    stageName: str | None = Field(default=None, alias="StageName")
+    startTime: datetime | None = Field(default=None, alias="StartTime")
+    status: str | None = Field(default=None, alias="Status")
+    updatedBy: str | None = Field(default=None, alias="UpdatedBy")
 
     class Config:
         extra = "ignore"
