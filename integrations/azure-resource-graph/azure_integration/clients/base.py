3179cb179293fd6516868c50d375c42654c8443a
Venfi Oranai <venfioranai@gmail.com>
2026-06-23 11:56:34 +0300

    Used v1 config method

--- integrations/azure-resource-graph/azure_integration/clients/base.py
+++ integrations/azure-resource-graph/azure_integration/clients/base.py
@@ -3,7 +3,6 @@ from azure.core.credentials_async import AsyncTokenCredential
 from pydantic.v1 import BaseModel, Field
 from typing import List, Optional
 from abc import ABC, abstractmethod
-from pydantic import ConfigDict
 
 
 class AzureRequest(BaseModel):
@@ -16,7 +15,8 @@ class AzureRequest(BaseModel):
     page_size: int = 100
     data_key: str = "data"
 
-    config: ConfigDict = ConfigDict(extra='forbid')
+    class Config:
+        extra = "forbid"
 
 
 class AbstractAzureClient(ABC):
