d09ca15ab8a82cb80354f598056831c634c2233c
Venfi Oranai <venfioranai@gmail.com>
2026-06-28 17:11:06 +0300

    Added utils to fetch regions

--- integrations/aws-v3/aws/utils/location_utils.py
+++ integrations/aws-v3/aws/utils/location_utils.py
@@ -1,11 +1,14 @@
 from typing import cast
 
+from aiobotocore.session import AioSession
+
 from aws import Consts
 from port_ocean.context.ocean import ocean
 
 
 class LocationUtils:
     _partition: str = ''
+    _available_regions: list[str] = []
 
     @classmethod
     def get_partition(cls) -> str:
@@ -18,3 +21,15 @@ class LocationUtils:
                 cls._partition = Consts.default_partition
 
         return cls._partition
+
+    @classmethod
+    async def get_all_available_regions(cls, session: AioSession) -> list[str]:
+        if not cls._available_regions:
+            cls._available_regions = await session.get_available_regions('ec2', partition_name=cls.get_partition())
+
+        return cls._available_regions
+
+    @classmethod
+    async def get_first_available_region(cls, session: AioSession) -> str | None:
+        regions = await cls.get_all_available_regions(session)
+        return regions[0] if regions else None
