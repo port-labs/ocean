ed2e2b302b9e754ba674e68c62a1b6e60aca2093
Venfi Oranai <venfioranai@gmail.com>
2026-06-29 10:59:22 +0300

    Stylers

--- integrations/aws-v3/aws/utils/location_utils.py
+++ integrations/aws-v3/aws/utils/location_utils.py
@@ -7,16 +7,20 @@ from port_ocean.context.ocean import ocean
 
 
 class LocationUtils:
-    _partition: str = ''
+    _partition: str = ""
     _available_regions: list[str] = []
 
     @classmethod
     def get_partition(cls) -> str:
         if not cls._partition:
-            if ocean.integration_config.get('aws_partition'):
-                cls._partition = cast(str, ocean.integration_config.get('aws_partition'))
-            elif ocean.integration_config.get('accountRoleArn'):
-                cls._partition = cast(str, ocean.integration_config.get('accountRoleArn')).split(':')[1]
+            if ocean.integration_config.get("aws_partition"):
+                cls._partition = cast(
+                    str, ocean.integration_config.get("aws_partition")
+                )
+            elif ocean.integration_config.get("accountRoleArn"):
+                cls._partition = cast(
+                    str, ocean.integration_config.get("accountRoleArn")
+                ).split(":")[1]
             else:
                 cls._partition = Consts.default_partition
 
@@ -25,7 +29,9 @@ class LocationUtils:
     @classmethod
     async def get_all_available_regions(cls, session: AioSession) -> list[str]:
         if not cls._available_regions:
-            cls._available_regions = await session.get_available_regions('ec2', partition_name=cls.get_partition())
+            cls._available_regions = await session.get_available_regions(
+                "ec2", partition_name=cls.get_partition()
+            )
 
         return cls._available_regions
 
@@ -35,5 +41,11 @@ class LocationUtils:
         return regions[0] if regions else None
 
     @classmethod
-    async def get_custom_partition_region_or_none(cls, session: AioSession) -> str | None:
-        return None if cls.get_partition() == Consts.default_partition else await cls.get_first_available_region(session)
+    async def get_custom_partition_region_or_none(
+        cls, session: AioSession
+    ) -> str | None:
+        return (
+            None
+            if cls.get_partition() == Consts.default_partition
+            else await cls.get_first_available_region(session)
+        )
