841662db69500c76702d72c7d415c788faf9cd51
Venfi Oranai <venfioranai@gmail.com>
2026-07-01 11:52:17 +0300

    Fix test

--- integrations/aws-v3/tests/utils/region_helper/test_get_partition.py
+++ integrations/aws-v3/tests/utils/region_helper/test_get_partition.py
@@ -1,5 +1,6 @@
 from unittest.mock import MagicMock, patch
 
+from aws.utils import Consts
 from aws.utils.region_helper import RegionHelper
 
 
@@ -8,11 +9,11 @@ class TestGetPartition:
         with patch(
             "aws.utils.region_helper.ocean", new_callable=MagicMock
         ) as mock_ocean:
-            mock_ocean.integration_config.get.side_effect = lambda key: {
+            mock_ocean.integration_config.get.side_effect = lambda key, *args: {
                 "aws_partition": "aws-cn"
             }.get(key)
 
             result = RegionHelper.get_partition()
 
         assert result == "aws-cn"
-        mock_ocean.integration_config.get.assert_called_once_with("aws_partition")
+        mock_ocean.integration_config.get.assert_called_once_with("aws_partition", Consts.default_partition)
