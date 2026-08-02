8011e9be0455f598125342779318cd3f89fbd25e
Venfi Oranai <venfioranai@gmail.com>
2026-08-02 16:02:44 +0300

    Ran black

--- integrations/aws-v3/aws/core/exporters/exporter_metadata.py
+++ integrations/aws-v3/aws/core/exporters/exporter_metadata.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass
 from typing import Any
 
-from aws.core.exporters.codepipeline import CodePipelineActionExecutionExporter, \
-    PaginatedCodePipelineActionExecutionRequest
+from aws.core.exporters.codepipeline import (
+    CodePipelineActionExecutionExporter,
+    PaginatedCodePipelineActionExecutionRequest,
+)
 from aws.core.exporters.s3.bucket.exporter import S3BucketExporter
 from aws.core.exporters.s3.bucket.models import PaginatedBucketRequest
 from aws.core.helpers.types import ObjectKind
@@ -18,7 +20,9 @@ class ExporterMetadata:
 
 
 kind_to_export_metadata: dict[ObjectKind, ExporterMetadata] = {
-    ObjectKind.S3_BUCKET: ExporterMetadata(S3BucketExporter, PaginatedBucketRequest, regional=False),
+    ObjectKind.S3_BUCKET: ExporterMetadata(
+        S3BucketExporter, PaginatedBucketRequest, regional=False
+    ),
     ObjectKind.CODEPIPELINE_ACTION_EXECUTION: ExporterMetadata(
         CodePipelineActionExecutionExporter, PaginatedCodePipelineActionExecutionRequest
     ),
