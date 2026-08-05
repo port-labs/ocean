7f03a85d25721e52209d8631c178541440321bdb
Venfi Oranai <venfioranai@gmail.com>
2026-08-05 20:24:48 +0300

    Uncomment commit

--- scripts/bump-all.sh
+++ scripts/bump-all.sh
@@ -80,8 +80,8 @@ for FOLDER in "${ROOT_DIR}"/integrations/*; do
     echo "Run towncrier build to increment the patch version"
     towncrier build --keep --version "${NEW_VERSION}" && rm changelog/*
     git add poetry.lock pyproject.toml CHANGELOG.md
-#    echo "Committing ${INTEGRATION}"
-#    SKIP="trailing-whitespace,end-of-file-fixer" git commit -m "Bumped ocean version to ${VERSION} for ${INTEGRATION}"
+    echo "Committing ${INTEGRATION}"
+    SKIP="trailing-whitespace,end-of-file-fixer" git commit -m "Bumped ocean version to ${VERSION} for ${INTEGRATION}"
     deactivate
 done
 cd "${CURRENT_DIR}" || return
