fac7784f2a82b4585d34c27dbbd6fd896f835ced
Hadar-Vaknin <hadarvaknin1231@gmail.com>
2026-08-12 13:58:47 +0300

    removed le key from spec

---
+++ integrations/aws-v3/tests/config/test_live_events.py
@@ -0,0 +1,27 @@
+import os
+from unittest.mock import patch
+
+from aws.config.live_events import LIVE_EVENTS_API_KEY_ENV, get_live_events_api_key
+
+
+def test_get_live_events_api_key_from_integration_config() -> None:
+    with patch("aws.config.live_events.ocean") as mock_ocean:
+        mock_ocean.integration_config = {"live_events_api_key": "from-config"}
+        with patch.dict(os.environ, {}, clear=True):
+            assert get_live_events_api_key() == "from-config"
+
+
+def test_get_live_events_api_key_from_env_when_missing_from_config() -> None:
+    with patch("aws.config.live_events.ocean") as mock_ocean:
+        mock_ocean.integration_config = {}
+        with patch.dict(
+            os.environ, {LIVE_EVENTS_API_KEY_ENV: "from-env"}, clear=True
+        ):
+            assert get_live_events_api_key() == "from-env"
+
+
+def test_get_live_events_api_key_returns_none_when_unset() -> None:
+    with patch("aws.config.live_events.ocean") as mock_ocean:
+        mock_ocean.integration_config = {}
+        with patch.dict(os.environ, {}, clear=True):
+            assert get_live_events_api_key() is None
