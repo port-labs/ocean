70600ec9626e5e973073addd9c1b61cd8f5b125b
Venfi Oranai <venfioranai@gmail.com>
2026-08-20 10:51:33 +0300

    Converted dynamic.py to v2

--- port_ocean/config/dynamic.py
+++ port_ocean/config/dynamic.py
@@ -1,47 +1,52 @@
 import json
-from typing import Type, Any, Optional
+from typing import Annotated, Any, Optional, Type
 
 from humps import decamelize
-from pydantic.v1 import (
-    BaseConfig,
-    BaseModel,
+from pydantic import (
     AnyUrl,
+    BaseModel,
+    BeforeValidator,
+    ConfigDict,
+    Field,
+    TypeAdapter,
     create_model,
-    Extra,
-    parse_obj_as,
-    validator,
+    field_validator,
 )
-from pydantic.v1.fields import ModelField, Field
+from pydantic.fields import FieldInfo
 
-from port_ocean.config.base import BaseOceanModel
-
-
-class NoTrailingSlashUrl(AnyUrl):
-    @classmethod
-    def validate(cls, value: Any, field: ModelField, config: BaseConfig) -> "AnyUrl":
-        if value is not None:
-            if isinstance(value, (bytes, bytearray)):
-                try:
-                    value = value.decode()
-                except UnicodeDecodeError as exc:
-                    raise ValueError("URL bytes must be valid UTF-8") from exc
-            else:
-                value = str(value)
-
-            if value != "/":
-                value = value.rstrip("/")
-        return super().validate(value, field, config)
+from port_ocean.config.base import BaseOceanModel, sensitive_field
 
 
-class Configuration(BaseModel, extra=Extra.allow):
+def _normalize_no_trailing_slash_url(value: Any) -> Any:
+    if value is not None:
+        if isinstance(value, (bytes, bytearray)):
+            try:
+                value = value.decode()
+            except UnicodeDecodeError as exc:
+                raise ValueError("URL bytes must be valid UTF-8") from exc
+        else:
+            value = str(value)
+
+        if value != "/":
+            value = value.rstrip("/")
+    TypeAdapter(AnyUrl).validate_python(value)
+    return value
+
+
+NoTrailingSlashUrl = Annotated[str, BeforeValidator(_normalize_no_trailing_slash_url)]
+
+
+class Configuration(BaseModel):
+    model_config = ConfigDict(extra="allow")
+
     name: str
     type: str
     required: bool = False
-    default: Optional[Any]
+    default: Optional[Any] = None
     sensitive: bool = False
 
 
-def dynamic_parse(value: Any, field: ModelField) -> Any:
+def dynamic_parse(value: Any, field: FieldInfo) -> Any:
     should_json_load = issubclass(field.annotation, dict) or issubclass(
         field.annotation, list
     )
@@ -54,11 +59,11 @@ def dynamic_parse(value: Any, field: ModelField) -> Any:
 
 
 def default_config_factory(configurations: Any) -> Type[BaseModel]:
-    configurations = parse_obj_as(list[Configuration], configurations)
+    configurations = TypeAdapter(list[Configuration]).validate_python(configurations)
     fields: dict[str, tuple[Any, Any]] = {}
 
     for config in configurations:
-        field_type: Type[Any]
+        field_type: Any
 
         match config.type:
             case "object":
@@ -78,18 +83,22 @@ def default_config_factory(configurations: Any) -> Type[BaseModel]:
 
         default: Any = ... if config.required else None
         if config.default is not None:
-            default = parse_obj_as(field_type, config.default)
+            default = TypeAdapter(field_type).validate_python(config.default)
         fields[decamelize(config.name)] = (
             field_type,
-            Field(default, sensitive=config.sensitive),
+            sensitive_field(default=default) if config.sensitive else Field(default),
         )
 
     dynamic_model = create_model(  # type: ignore
-        __model_name="Config",
+        "Config",
         __base__=BaseOceanModel,
         **fields,
         __validators__={
-            "dynamic_parse": validator("*", pre=True, allow_reuse=True)(dynamic_parse)
+            "dynamic_parse": field_validator("*", mode="before")(
+                lambda cls, value, info: dynamic_parse(
+                    value, cls.model_fields[info.field_name]
+                )
+            )
         },
     )
     return dynamic_model
