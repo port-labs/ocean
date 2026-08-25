ccff25f0134e0430c8579a216cb302d8191803a8
Venfi Oranai <venfioranai@gmail.com>
2026-08-25 11:23:33 +0300

    Export probe config from package

--- port_ocean/core/probe/__init__.py
+++ port_ocean/core/probe/__init__.py
@@ -1,3 +1,4 @@
 # mypy: implicit_reexport
+from port_ocean.core.probe.config import ProbeConfig
 from port_ocean.core.probe.context import ProbeContext
 from port_ocean.core.probe.result import ProbeCheck, ProbeResult, ProbeStatus
