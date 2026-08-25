69f1eee52b49ec6e76c5d6fb44c8e5d104f553d2
Venfi Oranai <venfioranai@gmail.com>
2026-08-25 11:06:04 +0300

    Context has init function

--- port_ocean/core/probe/context.py
+++ port_ocean/core/probe/context.py
@@ -1,17 +1,28 @@
 from datetime import datetime, timezone
+from pathlib import Path
 
 from loguru import logger
 
 from port_ocean.core.probe.result import ProbeResult
+from port_ocean.utils.misc import get_spec_kinds
 
 
 class ProbeContext:
     result: ProbeResult
     probe_id: str | None
+    available_kinds: list[str]
 
     def __init__(self, probe_id: str | None = None) -> None:
         self.probe_id = probe_id
+        self.available_kinds = []
         self.result = ProbeResult()
+
+    def initialize(self, path: str | Path = ".") -> None:
+        self.available_kinds = get_spec_kinds(path)
+        if self.probe_id is None:
+            logger.debug("Local probe: skipping start report")
+            return
+        logger.debug(f"Reporting probe start to Port for probe {self.probe_id}")
 
     def update_progress(self) -> None:
         if self.probe_id is None:
