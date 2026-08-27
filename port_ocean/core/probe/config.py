1fa3c71db417577aad9f98efcfeb70c58845a827
Venfi Oranai <venfioranai@gmail.com>
2026-08-27 09:31:57 +0300

    Added config class

---
+++ port_ocean/core/probe/config.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+from port_ocean.core.probe.models import ProbeMode
+
+
+@dataclass
+class ProbeConfig:
+    path: Path = Path(".")
+    """The runtime path of the process"""
+    kinds: list[str] | None = None
+    """A list of specific kinds to probe. If not supplied, the probe will run on all supported kinds."""
+    mode: ProbeMode = ProbeMode.SHALLOW
