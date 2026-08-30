8bb4efce2d55c4877aecda239072f965aafe052e
Venfi Oranai <venfioranai@gmail.com>
2026-08-30 11:50:13 +0300

    Added reporting mode enum

--- port_ocean/core/probe/__init__.py
+++ port_ocean/core/probe/__init__.py
@@ -4,6 +4,7 @@ from port_ocean.core.probe.context import ProbeContext
 from port_ocean.core.probe.models import (
     ProbeCheck,
     ProbeMode,
+    ProbeReportingMode,
     ProbeStatus,
     ProbeCheckStatus,
     ProbeReportStage,
