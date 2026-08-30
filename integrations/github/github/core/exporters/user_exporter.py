8a5a393f6cf3dbf6bb7117b5005ab9e28e78bd7c
jason.nwakaeze <jason.nwakaeze@port.io>
2026-08-30 10:42:56 +0100

    catch GraphQLForbiddenFieldError in verified domain email queries

--- integrations/github/github/core/exporters/user_exporter.py
+++ integrations/github/github/core/exporters/user_exporter.py
@@ -4,6 +4,7 @@ from port_ocean.core.ocean_types import ASYNC_GENERATOR_RESYNC_TYPE, RAW_ITEM
 from github.clients.http.graphql_client import GithubGraphQLClient
 from github.core.exporters.abstract_exporter import AbstractGithubExporter
 from github.core.options import SingleUserOptions, ListUserOptions
+from github.helpers.exceptions import GraphQLForbiddenFieldError
 from github.helpers.gql_queries import (
     FETCH_GITHUB_USER_GQL,
     FETCH_GITHUB_USER_WITH_VERIFIED_EMAILS_GQL,
@@ -32,9 +33,24 @@ class GraphQLUserExporter(AbstractGithubExporter[GithubGraphQLClient]):
             variables = {"login": login_option}
 
         payload = self.client.build_graphql_payload(query, variables)
-        response = await self.client.send_api_request(
-            self.client.base_url, method="POST", json_data=payload
-        )
+        try:
+            response = await self.client.send_api_request(
+                self.client.base_url, method="POST", json_data=payload
+            )
+        except GraphQLForbiddenFieldError:
+            if not include_verified_domain_emails:
+                raise
+            logger.warning(
+                f"organizationVerifiedDomainEmails returned FORBIDDEN for "
+                f"user '{login_option}' in '{organization}', falling back to query without verified emails"
+            )
+            payload = self.client.build_graphql_payload(
+                FETCH_GITHUB_USER_GQL, {"login": login_option}
+            )
+            response = await self.client.send_api_request(
+                self.client.base_url, method="POST", json_data=payload
+            )
+
         if not response:
             logger.warning(f"No user found with login: {login_option}")
             return None
@@ -65,8 +81,23 @@ class GraphQLUserExporter(AbstractGithubExporter[GithubGraphQLClient]):
             "organization": organization,
             "__path": "organization.membersWithRole",
         }
-        async for users in self.client.send_paginated_request(query, variables):
-            await enrich_members_with_saml_email(
-                self.client, organization, users, include_saml_email
+        try:
+            async for users in self.client.send_paginated_request(query, variables):
+                await enrich_members_with_saml_email(
+                    self.client, organization, users, include_saml_email
+                )
+                yield users
+        except GraphQLForbiddenFieldError:
+            if not include_verified_domain_emails:
+                raise
+            logger.warning(
+                f"organizationVerifiedDomainEmails returned FORBIDDEN for "
+                f"'{organization}', falling back to query without verified emails"
             )
-            yield users
+            async for users in self.client.send_paginated_request(
+                LIST_ORG_MEMBER_GQL, variables
+            ):
+                await enrich_members_with_saml_email(
+                    self.client, organization, users, include_saml_email
+                )
+                yield users
