b3b3a2f8d683b0a9854d1e25c8d0a417d1a46152
Venfi Oranai <venfioranai@gmail.com>
2026-09-02 10:49:16 +0300

    Removed redundant list conversion

--- port_ocean/core/probe/context.py
+++ port_ocean/core/probe/context.py
@@ -75,7 +75,7 @@ class ProbeContext:
                     list(configured_kinds_set - supported_kinds_set), supported_kinds
                 )
 
-            self.available_kinds = sorted(list(configured_kinds_set))
+            self.available_kinds = sorted(configured_kinds_set)
         else:
             self.available_kinds = get_port_app_config_kinds(port_app_config_class)
 
