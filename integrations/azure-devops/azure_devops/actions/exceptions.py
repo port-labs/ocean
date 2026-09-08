74ffd1167a709d647882cb752ea2c99a77d317d6
Yuval Bekker <yuvalb@port.io>
2026-09-08 12:06:17 +0300

    add pull request thread create action for ado

--- integrations/azure-devops/azure_devops/actions/exceptions.py
+++ integrations/azure-devops/azure_devops/actions/exceptions.py
@@ -56,3 +56,9 @@ class UpdatePullRequestError(AzureDevopsActionError):
     """Raised when the Azure DevOps API returns an error while updating a pull request."""
 
     DEFAULT_STATUS_LABEL = "Update failed"
+
+
+class CreatePullRequestThreadError(AzureDevopsActionError):
+    """Raised when the Azure DevOps API returns an error while creating a thread."""
+
+    DEFAULT_STATUS_LABEL = "Create failed"
--- integrations/azure-devops/azure_devops/actions/registry.py
+++ integrations/azure-devops/azure_devops/actions/registry.py
@@ -1,5 +1,8 @@
 from port_ocean.context.ocean import ocean
 
+from azure_devops.actions.create_pull_request_thread_executor import (
+    CreatePullRequestThreadExecutor,
+)
 from azure_devops.actions.trigger_pipeline_executor import TriggerPipelineExecutor
 from azure_devops.actions.update_pull_request_executor import UpdatePullRequestExecutor
 
@@ -8,3 +11,4 @@ def register_actions_executors() -> None:
     """Register all Azure DevOps action executors."""
     ocean.register_action_executor(TriggerPipelineExecutor())
     ocean.register_action_executor(UpdatePullRequestExecutor())
+    ocean.register_action_executor(CreatePullRequestThreadExecutor())
