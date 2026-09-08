0ce6c30f9a117768efb85e59a1527471f7d630a3
Venfi Oranai <venfioranai@gmail.com>
2026-09-08 10:11:14 +0300

    Added executor

--- integrations/jira/jira/actions/registry.py
+++ integrations/jira/jira/actions/registry.py
@@ -1,8 +1,10 @@
 from port_ocean.context.ocean import ocean
 
+from jira.actions.change_issue_status_executor import ChangeIssueStatusExecutor
 from jira.actions.create_issue_executor import CreateIssueExecutor
 
 
 def register_actions_executors() -> None:
     """Register all actions executors."""
     ocean.register_action_executor(CreateIssueExecutor())
+    ocean.register_action_executor(ChangeIssueStatusExecutor())
